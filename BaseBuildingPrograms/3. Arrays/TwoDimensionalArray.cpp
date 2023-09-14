c4a960adfc4a9a4b5c56a65d53f7c86634d62731
Bishnu Mishra <mishravishnu369@gmail.com>
2023-09-15 01:40:48 +0530

    added TransposeMatrix.cpp leetcode 867

--- BaseBuildingPrograms/3. Arrays/TwoDimensionalArray.cpp
+++ BaseBuildingPrograms/3. Arrays/TwoDimensionalArray.cpp
@@ -5,18 +5,16 @@
 using namespace std;
 
 int main() {
-    int arr[][4] = {   // mentioning at least column while initializing 2D array is important.
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12}
+    int arr[][3] = {   // mentioning at least column while initializing 2D array is important.
+        {1, 2, 3},
+        {4, 5, 6}
     };
-
     // 2D arrays are stored in memory like a 1D array only, elements are mapped using formula: c*(i+j), where c=no.of columns in array, i & j are row & column indexes respectively.
 
-    int row = 3;
-    int col = 4;
+    int row = 2;
+    int col = 3;
 
-    // print row wise
+    // print original matrix
     for(int i=0; i<row; i++) {
         for(int j=0; j<col; j++) {
             cout << arr[i][j] << " ";
@@ -27,17 +25,16 @@ int main() {
     cout << endl << endl;
 
     // Transpose of matrix
-    int brr[4][3];
-    for(int i=0; i<col; i++) {
-        for(int j=0; j<row; j++) {
-            brr[i][j] = arr[j][i];
+    int brr[col][row];
+    for(int i=0; i<row; i++) {
+        for(int j=0; j<col; j++) {
+            brr[j][i] = arr[i][j];
         }
     }
 
     // Printing brr(Transpose of A)
-    int new_row = 4, new_col = 3;
-    for(int i=0; i<new_row; i++) {
-        for(int j=0; j<new_col; j++) {
+    for(int i=0; i<col; i++) {
+        for(int j=0; j<row; j++) {
             cout << brr[i][j] << " ";
         }
         cout << endl;
--- Leetcode&gfg/2. Arrays/TransposeMatrix.cpp
+++ Leetcode&gfg/2. Arrays/TransposeMatrix.cpp
@@ -5,10 +5,17 @@
 using namespace std;
 
 vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-    int rows = matrix.size();
-    int columns = matrix[0].size();
-    
+    int row = matrix.size();   // no.of rows
+    int col = matrix[0].size();   // no.of of elements in each row, i.e, no.of columns
+    vector<vector<int>> t_matrix(col, vector<int>(row));   // creates a new 2D vector t_matrix with the dimensions swapped
 
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            t_matrix[j][i] = matrix[i][j];
+        }
+    }
+    
+    return t_matrix;
 }
 
 int main() {
@@ -17,16 +24,15 @@ int main() {
         {4, 5, 6},
     };
 
-    transpose(matrix);
+    vector<vector<int>> t_matrix = transpose(matrix);
 
     // Printing the vector
-    for (int i = 0; i < matrix.size(); i++) {   // matrix.size() gives number of rows
-        for (int j = 0; j < matrix[i].size(); j++) {   // matrix[i].size() gives number of elements in ith row i.e, no.of columns in matrix
-            cout << matrix[i][j] << " ";
+    for (int i = 0; i < t_matrix.size(); i++) {   // matrix.size() gives number of rows
+        for (int j = 0; j < t_matrix[i].size(); j++) {   // matrix[i].size() gives number of elements in ith row i.e, no.of columns in matrix
+            cout << t_matrix[i][j] << " ";
         }
         cout << endl;
     }
     
-
     return 0;
 }
