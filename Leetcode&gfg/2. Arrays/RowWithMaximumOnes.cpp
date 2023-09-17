69f1283d78e99cae0159ddd8d575a99e97605f9a
Bishnu Mishra <mishravishnu369@gmail.com>
2023-09-18 00:02:38 +0530

    added RowWithMaximumOnes.cpp in Leetcode&gfg

---
+++ Leetcode&gfg/2. Arrays/RowWithMaximumOnes.cpp
@@ -0,0 +1,51 @@
+// Leetcode: 2643. Row With Maximum Ones
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printVector(vector<int>& v) {
+    int n = v.size();
+
+    for (const auto& element : v) {
+        cout << element << " ";
+    }
+}
+
+// Brute force
+// T.C: O(m*n)
+// S.C: O(1)
+vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+    vector <int> ans;
+    int n = mat.size();   // no.of rows in mat
+    int maxOneCount = -1;
+    int highestOneIndexRow = -1;
+
+    for(int i=0; i<n; i++) {
+        int oneCount = 0;
+        for(int j=0; j<mat[i].size(); j++) {
+            if(mat[i][j] == 1) {
+                oneCount++;
+            }
+            if(oneCount > maxOneCount) {
+                maxOneCount = oneCount;
+                highestOneIndexRow = i;
+            }
+        }
+    }
+
+    ans.push_back(highestOneIndexRow);
+    ans.push_back(maxOneCount);
+
+    return ans;
+}
+
+int main() {
+    vector<vector<int>> v = {{0, 0, 0}, {0, 1, 1}};
+    vector <int> ans;
+
+    ans = rowAndMaximumOnes(v);
+    printVector(ans);
+
+    return 0;
+}
