8f5e4050f5ad82557362df9a60554bdbc5e31d5b
Bishnu Mishra <mishravishnu369@gmail.com>
2023-09-17 20:56:29 +0530

    added missingNumber.cpp in Leetcode&gfg

---
+++ Leetcode&gfg/2. Arrays/MissingNumber.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Brute force
+// S.C: O(n)
+// T.C: O(1)
+int missingNumber(vector<int>& nums) {
+    int n = nums.size();
+    int sum = 0;
+    for(int i=0; i<n; i++) {
+        sum+= nums[i];
+    }
+
+    int total_sum = (n*(n+1))/2;
+
+    return total_sum - sum;     
+}
+
+
+int main() {
+    vector <int> v = {3, 0, 1};
+    cout << missingNumber(v);
+
+    return 0;
+}
