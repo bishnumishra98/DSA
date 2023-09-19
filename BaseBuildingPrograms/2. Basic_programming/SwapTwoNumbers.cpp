a23c7d23df0339bfd5523b1bdf7ac3a95255b794
Bishnu Mishra <mishravishnu369@gmail.com>
2023-09-19 13:36:33 +0530

    added SwapTwoNumbers.cpp in BaseBuildingPrograms

---
+++ BaseBuildingPrograms/2. Basic_programming/SwapTwoNumbers.cpp
@@ -0,0 +1,46 @@
+// 2 numbers can be swapped in three different ways
+
+#include <iostream>
+using namespace std;
+
+void swapByTemp(int a, int b) {
+    cout << "Swap by Temp:\n";
+
+    int temp = a;   // temp = 2
+    a = b;   // a = 3
+    b = temp;   // b = 2
+
+    cout << "a = " << a << "   " << "b = " << b;
+    cout << endl << endl;
+}
+
+void swapByArithmetic_operation(int a, int b) {
+    cout << "Swap by Arithmetic_operation:\n";
+
+    a = a + b;   // 2 + 3 = 5
+    b = a - b;   // 5 - 3 = 2
+    a = a - b;   // 5 - 2 = 3
+
+    cout << "a = " << a << "   " << "b = " << b;
+    cout << endl << endl;
+}
+
+void swapByXOR(int a, int b) {
+    cout << "Swap by XOR:\n";
+
+    a = a ^ b;   // a = 2 ^ 3
+    b = a ^ b;   // b = 2 ^ 3 ^ 3 = 2
+    a = a ^ b;   // a = 2 ^ 3 ^ 2 = 3
+
+    cout << "a = " << a << "   " << "b = " << b;
+}
+
+
+int main() {
+    int a = 2, b = 3;
+    swapByTemp(a, b);
+    swapByArithmetic_operation(a, b);
+    swapByXOR(a, b);
+
+    return 0;
+}
