49925921b234c0a6941d6cc35acff2767b11ab0b
Bishnu Mishra <mishravishnu369@gmail.com>
2023-10-02 15:45:02 +0530

    added InsertionSort.cpp file in BaseBuildingPrograms

--- BaseBuildingPrograms/5. Sorting/InsertionSort.cpp
+++ BaseBuildingPrograms/5. Sorting/InsertionSort.cpp
@@ -1,4 +1,34 @@
-// Insertion Sort: 
+// Insertion Sort: Starting from 1st to 'n-1' index, pick one element at a time and comparing it with the
+// elements on its left, insert it on its correct index.
+
+// Approach   --->   Consider the following array: int arr[5] = {45, 35, 55, 15, 25};   here n = 5.
+
+// {45, 35, 55, 15, 25}
+//  0   1   2   3   4
+
+// For outer loop i=1,
+// {35, 45, 55, 15, 25}
+//  0   1   2   3   4
+//  j   i
+
+// For outer loop i=2, no change:
+// {35, 45, 55, 15, 25}
+//  0   1   2   3   4
+//      j   i
+
+// For outer loop i=3,
+// {15, 35, 45, 55, 25}
+//  0   1   2   3   4
+//          j   i
+
+// For outer loop i=4,
+// {15, 25, 35, 45, 55}
+//  0   1   2   3   4
+//              j   i
+
+// Hence, for n = 5, outer loop runs 'n-1' times. i represents outer loop, and j represents inner loop.
+// i runs from 1 to 'n-1'; and j runs from 'i-1' to 0. Dry run for better understanding.
+
 
 #include <iostream>
 #include <vector>
@@ -13,12 +43,28 @@ void print(vector<int>& v) {
 // T.C: O(n2)
 // S.C: O(1)
 void insertionSort(vector<int>& v) {
-    
+    int n = v.size();
+
+    for(int i=1; i<n; i++) {
+        int keyElement = v[i];
+        int j = i-1;
+
+        while(j>=0 && v[j]>keyElement) {
+            // right shifting elements by 1 index that are bigger than key and fall on left of key
+            v[j+1] = v[j];
+            j--;
+        }
+
+        // When program comes out of while loop, j is either -1 or v[j] is smaller than keyElement.
+        // v[j+1] is the index from where right elements are greater than keyElement and left elements
+        // are smaller than keyElement, i.e. it is the accurate index to place keyElement
+        v[j+1] = keyElement;
+    }
 }
 
 
 int main() {
-    vector <int> v = {45, 35, 55, 25, 15};
+    vector <int> v = {45, 35, 55, 15, 25};
 
     insertionSort(v);
 
--- BaseBuildingPrograms/5. Sorting/SelectionSort.cpp
+++ BaseBuildingPrograms/5. Sorting/SelectionSort.cpp
@@ -1,27 +1,32 @@
-// Selection Sort: Keep finding and inserting smallest element at ith index, where i runs 'n-1' times,
-// where n = size of array.
+// Selection Sort: Select the element at ith index and swap it with the smaller element found in the range
+// 'i+1' to 'n-1', where i runs 'n-1', where n = size of array.
 
-// Approach   --->   Consider the following array: int arr[5] = {45, 35, 55, 25, 15};   here n = 5.
+// Approach   --->   Consider the following array: int arr[5] = {55, 45, 35, 25, 15};   here n = 5.
 
-// {45, 35, 55, 25, 15}
+// {55, 45, 35, 25, 15}
 //  0   1   2   3   4
 
-// For the outer loop i=0, swap element at 0th index, i.e. 45 and smallest element at (1-4)th index, i.e. 15.
-// {15, 35, 55, 25, 45}
+// For the outer loop i=0, The element at ith index i.e. 0th index is 15 and
+// smallest element in ('i+1'-'n-1')th i.e. (1-4)th index is 55. So, 15 and 55 gets swapped.
+// {15, 45, 35, 25, 55}
 //  0   1   2   3   4
 
-// For the outer loop i=1, swap element at 1st index, i.e. 35 and smallest element at (2-4)th index, i.e. 25.
-// {15, 25, 55, 35, 45}
-//  0   1   2   3   4
-
-// For the outer loop i=2, swap element at 2nd index, i.e. 55 and smallest element at (3-4)th index, i.e. 35.
-// {15, 25, 35, 55, 45}
-//  0   1   2   3   4
-
-// For the outer loop i=3, swap element at 3rd index, i.e. 55 and smallest element,
-// aka the only element at 4th index, i.e. 45.
+// For the outer loop i=1, swap the element at ith index i.e. 1th index is 45 and
+// smallest element in ('i+1'-'n-1')th i.e. (2-4)th index is 25. So, 45 and 25 gets swapped.
 // {15, 25, 35, 45, 55}
 //  0   1   2   3   4
+
+// For the outer loop i=2, swap the element at ith index i.e. 2nd index is 35 and
+// smallest element in ('i+1'-'n-1')th i.e. (3-4)th index is 35. So, no change in array happens.
+// {15, 25, 35, 45, 55}
+//  0   1   2   3   4
+
+// For the outer loop i=3, swap the element at ith index i.e. 3rd index is 45 and
+// smallest element in ('i+1'-'n-1')th i.e. (4)th index is 55. So, no change in array happens.
+// {15, 25, 35, 45, 55}
+//  0   1   2   3   4
+
+// Hence, outer loop will always run 'n-1' times, thus it ranges from 0 to 'n-2', where n = size of array.
 
 
 #include <iostream>
@@ -58,7 +63,7 @@ void selectionSort(vector<int>& v) {
 
 
 int main() {
-    vector <int> v = {45, 35, 55, 25, 15};
+    vector <int> v = {55, 45, 35, 25, 15};
 
     selectionSort(v);
 
