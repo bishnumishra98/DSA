967099a040d943495429d3cd4b28744108c6e428
Bishnu Mishra <mishravishnu369@gmail.com>
2023-10-19 22:16:57 +0530

    added Basic math folder in BaseBuilding

--- BaseBuildingPrograms/7. Basic Maths/FindPrimesInALargeRange.cpp
+++ BaseBuildingPrograms/7. Basic Maths/FindPrimesInALargeRange.cpp
@@ -1,3 +1,5 @@
+// Understand SieveOfEratosthenes and SieveOfEratosthenesOptimised before solving this problem
+
 // The stack sizes in memory is typically between 1MB to 8MB.
 
 // Thus, if we create a 10^6 size array of type double, it would occupy 8,000,000 bytes, i.e 8MB of space.
@@ -23,5 +25,70 @@
 
 
 
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
 
+vector<bool> createSieveArray(int n) {
+    vector<bool> sieve(n+1, true);
+    sieve[0] = sieve[1] = false;
 
+    for (int i=2; i*i<=n; i++) {
+        if (sieve[i] == true) {
+            int j = i * i;
+            while (j<=n) {
+                sieve[j] = false;
+                j += i;
+            }
+        }
+    }
+
+    return sieve;
+}
+
+// T.C:
+// S.C:
+vector<bool> createSieveArray_largeRange(int L, int R) {
+    // Getting prime marking array to be used to mark primes in segmented sieve.
+    vector<bool> sieve = createSieveArray(sqrt(R));
+
+    vector<int> basePrimes;
+
+    for (int i = 0; i < sieve.size(); i++) {
+        if(sieve[i]) {
+            basePrimes.push_back(i);
+        } 
+    }
+
+    vector<bool> segSieve(R - L + 1, true);
+    if (L == 1) {
+        segSieve[0] = false;
+    }
+
+    for (auto prime : basePrimes) {
+        int first_mul = (L / prime) * prime;
+        first_mul = first_mul < L ? first_mul + prime : first_mul;
+        int j = max(first_mul, prime * prime);
+        while (j<=R) {
+            segSieve[j-L] = false;
+            j += prime;
+        }
+    }
+    
+    return segSieve;
+}
+
+int main() {
+    int left = 110, right = 130;
+
+    vector<bool> segsieve = createSieveArray_largeRange(left, right);
+
+    for (int i=0; i<segsieve.size(); i++) {
+        if(segsieve[i]) {
+            cout << left + i << " ";
+        }
+    }
+
+    return 0;
+}
