0bb9de2990885537ff141dcad0ec18d681672948
Bishnu Mishra <mishravishnu369@gmail.com>
2023-10-27 22:45:58 +0530

    added Pointer folder in BaseBuilding

--- BaseBuildingPrograms/9. Recursion/LinearSearchInAnArray.cpp
+++ BaseBuildingPrograms/9. Recursion/LinearSearchInAnArray.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int linearSearch(int *arr, int size, int index, int target) {
     //base case
-    if (index >= size) {
-        return -1;
+    if (index == size) {
+        return -1;   // element not found if index becomes equal to size of array
     }
 
     //processing
@@ -19,7 +19,7 @@ int linearSearch(int *arr, int size, int index, int target) {
 int main() {
     int arr[] = {10, 20, 30, 40, 50};
     int size = 5;
-    int target = 40;
+    int target = 50;
     int index = 0;
 
     if(linearSearch(arr, size, index, target) == -1) {
