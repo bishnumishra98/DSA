153c06bc7ce715e75fd7f602999ae4b6e9a5fb4c
Bishnu Mishra <mishravishnu369@gmail.com>
2023-11-05 23:48:38 +0530

    added HouseRobberII in BaseBuilding/Recursion

--- BaseBuildingPrograms/9. Recursion/HouseRobberII.cpp
+++ BaseBuildingPrograms/9. Recursion/HouseRobberII.cpp
@@ -1,21 +1,21 @@
-// leetcode: 198. House Robber   --->   You are a professional robber planning to rob houses
-// along a street. Each house has a certain amount of money stashed, the only constraint stopping
-// you from robbing each of them is that adjacent houses have security systems connected and it
-// will automatically contact the police if two adjacent houses were broken into on the same night.
-// Given an integer array nums representing the amount of money of each house, return the maximum
-// amount of money you can rob tonight without alerting the police.
+// leetcode: 213. House Robber II   --->   The question is same as House Robber, the only difference is
+// that this time all houses are arranged in a circle.
 
 // Example 1:
+// Input: nums = [2,3,2]
+// Output: 3
+// Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+// because they are adjacent houses.
+
+// Example 2:
 // Input: nums = [1,2,3,1]
 // Output: 4
 // Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
 // Total amount you can rob = 1 + 3 = 4.
 
-// Example 2:
-// Input: nums = [2,7,9,3,1]
-// Output: 12
-// Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
-// Total amount you can rob = 2 + 9 + 1 = 12.
+// Example 3:
+// Input: nums = [1,2,3]
+// Output: 3
 
 // Note: This problem is being solved with recursion for now, just to understand recursion better.
 // But optimal approach will be solved by dynamic programming.
@@ -42,20 +42,31 @@ int solve(vector<int>& nums, int start, int end) {
     return max(option1, option2);
 }
 
-// T.C: O(2^n);   where n = nums.size()
+// T.C: O(2^n) + O(2^n) = O(2^n);   where n = nums.size()
 // S.C: O(n)
 int rob(vector<int>& nums) {
     int size = nums.size();
-    int start = 0;
-    int end = size - 1;
 
-    int ans = solve(nums, start, end);
+    if(size == 1) {   // if only 1 house is present, I can rob that
+        return nums[0];
+    }
 
-    return ans;
+    int start = 0;   // first index
+    int end = size - 1;   // last index
+
+    // When I am at the first house, I won't rob the last house as last house is just before the first
+    // house. Thus, the array where I can rob exist only from the index '0' to 'second last index'.
+    int ans1 = solve(nums, start, end-1);
+
+    // When I am at the last house, I won't rob the first house as first house is just after the last
+    // house. Thus, the array where I can rob exist only from the index '1' to 'last index'.
+    int ans2 = solve(nums, start+1, end);
+
+    return max(ans1, ans2);
 }
 
 int main() {
-    vector <int> v = {1, 2, 3, 1};
+    vector <int> v = {2, 3, 2};
     cout << rob(v);
 
     return 0;
