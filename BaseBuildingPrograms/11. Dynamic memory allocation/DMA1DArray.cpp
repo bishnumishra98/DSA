8fafe5171f794ebfcf4e800117f5b8e999181b14
Bishnu Mishra <mishravishnu369@gmail.com>
2023-11-11 01:04:41 +0530

    dynamic memory allocation/BaseBuildingPrograms

--- BaseBuildingPrograms/11. Dynamic memory allocation/DMA1DArray.cpp
+++ BaseBuildingPrograms/11. Dynamic memory allocation/DMA1DArray.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 using namespace std;
 
+void populateData(int *p, int size) {
+    for(int i=0; i<size; i++) {
+        p[i] = i*2;
+    }
+}
+
+void print(int *p, int size) {
+    for(int i=0; i<size; i++) {
+        cout << p[i] << " ";
+    }
+}
+
 int main() {
     // i)   Allocating a 1D array using the keyword 'new':
-            int *ptr = new int[10];
+            int *ptr = new int[10];   // allocating an int type array of size 10,
+                                     // whose base address is pointed by 'ptr'
+
+            populateData(ptr, 10);   // populating values {0, 1, 4, 9, ..., 81} inside the array
+
+            print(ptr, 10);   // printing values inside the array
+            cout << endl << endl;
+
+            delete[] ptr;   // deallocating memory
+
+    
+    // ii)  Allocating a 1D array using the function 'malloc()':
+            int *ptr2 = (int*) malloc(10 * sizeof(int));   // allocating 10*4, i.e. 40 bytes
+            // of memory and casting it to an int pointer. The base address of this memory block
+            // is pointed to by 'ptr2'.
+
+            populateData(ptr2, 10);
+
+            print(ptr2, 10);
+
+            free(ptr2);   // deallocating memory
 
 
     return 0;
