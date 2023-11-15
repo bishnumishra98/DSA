b361ee0b5c0037eeeb8fbf04b7e545c29fd8dcec
Bishnu Mishra <mishravishnu369@gmail.com>
2023-11-15 13:11:34 +0530

    OOPs concept/BaseBuildingPrograms

---
+++ BaseBuildingPrograms/12. OOPs Concept/OOPs7_Constructors.cpp
@@ -0,0 +1,58 @@
+// A constructor is a special member function of a class that is automatically called each time 
+// an object of the class is created. The purpose of a constructor is to initialize the
+// object's data members and set up the object's initial state.
+
+// Properties of constructors are:
+// 1) Constructor is an instance member function of class. It can never be static.
+// 2) The name of constructor must be same as class name.
+// 3) It has no return type.
+// 4) Constructors must be placed in section of class.
+// 5) If we do not specify a constructor, C++ compiler generates a default constructor for object
+//    which expects no parameter and has an empty body.
+
+#include <iostream>
+using namespace std;
+
+class StudentsOfClass12 {
+    private:
+        int standard;
+        int age;
+        float weight;
+
+    public:
+    // Note: constructor is also called ctor.
+        // non-parameterized constructor: initializes the object with default values.
+        StudentsOfClass12() {
+            standard = 12;
+            age = 18;
+            weight = 60;
+        }
+        // Note: if the programmer does not explicitly define any constructor in the class, the C++ compiler
+        // generates a default constructor which looks the same as above, but with an empty body. 
+
+        // parameterized constructor: initializes the object with specified values.
+        StudentsOfClass12(int age, float w) {
+            standard = 12;
+            this->age = age;
+            weight = w;
+        }
+
+        void showStudentInfo() {
+            cout << "Standard: " << standard << "\n"
+                 << "Age: " << age << "\n"
+                 << "Weight: " << weight << "\n\n";
+        }
+};
+
+int main() {
+    StudentsOfClass12 s1;   // even if we don't pass values for this object, it will be initialized with
+    // an initial state of {12, 18, 60}. That's one of the big advantages using a constructor.
+    StudentsOfClass12 s2(18, 55);
+    StudentsOfClass12 s3(17, 65);
+
+    s1.showStudentInfo();
+    s2.showStudentInfo();
+    s3.showStudentInfo();
+
+    return 0;
+}
