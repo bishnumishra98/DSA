0a90581fd32221b1793c7094ce49bc1bd4584ade
Bishnu Mishra <mishravishnu369@gmail.com>
2023-12-05 23:30:35 +0530

    OOPs concept/BaseBuildingPrograms

--- BaseBuildingPrograms/12. OOPs Concept/OOPs32_VirtualTable.cpp
+++ BaseBuildingPrograms/12. OOPs Concept/OOPs32_VirtualTable.cpp
@@ -1,17 +1,17 @@
 // Module: Virtual functions
 
-// How virtual table help in achieving runtime polymorphism or late binding ?
-// -> The compiler makes a hidden pointer named *_vptr in the class containing virtual function
-//    at compile time. All the derived classes of that class will also inherit vptr. All
-//    objects have their own vptr, and the role of vptr is to point to a virtual table.
-// -> Then it makes a static array named vtable(virtual table), which is an array of function
-//    pointers. Note that the virtual table is created per class, not per object. All objects of
-//    the same class share the same virtual table.
+// How does the virtual table help in achieving runtime polymorphism or late binding?
+// -> The compiler generates a static array called vtable (virtual table) for each class,
+//    and this array consists of function pointers. It's important to note that the vtable is
+//    created per class, not per object. Objects of the same class points to the same vtable,
+//    using a hidden pointer member variable called *_vptr.
+// -> Each object has its own vptr, which points to the vtable of the type of object, i.e.
+//    class of object.
 // -> When a virtual function is called through a base class pointer or reference, the program
-//    uses the vptr to access the correct virtual table.
-// -> Based on that vtable, correct function is called with the help of array of function
-//    pointers present in the vtable.
-// -> For example: In the below illustration, 
+//    uses the vptr to access the correct vtable associated with the actual object's type.
+// -> Based on the vtable, the correct function is invoked by referencing the array of
+//    function pointers present in the vtable.
+
 
 #include <iostream>
 using namespace std;
@@ -23,6 +23,9 @@ public:
     }
     void erase() {
         cout << "Erasing a shape." << endl;
+    }
+    void enlarge() {
+        cout << "Enlarge the shape." << endl;
     }
 };
 
@@ -37,10 +40,12 @@ public:
 };
 
 int main() {
-    Circle circle;
+    // memory allocation in stack
+    // Circle circle;
+    // Shape* shapePtr = &circle;
 
-    // Base class pointer pointing to an object of the derived class
-    Shape* shapePtr = &circle;
+    // memory allocation in heap
+    Shape* shapePtr = new Circle();
 
     // Late binding through virtual function
     shapePtr->draw();   // o/p: Drawing a circle.
@@ -48,26 +53,18 @@ int main() {
     // Early binding, as erase() is not a virtual function
     shapePtr->erase();   // o/p: Erasing a shape.
 
+    // As enlarge() is inherited in Circle
+    shapePtr->enlarge();   // o/p: Enlarge the shape.
+
     return 0;
 }
 
-// Q) Can virtual functions be static ?
-// A) No, virtual functions cannot be static in C++. This is because virtual functions
-//    are member functions of a class, and static member functions are associated with
-//    the class itself, not with any particular object of the class. When a virtual
-//    function is called, the virtual table (vtable) of the object is used to determine
-//    which implementation of the function to call. Static member functions do not have
-//    vtables, so they cannot be virtual.
-
 // Q) Can virtual functions be static?
 // A) No, virtual functions cannot be static in C++. This is because virtual functions
-//    are member functions of a class, and static member functions are associated with
-//    the class itself, not with any particular object of the class. When a virtual
-//    function is declared in a class, it is meant to be overridden by derived classes.
-//    When a virtual function is called through a base class pointer or reference,
-//    the virtual table (vtable) of the actual object's type is used to determine
-//    which implementation of the function to call. Static member functions, on the
-//    other hand, do not participate in the polymorphic behavior of virtual functions
-//    and do not have vtables. Therefore, they cannot be declared as virtual.
+//    are member functions of a class(associated to instances of class), on the other hand
+//    static member functions are associated with the class itself, not with any particular
+//    instance of the class. When a virtual function is declared in a class, it is meant to
+//    be overridden by derived classes. But static member functions do not participate in
+//    polymorphic behavior like virtual functions do. Therefore, they cannot be static.
 
 
