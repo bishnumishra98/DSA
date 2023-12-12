62de4fd7d713fec59408f2d813a2bf5aab6b63f5
Bishnu Mishra <mishravishnu369@gmail.com>
2023-12-13 01:30:58 +0530

    OOPs concept/BaseBuildingPrograms

---
+++ BaseBuildingPrograms/13. Linked_List/LL07_DoublyLinkedListBasics.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+// interface of node for doubly linked list
+class Node {
+    public:
+        int data;
+        Node* prev;
+        Node* next;
+        Node() {
+            this->prev = NULL;
+            this->next = NULL;
+        }
+        Node(int data) {
+            this->data = data;
+            this->prev = NULL;
+            this->next = NULL;
+        }
+};
+
+// function to print elements of linked list
+void printElements(Node* head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+// function to find length of linked list, i.e. no.of nodes
+int lengthLL(Node* head) {
+    Node* temp = head;
+    int len = 0;
+    while(temp != NULL) {
+        len++;
+        temp = temp->next;
+    }
+    return len;
+}
+
+// function for printing details of linked list
+void printLLDetails(Node* head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << "Node_address: " << temp << "\n";
+        cout << "Node_Data: " << temp->data << "\n";
+        cout << "Previous_Node_address: " << temp->prev << "\n";
+        cout << "Next_Node_address: " << temp->next << "\n\n";
+        temp = temp->next;   // removing 'temp' from this Node and pointing to next Node
+    }
+}
+
+int main() {
+    // creating nodes
+    Node* first = new Node(2);
+    Node* second = new Node(4);
+    Node* third = new Node(6);
+    Node* fourth = new Node(8);
+    Node* fifth = new Node(10);
+
+    // creating linked list
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+
+    second->prev = first;
+    third->prev = second;
+    fourth->prev = third;
+    fifth->prev = fourth;
+
+    Node* head = first;
+
+    cout << "Details of linked list are:\n";
+    printLLDetails(head);
+
+    return 0;
+}
+
+// o/p:
+// Details of linked list are:
+// Node_address: 0xe9bf20
+// Node_Data: 2
+// Previous_Node_address: 0
+// Next_Node_address: 0xe9bf38       
+
+// Node_address: 0xe9bf38
+// Node_Data: 4
+// Previous_Node_address: 0xe9bf20   
+// Next_Node_address: 0xe9bf50       
+
+// Node_address: 0xe9bf50
+// Node_Data: 6
+// Previous_Node_address: 0xe9bf38   
+// Next_Node_address: 0xe9bf68       
+
+// Node_address: 0xe9bf68
+// Node_Data: 8
+// Previous_Node_address: 0xe9bf50   
+// Next_Node_address: 0xe9bf80       
+
+// Node_address: 0xe9bf80
+// Node_Data: 10
+// Previous_Node_address: 0xe9bf68   
+// Next_Node_address: 0
---
+++ BaseBuildingPrograms/13. Linked_List/LL08_InsertionInDoublyLinkedList.cpp
@@ -0,0 +1,15 @@
+// A Node in linked list can be inserted in any one of the three areas:
+// 1) At head of linked list:
+//      
+// 2) At tail of linked list:
+//      
+// 3) In between the linked list, i.e. at a particular position(let positions start from 1):
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
