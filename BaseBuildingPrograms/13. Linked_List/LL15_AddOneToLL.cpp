be12e6f0723504aab57a31fd90ee93082f9d198f
Bishnu Mishra <mishravishnu369@gmail.com>
2023-12-30 23:11:29 +0530

    LinkedList

--- BaseBuildingPrograms/13. Linked_List/LL15_AddOneToLL.cpp
+++ BaseBuildingPrograms/13. Linked_List/LL15_AddOneToLL.cpp
@@ -59,30 +59,32 @@ Node* reverseLL(Node* head) {
     return head;
 }
 
-// T.C: O(3n) = O(n)
+// T.C: O(n)
 // S.C: O(1)
 Node* addOneToLL(Node* head) {
-    head = reverseLL(head);   // reversing list to make addition easy
-            
+    head = reverseLL(head);   // reversing list to make addition easy        
     Node* temp = head;
-    int carry = 1;
     
-    while(carry) {
-        temp->data += 1;   // adding one to temp node
+    while(1) {
+        temp->data += 1;   // adding 1 to temp node
         
-        if(temp->data < 10) {
-            return reverseLL(head);   // if no carry we can reverse back list and return it
-        } else {
-            temp->data = 0;   // else we continue with taking carry forward
+        if(temp->data < 10) {    // if 'data < 10', no need of any carry. We can return reversed list.
+            return reverseLL(head);
+        } else {   // else we continue with making data as 0 and add 1(carry) to next node
+            temp->data = 0;
         }
             
-        if(temp->next == NULL) break;   // if, end of list, we break from loop  
-        else temp = temp->next;   // else we move to next node
+        if(temp->next != NULL) {   // if we didn't reached last node of LL, keep moving forward
+            temp = temp->next;
+        } else {
+            break;   // If last node of LL is reached, break from the loop
+        }
     }
 
-    // If we reach here, it means input was something like 999, that's why list ended with 1 carry.
-    // In this case, we insert new insert a new Node with 1, and attach it to end of list.
-    // Then return the reversed list.
+    // Reaching here implies that the last node's data is still not less than 10, and the LL has
+    // ended. This means there is a carry of 1 left to be inserted after the last node. This only
+    // happens when input is all 9. For example: 999, 9999, etc. Thus, we insert a new node
+    // with data as 1 at the end of the LL and return the reversed list.
     temp->next = new Node(1);  // adding new node for the carried 1
     return reverseLL(head);
 }
