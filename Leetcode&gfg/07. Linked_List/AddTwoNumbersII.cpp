dcdc6774bc15c739e0c9a06d8e3bdf7d504b8026
Bishnu Mishra <mishravishnu369@gmail.com>
2024-01-08 21:35:45 +0530

    LinkedList

--- Leetcode&gfg/07. Linked_List/AddTwoNumbersII.cpp
+++ Leetcode&gfg/07. Linked_List/AddTwoNumbersII.cpp
@@ -16,6 +16,7 @@
 // Output: [0]
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // Definition for singly-linked list.
@@ -29,10 +30,49 @@ struct ListNode {
 
 class Solution {
 public:
-    // T.C:
-    // S.C: 
+    ListNode* reverseLL(ListNode* head) {
+        ListNode* prevNode = NULL;
+        ListNode* currNode = head;
+
+        while(currNode != NULL) {
+            ListNode* nextNode = currNode->next;
+            currNode->next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+
+        return prevNode;
+    }
+
+    // Note that this question can be done without reversing the lists by using stacks
+    // T.C: O(max(m,n));   where m & n are lengths of l1 & l2 respectively.
+    // S.C: O(max(m,n))
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        l1 = reverseLL(l1);
+        l2 = reverseLL(l2);
+
+        // same logic as in Leetcode-2(AddTwoNumbers)
+        ListNode* ans = new ListNode(0);
+        ListNode* p1 = l1, *p2 = l2, *temp = ans;
+
+        int carry = 0;
+        while(p1 != NULL || p2 != NULL) {
+            int x1 = (p1 != NULL) ? p1->val : 0;
+            int x2 = (p2 != NULL) ? p2->val : 0;
+            int sum = x1 + x2 + carry;
+            carry = sum/10;
+            temp->next = new ListNode(sum%10);
+            temp = temp->next;
+
+            if(p1 != NULL) p1 = p1->next;
+            if(p2 != NULL) p2 = p2->next;
+        }
+
+        if(carry>0) {
+            temp->next = new ListNode(carry);
+        }
+
+        return reverseLL(ans->next);
     }
 };
 
