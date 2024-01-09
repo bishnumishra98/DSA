f9454948d828299343a27de50da7207ef5de42ec
Bishnu Mishra <mishravishnu369@gmail.com>
2024-01-09 22:08:19 +0530

    LinkedList

---
+++ BaseBuildingPrograms/14. Stack/S3_ReverseAStringUsingStack.cpp
@@ -0,0 +1,24 @@
+// If we push anything inside stack, it comes out in LIFO(Last In First Out) fashion.
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    string str = "Golu";
+    stack <char> st;
+
+    for(int i=0; i<str.length(); i++) {
+        char ch = str[i];
+        st.push(ch);
+    }
+
+    while(!st.empty()) {
+        cout << st.top();
+        st.pop();
+    }
+
+    cout << endl;
+
+    return 0;
+}
