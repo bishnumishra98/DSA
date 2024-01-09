78c8a2de61839a0cacc049e9361f1a0ef2deb174
Bishnu Mishra <mishravishnu369@gmail.com>
2024-01-09 12:47:44 +0530

    LinkedList

--- BaseBuildingPrograms/13. Linked_List/LL15_AddOneToLL.cpp
+++ BaseBuildingPrograms/13. Linked_List/LL15_AddOneToLL.cpp
@@ -58,33 +58,63 @@ Node* reverseLL(Node* head) {
     return prevNode;
 }
 
+// Approach 1
+// T.C: O(n)
+// S.C: O(1)
+// Node* addOneToLL(Node* head) {
+//     head = reverseLL(head);   // reversing list to make addition easy       
+//     Node* temp = head;
+    
+//     while(1) {
+//         temp->data += 1;   // adding 1 to temp node
+        
+//         if(temp->data < 10) {    // if 'data < 10', no need of any carry. We can return reversed list.
+//             return reverseLL(head);
+//         } else {   // else we continue with making data as 0 and add 1(carry) to next node
+//             temp->data = 0;
+//         }
+            
+//         if(temp->next != NULL) {   // if we didn't reached last node of LL, keep moving forward
+//             temp = temp->next;
+//         } else {
+//             break;   // If last node of LL is reached, break from the loop
+//         }
+//     }
+
+//     // Reaching here implies that the last node's data is still not less than 10, and the LL has
+//     // ended. This means there is a carry of 1 left to be inserted after the last node. This only
+//     // happens when input is all 9. For example: 999, 9999, etc. Thus, we insert a new node
+//     // with data as 1 at the end of the LL and return the reversed list.
+//     temp->next = new Node(1);  // adding new node for the carried 1
+//     return reverseLL(head);
+// }
+
+// Approach 2
 // T.C: O(n)
 // S.C: O(1)
 Node* addOneToLL(Node* head) {
-    head = reverseLL(head);   // reversing list to make addition easy        
-    Node* temp = head;
-    
-    while(1) {
-        temp->data += 1;   // adding 1 to temp node
-        
-        if(temp->data < 10) {    // if 'data < 10', no need of any carry. We can return reversed list.
-            return reverseLL(head);
-        } else {   // else we continue with making data as 0 and add 1(carry) to next node
-            temp->data = 0;
-        }
-            
-        if(temp->next != NULL) {   // if we didn't reached last node of LL, keep moving forward
-            temp = temp->next;
-        } else {
-            break;   // If last node of LL is reached, break from the loop
-        }
+    head = reverseLL(head);   // reversing list to make addition easy 
+    Node* p = head;
+    Node* temp = NULL;   // temp will denote tail at end of while loop
+
+    int carry = 1;   // as 1 is to be added to LL, we assume 1 is the carry in the beginning
+    while(p != NULL) {
+        int x = (p != NULL) ? p->data : 0;
+        int sum = x + carry;
+        carry = sum / 10;
+        p->data = sum % 10;
+        temp = p;
+        p = p->next;   // when p reaches NULL, temp will reach tail of LL
     }
 
-    // Reaching here implies that the last node's data is still not less than 10, and the LL has
-    // ended. This means there is a carry of 1 left to be inserted after the last node. This only
+    // If carry is still greater than 0(obviously it will be 1), and the LL has ended; it means
+    // there is still a carry of 1 left to be inserted after the last node. This only
     // happens when input is all 9. For example: 999, 9999, etc. Thus, we insert a new node
     // with data as 1 at the end of the LL and return the reversed list.
-    temp->next = new Node(1);  // adding new node for the carried 1
+    if(carry > 0) {
+        temp->next = new Node(1);
+    }
+
     return reverseLL(head);
 }
 
---
+++ BaseBuildingPrograms/14. Stack/S1_IntroductionToStack.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    // creation of int and string type stack
+    stack <int> st;
+    stack <string> st2;
+
+    // insertion
+    st.push(10);
+    st.push(20);
+
+    return 0;
+}
