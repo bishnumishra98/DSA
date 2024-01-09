7781a71126e81aa9c168b301c013f8b52efa632a
Bishnu Mishra <mishravishnu369@gmail.com>
2024-01-09 15:55:03 +0530

    LinkedList

--- BaseBuildingPrograms/14. Stack/S1_IntroductionToStack.cpp
+++ BaseBuildingPrograms/14. Stack/S1_IntroductionToStack.cpp
@@ -3,13 +3,29 @@
 using namespace std;
 
 int main() {
+    // Note: We are using STL implementation of stack
     // creation of int and string type stack
     stack <int> st;
     stack <string> st2;
 
-    // insertion
+    // Basic stack operations: push(), pop(), size(), top(), empty()
+    cout << "Stack empty status: " << st.empty() << endl;   // 1
+    cout << "Pushed 10, 20 in stack" << endl;
     st.push(10);
     st.push(20);
+    cout << "Size: " << st.size() << endl;   // 2
+    cout << "Top element: " << st.top() << endl;   // 20
+    cout << "Stack empty status: " << st.empty() << endl;   // 0
+    cout << "Popping 1 element from stack" << endl;
+    st.pop();
+    cout << "Size: " << st.size() << endl;   // 1
+    cout << "Top element: " << st.top() << endl;   // 10
+    cout << "Stack empty status: " << st.empty() << endl;   // 0
+
+    cout << "Popping 1 more element from stack" << endl;
+    st.pop();
+    cout << "Size: " << st.size() << endl;   // 0
+    cout << "Stack empty status: " << st.empty() << endl;   // 1
 
     return 0;
 }
