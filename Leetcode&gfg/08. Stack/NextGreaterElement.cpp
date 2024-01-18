60c9523d9a08d89cab81c5cf378d70b7ac4d181b
Bishnu Mishra <mishravishnu369@gmail.com>
2024-01-18 09:03:23 +0530

    Stack

---
+++ Leetcode&gfg/08. Stack/NextGreaterElement.cpp
@@ -0,0 +1,40 @@
+// This problem is exactly similar to 'BaseBuildingPrograms\14. Stack\S8_NextSmallerElement.cpp',
+// the only difference is that we have to next greater element here.
+
+// gfg: Next Greater Element   --->   Given an array arr[ ] of size N having elements, the task is
+// to find the next greater element for each element of the array in order of their appearance in
+// the array. Next greater element of an element in the array is the nearest element on the right
+// which is greater than the current element. If there does not exist next greater of current
+// element, then next greater element for current element is -1. For example, next greater of the
+// last element is always -1.
+
+// Example 1:
+// Input: 
+// N = 4, arr[] = [1 3 2 4]
+// Output:
+// 3 4 4 -1
+
+// Example 2:
+// Input: 
+// N = 5, arr[] [6 8 0 1 3]
+// Output:
+// 8 -1 1 3 -1
+
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+    }
+};
+
+int main() {
+
+    return 0;
+}
