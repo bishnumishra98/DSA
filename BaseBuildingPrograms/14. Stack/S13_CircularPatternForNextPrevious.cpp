f315c3113ac7c822efffc8cb535cd4750e2259a4
Bishnu Mishra <mishravishnu369@gmail.com>
2024-01-19 02:16:01 +0530

    Stack

--- BaseBuildingPrograms/14. Stack/S13_CircularPatternForNextPrevious.cpp
+++ BaseBuildingPrograms/14. Stack/S13_CircularPatternForNextPrevious.cpp
@@ -10,7 +10,17 @@
 
 // Algorithm:
 // Same as for regular next or previous problems. The only difference is that now we have to
-// duplicate the original array and double its size, then apply same logic.
+// duplicate the original array and double its size, then apply same logic. By duplicating the
+// original array, we get a set of array for the last element of the given array so that the last
+// element may get its next greater element anywhere in the duplicate set of elements.
+// Don't initialize a new array of same size, it would increase space complexity. Instead think of
+// it as an imaginary array, and let index of the array range from 0 to 2n-1; and replace all
+// 'i' with 'i%n'. For example, for given array of size 5:
+// Array:           6  8  0  1  3
+// Indexes(i):      0  1  2  3  4
+
+// ImaginaryArray:  6  8  0  1  3  6  8  0  1  3
+// Indexes(i%n):    0  1  2  3  4  0  1  2  3  4
 
 #include <iostream>
 #include <vector>
@@ -19,14 +29,14 @@ using namespace std;
 
 // T.C: O(n)
 // S.C: O(n)
-vector<int> nextLargerElement_circularArray(int* arr, int size) {
-    vector <int> ans(size);
+vector<int> nextGreaterElement_circularArray(int* arr, int n) {
+    vector <int> ans(n);
     stack <int> st;
     st.push(-1);
 
     // traversing array from right to left
-    for(int i=size-1; i>=0; i--) {
-        int currElement = arr[i];
+    for(int i=2*n-1; i>=0; i--) {    // CHANGE 1
+        int currElement = arr[i%n];   // CHANGE 2
 
         // popping out stack elements until array element becomes smaller than stack top element.
         while(!st.empty() && currElement >= st.top()) {
@@ -34,10 +44,12 @@ vector<int> nextLargerElement_circularArray(int* arr, int size) {
         }
 
         // storing top element of stack in ans
-        if(!st.empty())
-            ans[i] = st.top();
-        else
-            ans[i] = -1;
+        if(i<n) {   // ADDITION 1
+            if(!st.empty())
+                ans[i] = st.top();
+            else
+                ans[i] = -1;
+        }
 
         // pushing array element into stack
         st.push(currElement);
@@ -47,10 +59,10 @@ vector<int> nextLargerElement_circularArray(int* arr, int size) {
 }
 
 int main() {
-    int arr[4] = {1, 3, 2, 4};
-    int size = 4;
+    int arr[5] = {6, 8, 0, 1, 3};
+    int size = 5;
 
-    vector <int> ans = nextLargerElement_circularArray(arr, size);
+    vector <int> ans = nextGreaterElement_circularArray(arr, size);
 
     for(auto i: ans) {
         cout << i << " ";
