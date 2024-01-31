19582d0407f5352ce07a203a9ae88ea77cfb7923
Bishnu Mishra <mishravishnu369@gmail.com>
2024-01-31 16:11:21 +0530

    Queue

--- BaseBuildingPrograms/13. Linked_List/LL01_Introduction.cpp
+++ BaseBuildingPrograms/13. Linked_List/LL01_Introduction.cpp
@@ -1,17 +1,18 @@
 // A linked list is a linear data structure in which elements are stored in nodes, and each node
-// points to the next node in the sequence. Unlike arrays, linked lists do not require contiguous
-// memory locations. Instead, each node contains data and a reference (or link) to the next node in
-// the sequence. Some properties of linked list are:
+// points to the next node in the sequence. Unlike arrays, linked lists do not require
+// contiguous memory locations. Instead, each node contains data and a reference (or link) to
+// the next node in the sequence. Some properties of linked list are:
 // 1) It is non contiguous, unlike arrays or vectors which are contiguous
 // 2) Less wastage of memory than vectors
-// 3) Insertion of element or shifting elements take constant time O(1), unlike arrays or vectors
-//    which takes O(n) to execute these operations.
+// 3) Insertion of element or shifting elements take constant time O(1), unlike arrays or
+//    vectors which takes O(n) to execute these operations.
 // 4) It can be grown or shrink dynamically at runtime, like vectors.
 // 5) Linked lists do not have indices, they have concept of address.
 
 // The basic building block of a linked list is a node, and a node typically has two fields:
 // -> Data Field: Contains the actual data or value of the node.
-// -> Next Field (or Link): Points to the next node, i.e. stores address of next node in the sequence.
+// -> Next Field (or Link): Points to the next node, i.e. stores address of next node in the
+//    sequence.
 
 // Types of linked list:
 // 1) Singly linked list
--- BaseBuildingPrograms/15. Queue/Q13_FirstNonRepeatingCharacterInAStream.cpp
+++ BaseBuildingPrograms/15. Queue/Q13_FirstNonRepeatingCharacterInAStream.cpp
@@ -1,10 +1,60 @@
 // There is a string containing a stream. Return first non-repeating character in the stream.
 // If no non-repeating character is found in the stream, return #.
 
+// Example 1:
+// Input:
+// ababc
+// Output:
+// aab#c
+// Explanation:
+// For 'a', first non-repeating character is 'a'.
+// For 'ab', first non-repeating character is 'a'.
+// For 'aba', first non-repeating character is 'b'.
+// For 'abab', first non-repeating character is '#'.
+// For 'ababc', first non-repeating character is 'c'.
+
 #include <iostream>
+#include <queue>
 using namespace std;
 
+string firstNonRepeatingCharacter(string str) {
+    int n = str.length();
+    string ans(n, ' ');   // initialized a string of length 'n' with spaces
+    queue <char> q;
+    int freq[26] = {0};   // as there are only 26 alphabets in English
+    // int freq[n] = {x}; means initializing 1st element of array with 'x', and rest with '0'.
+
+    for(int i=0; i<n; i++) {
+        char ch = str[i];
+        freq[ch - 'a']++;
+        q.push(ch);
+
+        // Process the queue to find the first non-repeating character
+        while(!q.empty()) {
+            char frontChar = q.front();
+            if (freq[frontChar - 'a'] > 1) {
+                // If frontChar occurred more than once, remove it from the queue
+                q.pop();
+            } else {
+                // If frontChar occurred only once, i.e., 'freq[frontChar - 'a'] == 1',
+                // update the answer and break the loop
+                ans[i] = frontChar;
+                break;
+            }
+        }
+
+        // If the queue is empty, set the answer character to '#'
+        if(q.empty()) {
+            ans[i] = '#';
+        }
+    }
+
+    return ans;
+}
+
 int main() {
+    string str = "ababc";
+    cout << firstNonRepeatingCharacter(str);
 
     return 0;
 }
