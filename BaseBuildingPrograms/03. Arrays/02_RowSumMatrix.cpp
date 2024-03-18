731ecb4d212a37322906b5610d87f1e30adbc5b5
Bishnu Mishra <mishravishnu369@gmail.com>
2024-03-18 13:42:27 +0530

    updated some comments in Arraybasebuilding folder

--- BaseBuildingPrograms/03. Arrays/02_RowSumMatrix.cpp
+++ BaseBuildingPrograms/03. Arrays/02_RowSumMatrix.cpp
@@ -4,22 +4,23 @@
 using namespace std;
 
 int main() {
-     int arr[][4] = {   
+    int col = 4;
+
+    int arr[][col] = {
         {1, 2, 3, 4},
         {5, 6, 7, 8},
         {9, 10, 11, 12}
     };
 
-    int row = 3;
-    int col = 4;
+    int row = sizeof(arr) / sizeof(arr[0]);
 
-    for(int i=0; i<col; i++) {
+    for(int i=0; i<row; i++) {
         int sum = 0;
-        for(int j=0; j<row; j++) {
-            sum += arr[j][i];
+        for(int j=0; j<col; j++) {
+            sum += arr[i][j];
         }
-        cout << sum;
-        cout << endl;
+        cout << sum << endl;
     }
+
     return 0;
 }
--- BaseBuildingPrograms/03. Arrays/04_TwoDimensionalVector.cpp
+++ BaseBuildingPrograms/03. Arrays/04_TwoDimensionalVector.cpp
@@ -12,15 +12,14 @@ int main() {
     cout << endl;
 
     // Inserting values into the 2D vector
-    for (int i = 0; i < row; i++) {
-
-        cout << "Enter the number of elements in " << i << "th row: ";
+    for(int i = 0; i < row; i++) {
+        cout << "Enter the number of elements in " << i << " indexed row: ";
         int col;
         cin >> col;
-        vector<int> v;
+        vector <int> v;
 
         cout << "Enter the elements one by one:\n";
-        for (int j = 0; j<col; j++) {
+        for(int j = 0; j<col; j++) {
             int element;
             cin >> element;
             v.push_back(element);
@@ -31,8 +30,8 @@ int main() {
     }
 
     // Printing the vector
-    for (int i = 0; i<matrix.size(); i++) {
-        for (int j = 0; j<matrix[i].size(); j++) {
+    for(int i = 0; i<matrix.size(); i++) {   // matrix.size() returns the number of rows in the 2D vector matrix
+        for (int j = 0; j<matrix[i].size(); j++) {   // matrix[i].size() returns the number of elements in the i-th row of the 2D vector 
             cout << matrix[i][j] << " ";
         }
         cout << endl;
