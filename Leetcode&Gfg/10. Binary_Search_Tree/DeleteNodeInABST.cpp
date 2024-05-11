436b7fd2ba651be727f8049ddddd7225b3474062
Bishnu Mishra <mishravishnu369@gmail.com>
2024-05-12 01:01:29 +0530

    BST

--- Leetcode&Gfg/10. Binary_Search_Tree/DeleteNodeInABST.cpp
+++ Leetcode&Gfg/10. Binary_Search_Tree/DeleteNodeInABST.cpp
@@ -56,10 +56,49 @@ void levelOrderTraversal_LevelByLevel(TreeNode* root) {
 
 class Solution {
 public:
-    // T.C:
-    // S.C: 
+    TreeNode* maxNodeInBST(TreeNode* root) {
+        if(!root) return NULL;
+        TreeNode* temp = root;
+        while(temp->right) {
+            temp = temp->right;
+        }
+        return temp;
+    }
+
+    // T.C: O(H); H = logN in average case, while H = N in worst case
+    // S.C: O(N)
     TreeNode* deleteNode(TreeNode* root, int key) {
-        
+        if(!root) return NULL;   // If root is NULL, there's nothing to delete. So return NULL.
+
+        if(key == root->val) {   // if key found, there can be any 1 out of 4 scenarios to delete it
+            if(!root->left && !root->right) {   // case 1: The node to delete is a leaf node
+                delete root;
+                return NULL;
+            } else if(root->left && !root->right) {   // case 2: The node to delete has a left child node, but no right child node
+                TreeNode* leftSubTree = root->left;
+                delete root;
+                return leftSubTree;
+            } else if(root->right && !root->left) {   // case 3: The node to delete has a right child node, but no left child node
+                TreeNode* rightSubTree = root->right;
+                delete root;
+                return rightSubTree;
+            } else {   // case 4: The node to delete has both left and right child nodes
+                // Step 1: Bring max node of left subtree in variable say 'maxi'
+                TreeNode* maxi = maxNodeInBST(root->left);
+                // Step 2: Replace 'root->val' with the 'maxi->val'
+                root->val = maxi->val;
+                // Step 3: Delete 'maxi' node, and return root
+                root->left = deleteNode(root->left, maxi->val);
+                return root;
+            }
+        } else if(key < root->val) {   // if key not found, and 'key < root->val', try to search and delete the key in left subtree
+            root->left = deleteNode(root->left, key);
+        } else {   //  if still key is not found, key must be greater than 'root->val', thus search and delete the key in right subtree
+            root->right = deleteNode(root->right, key);
+        }
+
+        // After handling the deletion scenarios, we need to return the modified subtree(with the deleted node removed)
+        return root;
     }
 };
 
