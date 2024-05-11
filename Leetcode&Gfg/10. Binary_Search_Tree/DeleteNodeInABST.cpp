1534301fa9fd17222095ce24739a43447ecf0ebe
Bishnu Mishra <mishravishnu369@gmail.com>
2024-05-11 22:01:51 +0530

    BST

--- Leetcode&Gfg/10. Binary_Search_Tree/DeleteNodeInABST.cpp
+++ Leetcode&Gfg/10. Binary_Search_Tree/DeleteNodeInABST.cpp
@@ -5,6 +5,11 @@
 // 2. If the node is found, delete the node.
 
 // Example 1:
+//      5                        5
+//     / \                      / \
+//    3   6        --->        4   6  
+//   / \   \                  /     \
+//  2   4   7                2       7
 // Input: root = [5,3,6,2,4,null,7], key = 3
 // Output: [5,4,6,2,null,null,7]
 // Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
@@ -27,9 +32,50 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+void levelOrderTraversal_LevelByLevel(TreeNode* root) {
+    queue <TreeNode*> q;
+    q.push(root);
+    q.push(NULL);   // NULL acts as an indicator to change line
+
+    while(!q.empty()) {
+        TreeNode* front = q.front();
+        q.pop();
+
+        if(front == NULL) {
+            cout << endl;
+            if(!q.empty()) q.push(NULL);
+        } else {
+            if(front->left) q.push(front->left);
+            if(front->right) q.push(front->right);
+            cout << front->val << " ";
+        }
+    }
+}
+
+// ---------------------------------------------------------------------------------------------------
+
 class Solution {
 public:
+    // T.C:
+    // S.C: 
     TreeNode* deleteNode(TreeNode* root, int key) {
         
     }
 };
+
+int main() {
+    TreeNode* root = new TreeNode(5);
+    root->left = new TreeNode(3);
+    root->right = new TreeNode(6);
+    root->left->left = new TreeNode(2);
+    root->left->right = new TreeNode(4);
+    root->right->right = new TreeNode(7);
+
+    Solution sol;
+    int key = 3;
+    TreeNode* ans = sol.deleteNode(root, key);
+
+    levelOrderTraversal_LevelByLevel(root);
+
+    return 0;
+}
