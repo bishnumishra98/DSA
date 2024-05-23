0aaae55fda0a1b55d21196c2e4abba5f38d1d546
Bishnu Mishra <mishravishnu369@gmail.com>
2024-05-23 15:22:31 +0530

    heap

--- BaseBuildingPrograms/18. Heaps/H05_BuildMinHeapUsingRecursion.cpp
+++ BaseBuildingPrograms/18. Heaps/H05_BuildMinHeapUsingRecursion.cpp
@@ -1,3 +1,5 @@
+// Exactly same as 'H04_BuildMaxHeapUsingRecursion.cpp', just there will be a change in if condition of heapify().
+
 #include <iostream>
 using namespace std;
 
@@ -13,25 +15,25 @@ void printHeap(int *arr, int size) {
 void heapify(int *arr, int n, int index) {
     int leftIndex = (2 * index) + 1; // Left child index for 0-based index
     int rightIndex = (2 * index) + 2; // Right child index for 0-based index
-    int largestIndex = index;
+    int smallestIndex = index;
 
-    if(leftIndex < n && arr[leftIndex] > arr[largestIndex])
-        largestIndex = leftIndex;
+    if(leftIndex < n && arr[leftIndex] < arr[smallestIndex])
+        smallestIndex = leftIndex;
 
-    if(rightIndex < n && arr[rightIndex] > arr[largestIndex])
-        largestIndex = rightIndex;
+    if(rightIndex < n && arr[rightIndex] < arr[smallestIndex])
+        smallestIndex = rightIndex;
 
-    if(largestIndex != index) {
-        swap(arr[index], arr[largestIndex]);
+    if(smallestIndex != index) {
+        swap(arr[index], arr[smallestIndex]);
 
         // Recursively heapify the affected sub-tree
-        heapify(arr, n, largestIndex);
+        heapify(arr, n, smallestIndex);
     }
 }
 
 // T.C: O(N)
 // S.C: O(h);   where h = logN
-void buildMaxHeap(int *arr, int n) {
+void buildMinHeap(int *arr, int n) {
     // Start from the last internal node all the way up to the root node
     for (int i = n/2 - 1; i >= 0; i--) {
         heapify(arr, n, i);
@@ -39,18 +41,18 @@ void buildMaxHeap(int *arr, int n) {
 }
 
 int main() {
-    // Lets create a max heap for {10, 20, 30, 40, 50, 60}
+    // Lets create a min heap for {60, 50, 40, 30, 20, 10}
     // The heap for above array will look like this:
-    //      _ 60 _ 
+    //      _ 10 _ 
     //     /      \
-    //    50      30
+    //    20      30
     //   /  \     /
-    //  40  20   10
+    //  40  50   60
 
-    int arr[] = {10, 20, 30, 40, 50, 60}; 
+    int arr[] = {60, 50, 40, 30, 20, 10}; 
     int n = 6;    // Size of the heap (number of elements in the heap)
     printHeap(arr, n);   // Before heapification
-    buildMaxHeap(arr, n);   // Build the max heap
+    buildMinHeap(arr, n);   // Build the min heap
     printHeap(arr, n);   // After heapification
 
     return 0;
