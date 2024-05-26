2ef759e3deec7c77f5526815a69e6ba6af4c95cb
Bishnu Mishra <mishravishnu369@gmail.com>
2024-05-26 21:35:07 +0530

    heap

--- Leetcode&Gfg/11. Heaps/SmallestRangeCoveringElementsFromKLists.cpp
+++ Leetcode&Gfg/11. Heaps/SmallestRangeCoveringElementsFromKLists.cpp
@@ -21,11 +21,77 @@ public:
     // T.C:
     // S.C: 
     vector<int> smallestRange(vector<vector<int>>& nums) {
+        // Min-heap to store elements as (value, list index, element index)
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minHeap;
+        // When we specify 'greater<vector<int>>', the priority_queue uses the 'operator>' to order the elements.
+        // For a vector, this means it will compare the elements lexicographically, which works if the first element
+        // of the vector is what you want to prioritize (the smallest element in our case). Thus, creating a custom
+        // comparator is not mandatory here. However, if you want, you can create a custom comparator like this:
+        /*
+        struct Compare {
+            bool operator()(const vector<int>& a, const vector<int>& b) {
+                return a[0] > b[0];
+            }
+        };
+        */
+
+        // Track the maximum element in the current range
+        int maxElement = INT_MIN;
         
+        // Initialize the heap with the first element of each list, also store the maximum element of them.
+        for(int i = 0; i < nums.size(); ++i) {
+            if(!nums[i].empty()) {   // Ensure the vector is not empty
+                minHeap.push({nums[i][0], i, 0});   // Push (value, list index, element index)
+                maxElement = max(maxElement, nums[i][0]);
+            }
+        }
+
+        // Initialize the result range
+        int start = minHeap.top()[0];
+        int end = maxElement;
+        
+        // Process the elements in the heap
+        while(true) {
+            // Get the smallest element from the heap
+            vector<int> current = minHeap.top();
+            // auto current = minHeap.top();   // we can even use 'auto' data-type here
+            minHeap.pop();
+            int minValue = current[0];
+            int listIndex = current[1];
+            int elementIndex = current[2];
+
+            // Update the range if the current range is smaller
+            if(maxElement - minValue < end - start) {
+                start = minValue;
+                end = maxElement;
+            }
+
+            // If the current list is exhausted, break the loop
+            if(elementIndex + 1 == nums[listIndex].size()) break;
+
+            // Insert the next element of the current list into the heap
+            int nextValue = nums[listIndex][elementIndex + 1];
+            minHeap.push({nextValue, listIndex, elementIndex + 1});
+            maxElement = max(maxElement, nextValue);
+        }
+
+        return {start, end};
     }
 };
 
 int main() {
+    vector<vector<int>> v = {
+        {4,10,15,24,26},
+        {0,9,12,20},
+        {5,18,22,30}
+    };
+
+    Solution sol;
+    vector<int> ans = sol.smallestRange(v);
+
+    for(auto i: ans) {
+        cout << i << " ";
+    }
 
     return 0;
 }
