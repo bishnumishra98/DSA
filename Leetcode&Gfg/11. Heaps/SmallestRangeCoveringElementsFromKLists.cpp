2db00a0ba6d9826b34ffc56ac0f0f41dd3bdd076
Bishnu Mishra <mishravishnu369@gmail.com>
2024-05-26 19:08:20 +0530

    heap

---
+++ Leetcode&Gfg/11. Heaps/SmallestRangeCoveringElementsFromKLists.cpp
@@ -0,0 +1,31 @@
+// Leetcode: 632. Smallest Range Covering Elements from K Lists   --->   You have k lists of sorted integers in
+// non-decreasing order. Find the smallest range that includes at least one number from each of the k lists.
+
+// Example 1:
+// Input: nums = [[4,10,15,24,26],[0,9,12,20],[5,18,22,30]]
+// Output: [20,24]
+// Explanation: 
+// List 1: [4, 10, 15, 24,26], 24 is in range [20,24].
+// List 2: [0, 9, 12, 20], 20 is in range [20,24].
+// List 3: [5, 18, 22, 30], 22 is in range [20,24].
+
+// Example 2:
+// Input: nums = [[1,2,3],[1,2,3],[1,2,3]]
+// Output: [1,1]
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // T.C:
+    // S.C: 
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        
+    }
+};
+
+int main() {
+
+    return 0;
+}
