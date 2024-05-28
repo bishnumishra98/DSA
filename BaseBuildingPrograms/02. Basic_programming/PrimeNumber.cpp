4e8df5e4a1b5be5d09a51e7aaddfbff4b99861ad
Bishnu Mishra <mishravishnu369@gmail.com>
2024-05-28 15:25:27 +0530

    nothing much

--- BaseBuildingPrograms/02. Basic_programming/PrimeNumber.cpp
+++ BaseBuildingPrograms/02. Basic_programming/PrimeNumber.cpp
@@ -5,28 +5,22 @@ using namespace std;
 // T.C: O(sqrt(n))
 // S.C: O(1)
 bool checkPrime(int n) {
-    if(n<=1) {
-        return false;
-    }
+    if(n<=1) return false;   // numbers less than equal 1 are non-prime
 
-    // a non-prime number will always have a factor >=2 and <= sqrt(n)
+    // a non-prime number will always have a factor between 2 and sqrt(n)
     for(int i=2; i<=sqrt(n); i++) {
-        if(n%i==0) {
-            return false;   // found a factor
-        }
+        if(n%i==0) return false;   // found a factor, thus return false
     }
     
+    // If program comes here, it means no factors are found. Thus, it is a prime number. Hence, return true.
     return true;
 }
 
 int main() {
     int n = 31;
 
-    if(checkPrime(n)) {
-        cout << "Prime number";
-    } else {
-        cout << "Composite number";
-    }
-
+    if(checkPrime(n)) cout << "Prime number";
+    else cout << "Composite number";
+    
     return 0;
 }
