229983ec37c36d79a3a79e72e291c70587c0cb03
Bishnu Mishra <mishravishnu369@gmail.com>
2024-05-28 17:28:36 +0530

    nothing much

--- BaseBuildingPrograms/02. Basic_programming/PalindromeString.cpp
+++ BaseBuildingPrograms/02. Basic_programming/PalindromeString.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
+// We can use two pointer approach to find a palindrome string
 // T.C: O(n)
 // S.C: O(1)
 bool checkPalindromeString(string str) {
-    int i = 0;
-    int j = str.length() - 1;
+    int i = 0;   // i points to first index of 'str'
+    int j = str.length() - 1;   // j points to last index of 'str'
 
-    // palindrome string logic
-    while(i<j) {
+    // Increment i by 1, and decrement j by 1 on each iteration,
+    // until i crosses j. On each iteration, check if str[i] and str[j]
+    // are equal or not. If they are not equal, then it is not a
+    // palindrome string. Thus, return false.
+    while(i < j) {
         if(str[i] == str[j]) {
             i++;
             j--;
@@ -17,17 +21,15 @@ bool checkPalindromeString(string str) {
         }
         
     }
+
+    // If program comes out of while loop, it is a palindrome.
+    // Thus, return true.
     return true;
 }
 
 int main() {
     string str = "obkbo";
-
-    if(checkPalindromeString(str)) {
-        cout << "Palindrome";
-    } else {
-        cout << "Not a palindrome";
-    }
-
+    if(checkPalindromeString(str)) cout << "Palindrome";
+    else cout << "Not a palindrome";
     return 0;
 }
