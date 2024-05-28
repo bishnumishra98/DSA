756904711a1890bb23149debbfc0c1e8562a0ab5
Bishnu Mishra <mishravishnu369@gmail.com>
2024-05-28 16:26:26 +0530

    n

--- BaseBuildingPrograms/02. Basic_programming/FibonacciSeries.cpp
+++ BaseBuildingPrograms/02. Basic_programming/FibonacciSeries.cpp
@@ -9,20 +9,22 @@ using namespace std;
 // T.C: O(n)
 // S.C: O(1)
 int fibo_ite(int n) {
-    int n0 = 0;
-    int n1 = 1;
+    int n1 = 0;   // Initializing first element of fibonacci series
+    int n2 = 1;   // Initializing second term of fibonacci series
 
-    if(n == 0) return n0;
-    if(n == 1) return n1;
+    if(n <= 0) return 0;   // n <= 0 is invalid, so return 0
+    if(n == 1) return n1;   // 1st element of fibonacci series is n1
+    if(n == 2) return n2;   // 2nd element of fibonacci series is n2
 
-    int n2;
-    for(int i=1; i<n; i++) {
-        n2 = n1 + n0;
-        n0 = n1;
-        n1 = n2;
+    // Initializing 'n3' that will store sum of last two elements of the series
+    int n3;   
+    for(int i=3; i<=n; i++) {   // Iterating from the 3rd term to the nth term
+        n3 = n2 + n1;   // n3 is sum of n2 and n1
+        n1 = n2;   // Updating value of n1 with n2
+        n2 = n3;   // Updating value of n2 with n3
     }
 
-    return n2;
+    return n3;   // return the nth number of fibonacci series that is stored in n3
 }
 
 // Return nth number of fibonacci series
@@ -30,8 +32,9 @@ int fibo_ite(int n) {
 // T.C: O(2^n)
 // S.C: O(n)
 int fibo_rec(int n) {
-    if(n==0) return 0;
-    if(n==1) return 1;
+    if(n<=0) return 0;
+    if(n==1) return 0;
+    if(n==2) return 1;
     return fibo_rec(n-1) + fibo_rec(n-2);
 }
 
@@ -75,9 +78,9 @@ int main() {
     // Fibonacci Series: 0 1 1 2 3 5 8 13 21 34 ...
 
     // print the 7th number of fibonacci series
-    cout << fibo_ite(7);   // o/p: 13
+    cout << fibo_ite(7);   // o/p: 8
     cout << endl;
-    cout << fibo_rec(7);   // o/p: 13
+    cout << fibo_rec(7);   // o/p: 8
     cout << endl;
 
     // print the first 6 numbers of fibonacci series
