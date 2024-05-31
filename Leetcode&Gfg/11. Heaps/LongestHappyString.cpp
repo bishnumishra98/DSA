4a6e959484d9397cbf5c31bfc4dbed28b5684915
Bishnu Mishra <mishravishnu369@gmail.com>
2024-05-31 15:55:18 +0530

    heap

--- Leetcode&Gfg/11. Heaps/LongestHappyString.cpp
+++ Leetcode&Gfg/11. Heaps/LongestHappyString.cpp
@@ -5,7 +5,7 @@
 // s contains at most b occurrences of the letter 'b'.
 // s contains at most c occurrences of the letter 'c'.
 // Given three integers a, b, and c, return the longest possible happy string. If there are multiple longest happy strings,
-// return any of them. If there is no such string, return the empty string "".
+// return any of them.
 
 // Example 1:
 // Input: a = 1, b = 1, c = 7
@@ -17,7 +17,7 @@
 // Output: "aabaa"
 // Explanation: It is the only correct answer in this case.
 
-// Algorithm: Similar to 'Leetcode&Gfg\11. Heaps\ReorganizeString.cpp'.
+// Algorithm: Exactly similar to 'Leetcode&Gfg\11. Heaps\ReorganizeString.cpp'.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -27,7 +27,7 @@ class Info {
     public:
         char ch;
         int count;
-        // Default constructor not required as we will initialize object directly with arguments.
+        // Default constructor not required as objects can be initialzied directly with arguments.
         // Parameterized constructor
         Info(char ch, int count) {
             this->ch = ch;
@@ -48,8 +48,79 @@ public:
     // T.C:
     // S.C: 
     string longestDiverseString(int a, int b, int c) {
+        /*
+        // Making maxHeap using custom data-type
         priority_queue<Info, vector<Info>, Compare> maxHeap;
+        if(a > 0) {
+            Info temp('a', a);
+            maxHeap.push(temp);
+        }
+        if(b > 0) {
+            Info temp('b', b);
+            maxHeap.push(temp);
+        }
+        if(c > 0) {
+            Info temp('c', c);
+            maxHeap.push(temp);
+        }
+        */
 
+        // Making maxHeap using pair(int, char). Keeping first element of pair as 'int'
+        // gives an advantage of not making custom comparator to create the maxHeap.
+        priority_queue<pair<int, char>> maxHeap;
+        if(a > 0) maxHeap.push(make_pair(a, 'a'));
+        if(b > 0) maxHeap.push(make_pair(b, 'b'));
+        if(c > 0) maxHeap.push(make_pair(c, 'c'));
+
+        string ans = "";
+        while(maxHeap.size() > 1) {
+            auto firstElement = maxHeap.top();
+            maxHeap.pop();
+            auto secondElement = maxHeap.top();
+            maxHeap.pop();
+
+            // If frequency of firstElement is greater than equal to 2, it can be inserted atmost 2 times side by side
+            if(firstElement.first >= 2) {
+                ans.push_back(firstElement.second);   // push this character in string 'ans' 2 times
+                ans.push_back(firstElement.second);
+                firstElement.first -= 2;   // decrement frequency of this character by 2
+            } else {   // Frequency of firstElement is 1
+                ans.push_back(firstElement.second);   // push this character in string 'ans' only once
+                firstElement.first -= 1;   // decrement frequency of this character by 1
+            }
+
+            // If frequency of secondElement is greater than equal to 2, and greater than equal to
+            // frequency of firstElement, it can be inserted atmost 2 times side by side.
+            if(secondElement.first >= 2 && secondElement.first >= firstElement.first) {
+                ans.push_back(secondElement.second);   // push this character in string 'ans' 2 times
+                ans.push_back(secondElement.second);
+                secondElement.first -= 2;   // decrement frequency of this character by 2
+            } else {   // Frequency of secondElement is 1
+                ans.push_back(secondElement.second);   // push this character in string 'ans' only once
+                secondElement.first -= 1;   // decrement frequency of this character by 1
+            }
+
+            // If frequency of 'firstElement' is greater than 0, insert this element back to maxHeap
+            if(firstElement.first > 0) maxHeap.push(firstElement);
+            // If frequency of 'secondElement' is greater than 0, insert this element back to maxHeap
+            if(secondElement.first > 0) maxHeap.push(secondElement);
+        }
+
+        // It may be a possibility, that is still 1 element left in maxHeap
+        if(maxHeap.size() == 1) {
+            auto lastElement = maxHeap.top();   // 'pair<int, char>' can also be written instead of 'auto'
+            maxHeap.pop();
+
+            // If frequency of lastElement is greater than equal to 2, it can be inserted atmost 2 times side by side
+            if(lastElement.first >= 2) {
+                ans.push_back(lastElement.second);   // push this character in string 'ans' 2 times
+                ans.push_back(lastElement.second);
+            } else {   // Frequency of lastElement is 1
+                ans.push_back(lastElement.second);   // push this character in string 'ans' only once
+            }
+        }
+
+        return ans;   // longest possible string formed. Return it.
     }
 };
 
