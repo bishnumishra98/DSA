7295f14f038bccaac79e55f233c827b3c4f5852a
Bishnu Mishra <mishravishnu369@gmail.com>
2024-06-08 03:45:44 +0530

    dp

--- Leetcode&Gfg/13. Dynamic_Programming/CoinChange.cpp
+++ Leetcode&Gfg/13. Dynamic_Programming/CoinChange.cpp
@@ -103,9 +103,36 @@ int coinChange_memoization(vector<int>& coins, int amount) {
 // T.C: O(n*amount);    because each amount from 1 to amount is computed once, and for each amount, we consider all n coins.
 // S.C: O(amount)
 int coinChange_tabulation(vector<int>& coins, int amount) {
-    int ans = solve(coins, amount);
-    return ((ans == INT_MAX) ? -1 : ans);
+    // Step 1: Create a dp array of size 'amount + 1' with initial values 'INT_MAX'
+    vector<int> dp(amount + 1, INT_MAX);
+
+    // Step 2: Fill initial data in dp according to the base case.
+    // Base case: if the amount is 0, no coins are needed to make this amount
+    dp[0] = 0;
+
+    // Step 3: Fill the rest of the dp array using iteration.
+    // Building answers for coins required to build values from 1 to amount.
+    for (int val = 1; val <= amount; val++) {
+        // Loop through each coin in the list of coins
+        for (int i = 0; i < coins.size(); i++) {
+            // If val is greater than or equal to coins[i], we can consider this coin
+            if (val - coins[i] >= 0) {
+                // Solve for the amount minus the current coin
+                int ans = dp[val - coins[i]];
+
+                // If the ans is not INT_MAX, it means a valid solution was found
+                if (ans != INT_MAX) {
+                    // Update the dp value for the current amount
+                    dp[val] = min(dp[val], 1 + ans);
+                }
+            }
+        }
+    }
+
+    // If dp[amount] is still INT_MAX, it means we couldn't form the amount with given coins
+    return dp[amount] == INT_MAX ? -1 : dp[amount];
 }
+
 
 int main() {
     vector <int> coins = {1, 2, 5};
