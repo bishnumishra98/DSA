eb977315b8084991ac670d471a93de073a4e5b54
Bishnu Mishra <mishravishnu369@gmail.com>
2024-06-09 15:35:44 +0530

    n

--- BaseBuildingPrograms/09. Recursion/PaintingFence.cpp
+++ BaseBuildingPrograms/09. Recursion/PaintingFence.cpp
@@ -1,4 +1,4 @@
-// gfg articke: Given a fence with n posts and k colors, find out the number of ways of painting the fence such
+// gfg article: Given a fence with n posts and k colors, find out the number of ways of painting the fence such
 // that at most 2 adjacent posts have the same color.
 
 // Example 1:
