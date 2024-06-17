aa1f1cbd0da6d694eedaabd1987af4e7cf5b246e
Bishnu Mishra <mishravishnu369@gmail.com>
2024-06-18 02:34:05 +0530

    recursion

---
+++ BaseBuildingPrograms/09A. Basic Recursion/07_AllSubsequenceOfArray.cpp
@@ -0,0 +1,48 @@
+// Given an array, print all its subsequence. All subsequence must follow the ordering of elements as it is in parent array.
+// For example, {8, 6} cannot be a subsequence, but {6, 8} is a subsequence.
+// Example:
+// Input:
+// {6, 7, 8}
+// Output:
+// {6, 7, 8}, {6, 7}, {6, 8}, {6}, {7, 8}, {7}, {8}, {}
+
+// This type of problem follows TAKE/NOT TAKE pattern.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// T.C: O((2^n)*n)
+// S.C: O(n);   maximum depth of recursive tree is no.of elements in 'arr'.
+void printSubsequence(int index, int arr[], int n, vector<int>& ds) {
+    // Base case: if index >= n, print the subsequence formed inside 'ds' and return.
+    if(index >= n) {
+        for(int i: ds) {
+            cout << i << " ";
+        }
+        if(ds.size() == 0) cout << "{}";   // just to show no element. This is optional.
+        cout << endl;
+        return;
+    }
+
+    // Case 1: Take the current element of 'arr' and proceed to make the subsequence
+    ds.push_back(arr[index]);
+    printSubsequence(index+1, arr, n, ds);
+    ds.pop_back();   // ds.pop_back() is used to remove the last element that was added to the 'ds' vector in the previous
+    // step. This is necessary because, after all subsequences that include the current element (at arr[index]) have been
+    // printed, you need to backtrack and remove that element from 'ds' to explore subsequences that do not include this element.
+
+    // Case 2: Not take the current element of 'arr' and proceed to make the subsequence
+    printSubsequence(index+1, arr, n, ds);
+}
+
+int main() {
+    int arr[] = {6, 7, 8};
+    int start_index = 0;
+    int n = 3;
+    vector<int> ds;   // used as a helper data-structure to store subsequences
+
+    printSubsequence(start_index, arr, n, ds);
+
+    return 0;
+}
