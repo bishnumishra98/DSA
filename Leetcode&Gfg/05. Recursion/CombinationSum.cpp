77635052844ef89b156fc9df92439e20dfb832b6
Bishnu Mishra <mishravishnu369@gmail.com>
2024-06-21 02:01:31 +0530

    recursion

--- Leetcode&Gfg/05. Recursion/CombinationSum.cpp
+++ Leetcode&Gfg/05. Recursion/CombinationSum.cpp
@@ -1,3 +1,55 @@
 // This type of problem follows PICK/NOT PICK pattern, that we learnt in
 // 'BaseBuildingPrograms/09. Recursion_I/08_AllSubsequenceTargetSum.cpp'.
 // The only difference being we can pick an element multiple times to make the target.
+
+// Leetcode 39. Combination Sum    --->   Given an array of distinct integers candidates and a target integer target,
+// return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the
+// combinations in any order.
+// The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
+// frequency of at least one of the chosen numbers is different.
+// The test cases are generated such that the number of unique combinations that sum up to target is less than
+// 150 combinations for the given input.
+
+// Example 1:
+// Input: candidates = [2,3,6,7], target = 7
+// Output: [[2,2,3],[7]]
+// Explanation:
+// 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+// 7 is a candidate, and 7 = 7.
+// These are the only two combinations.
+
+// Example 2:
+// Input: candidates = [2,3,5], target = 8
+// Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+// Example 3:
+// Input: candidates = [2], target = 1
+// Output: []
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // T.C: 
+    // S.C: 
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+    }
+};
+
+int main() {
+    vector<int> candidates = {2, 3, 6, 7};
+    int target = 7;
+
+    Solution sol;
+    vector<vector<int>> v = sol.combinationSum(candidates, target);
+    for(int i=0; i<v.size(); i++) {
+        for(int j=0; j<v[i].size(); j++) {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
