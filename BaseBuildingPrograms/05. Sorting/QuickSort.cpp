1efc0510076e829ac9d67d582e6fd603f06f67b2
Bishnu Mishra <mishravishnu369@gmail.com>
2024-06-21 01:04:49 +0530

    quicksort

--- BaseBuildingPrograms/05. Sorting/QuickSort.cpp
+++ BaseBuildingPrograms/05. Sorting/QuickSort.cpp
@@ -1,8 +1,7 @@
-// Quick Sort is a popular sorting algorithm that uses a divide-and-conquer strategy to sort elements
-// in an array or list. It works by selecting a pivot element and partitioning the array into two
-// sub-arrays according to the pivot. Elements less than the pivot are placed to its left, and
-// elements greater than the pivot are placed to its right. This process is then applied recursively
-// to the sub-arrays, and finally the array gets sorted.
+// Quick sort is a method for organizing a list of things into order (from smallest to largest). Here's a simple way to understand it:
+// 1. Pick a Pivot: Choose one item from the list. This item is called the "pivot."
+// 2. Sort Around Pivot: Rearrange the list so that all items smaller than the pivot come before it, and all items larger come after it.
+// 3. Repeat: Now, take the smaller and larger sections and do the same thing (pick a pivot, sort around it) until the whole list is sorted.
 
 #include <iostream>
 using namespace std;
@@ -14,13 +13,13 @@ int partition(int arr[], int low, int high) {
     int j = high;
 
     while(i < j) {
-        while(arr[i] <= pivot && i <= high-1) i++;   // Move i to the right while elements are less than or equal to pivot and within bounds
-        while(arr[j] > pivot && j >= low+1) j--;   // Move j to the left while elements are greater than pivot and within bounds
+        while(pivot >= arr[i] && i <= high-1) i++;   // Move i to the right while elements are less than or equal to pivot and within bounds
+        while(pivot < arr[j] && j >= low+1) j--;   // Move j to the left while elements are greater than pivot and within bounds
         if(i < j) swap(arr[i], arr[j]);   // If i is still less than j, swap elements at i and j
     }
     
     swap(arr[low], arr[j]);   // Swap pivot element with the element at j to place the pivot in its correct position
-    return j;   // Return the partition index
+    return j;   // Return the partition index. Element at index j is at its correct position now.
 }
 
 // T.C: O(nlogn);   where n = size of array.
@@ -36,7 +35,7 @@ void quickSort(int arr[], int low, int high) {
 }
 
 int main() {
-    int arr[] = {10, 50, 30, 20, 40, 20, 10};
+    int arr[] = {30, 40, 20, 70, 10, 60, 50};
     int size = sizeof(arr)/sizeof(arr[0]);
     int low = 0;
     int high = size - 1;
