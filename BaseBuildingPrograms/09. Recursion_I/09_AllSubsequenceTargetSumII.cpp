38617bbf43027e96d4aba0d397cbf380949a6d3b
Bishnu Mishra <mishravishnu369@gmail.com>
2024-06-23 01:45:33 +0530

    removed sum

--- BaseBuildingPrograms/09. Recursion_I/09_AllSubsequenceTargetSumII.cpp
+++ BaseBuildingPrograms/09. Recursion_I/09_AllSubsequenceTargetSumII.cpp
@@ -21,11 +21,11 @@ using namespace std;
 
 // T.C: O(2^n)
 // S.C: O(n)
-void printAnySubsequence_approach1(int index, int arr[], int n, int& target, vector<int>& ds, int sum, bool& found) {
+void printAnySubsequence_approach1(int index, int arr[], int n, int target, vector<int>& ds, bool& found) {
     // Base case: If we've considered all elements
     if(index >= n) {
-        // If the sum of the current subsequence equals the target, return true
-        if(sum == target) {
+        // If the target equals zero, the subsequence is found in 'ds'. Print it and mark found as true.
+        if(target == 0) {
             for(int i: ds) {
                 cout << i << " ";
             }
@@ -38,21 +38,21 @@ void printAnySubsequence_approach1(int index, int arr[], int n, int& target, vec
     // Case 1: If subsequence is not found yet, try including the current element and proceed to make the subsequence
     if(!found) {
         ds.push_back(arr[index]);
-        printAnySubsequence_approach1(index + 1, arr, n, target, ds, sum + arr[index], found);
+        printAnySubsequence_approach1(index + 1, arr, n, target - arr[index], ds, found);
         ds.pop_back();
     }
     
     // Case 2: If subsequence is not found yet, try excluding the current element and proceed to make the subsequence
-    if(!found) printAnySubsequence_approach1(index + 1, arr, n, target, ds, sum, found);
+    if(!found) printAnySubsequence_approach1(index + 1, arr, n, target, ds, found);
 }
 
 // T.C: O(2^n)
 // S.C: O(n)
-bool printAnySubsequence_approach2(int index, int arr[], int n, int& target, vector<int>& ds, int sum) {
+bool printAnySubsequence_approach2(int index, int arr[], int n, int target, vector<int>& ds) {
     // Base case: If we've considered all elements
     if(index >= n) {
-        // If the sum of the current subsequence equals the target, return true
-        if(sum == target) {
+        // If the target equals zero, subsequence is found in 'ds'. Print it and return true.
+        if(target == 0) {
             for(int i: ds) {
                 cout << i << " ";
             }
@@ -65,12 +65,12 @@ bool printAnySubsequence_approach2(int index, int arr[], int n, int& target, vec
     // Case 1: Including the current element and proceed to make the subsequence
     ds.push_back(arr[index]);
     // If this function call returns a true, it means a subsequence has already been found via this path. Thus, return true.
-    if(printAnySubsequence_approach2(index + 1, arr, n, target, ds, sum + arr[index]) == true) return true;
+    if(printAnySubsequence_approach2(index + 1, arr, n, target - arr[index], ds) == true) return true;
     ds.pop_back();
 
     // Case 2: Excluding the current element and proceed to make the subsequence
     // If this function call returns a true, it means a subsequence has already been found via this path. Thus, return true.
-    if(printAnySubsequence_approach2(index + 1, arr, n, target, ds, sum) == true) return true;
+    if(printAnySubsequence_approach2(index + 1, arr, n, target, ds) == true) return true;
 
     // If program comes here, it means no subsequence has been found in any of the 2 paths yet. Thus, return false.
     return false;
@@ -78,18 +78,16 @@ bool printAnySubsequence_approach2(int index, int arr[], int n, int& target, vec
 
 int main() {
     int arr[] = {1, 2, 1, 4, 2, 3};
-    int n = 6;
+    int n = sizeof(arr) / sizeof(arr[0]);
     int target = 4;
     int index = 0;   // starting index
 
     vector<int> ds;   // used as a helper data-structure to store subsequences
-    int sum = 0;   // used as a helper data-structure to store sum of elements in the subsequences
     bool found = false;   // used as a helper data-structure to validate if any subsequence found or not
-
-    printAnySubsequence_approach1(index, arr, n, target, ds, sum, found);
+    printAnySubsequence_approach1(index, arr, n, target, ds, found);
     if(!found) cout << -1 << endl;   // Print -1 if no valid subsequence was found
-    
-    if(!printAnySubsequence_approach2(index, arr, n, target, ds, sum)) {
+    cout << endl;
+    if(!printAnySubsequence_approach2(index, arr, n, target, ds)) {
         cout << -1 << endl;   // Print -1 if no valid subsequence was found
     }
 
