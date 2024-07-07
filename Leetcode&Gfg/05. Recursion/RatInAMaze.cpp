3fc19ec86102288c6e5ec21ae620b279d0ed5d23
Bishnu Mishra <mishravishnu369@gmail.com>
2024-07-07 21:43:27 +0530

    ratinamaze

--- Leetcode&Gfg/05. Recursion/RatInAMaze.cpp
+++ Leetcode&Gfg/05. Recursion/RatInAMaze.cpp
@@ -1,19 +1,76 @@
+// GFG: Rat in a Maze Problem - I   --->   Consider a rat placed at (0, 0) in a square matrix of order N * N. It has to reach
+// the destination at (N - 1, N - 1). Find all possible paths that the rat can take to reach from source to destination. The
+// directions in which the rat can move are 'U'(up), 'D'(down), 'L' (left), 'R' (right). Value 0 at a cell in the matrix
+// represents that it is blocked and rat cannot move to it while value 1 at a cell in the matrix represents that rat can be
+// travel through it.
+// Note: In a path, no cell can be visited more than one time. If the source cell is 0, the rat cannot move to any other cell.
+
+// Example 1:
+// Input:
+// N = 4
+// m[][] = {{1, 0, 0, 0},
+//          {1, 1, 0, 1}, 
+//          {1, 1, 0, 0},
+//          {0, 1, 1, 1}}
+// Output:
+// DDRDRR DRDDRR
+// Explanation:
+// The rat can reach the destination at (3, 3) from (0, 0) by two paths - DRDDRR and DDRDRR, when printed in sorted order we get DDRDRR DRDDRR.
+
+// Example 2:
+// Input:
+// N = 2
+// m[][] = {{1, 0},
+//          {1, 0}}
+// Output:
+// -1
+// Explanation:
+// No path exists and destination cell is blocked.
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
+class Solution{
+    public:
+    // T.C: 
+    // S.C: 
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        
+    }
+};
+
+int main() {
+    vector<vector<int>> m = {{1, 0, 0, 0},
+                             {1, 1, 0, 1}, 
+                             {1, 1, 0, 0},
+                             {0, 1, 1, 1}};
+    int N = 4;
+    
+    Solution sol;
+    vector<string> ans = sol.findPath(m, N);
+    for(string s: ans) cout << s << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+/*
 bool isAllowed(int maze[][4], int row, int col, int newi, int newj, vector<vector<bool>> &visited) {
     // It is ony allowed to visit a location, if:
     // i)   location to be visited is not out of bound
     // ii)  it's allowed to visit that particular location in the maze, i.e path is open, i.e. 1
     // iii) the location to be visited is marked 'not visited' i.e. 'false' in 'visited' array'
-    if((newi>=0 && newi<row) && (newj>=0 && newj<col)
-        && (maze[newi][newj] == 1)
-        && (visited[newi][newj] == false)) {
-            return true;
-    } else {
-        return false;
-    }
+    if((newi>=0 && newi<row) && (newj>=0 && newj<col) && (maze[newi][newj] == 1) && (visited[newi][newj] == false)) return true;
+    else return false;
 }
 
 void printAllPath(int maze[][4], int row, int col, int i, int j, string &output, vector<vector<bool>> &visited) {
@@ -81,8 +138,8 @@ int main() {
     int maze[4][4] = {
         {1, 0, 0, 0},
         {1, 1, 0, 0},
-        {1, 1, 1, 0},
-        {1, 1, 1, 1}
+        {1, 1, 0, 0},
+        {0, 1, 1, 1}
     };
     int row = 4;
     int col = 4;
@@ -106,3 +163,4 @@ int main() {
 
     return 0;
 }
+*/
