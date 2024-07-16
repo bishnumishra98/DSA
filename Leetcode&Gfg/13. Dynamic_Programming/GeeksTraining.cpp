64b32900474b8e3b93b99371e372fe7f8adb0c95
Bishnu Mishra <mishravishnu369@gmail.com>
2024-07-17 02:10:02 +0530

    geekTraining

--- Leetcode&Gfg/13. Dynamic_Programming/GeeksTraining.cpp
+++ Leetcode&Gfg/13. Dynamic_Programming/GeeksTraining.cpp
@@ -28,6 +28,8 @@ using namespace std;
 
 class Solution {
 public:
+    // solve() function returns the maximum points collected till day 'day'. It takes two varying arguments: current day
+    // and the last day's task that was performed so that we do not repeat the last day's task on current day.
     int solve(int day, int last, vector<vector<int>>& points) {
         // Base case: When you are at index 0, i.e., 0th day; return the maximum points possible from the all days except the last day.
         if(day == 0) {
@@ -50,7 +52,7 @@ public:
     // T.C: O(3^n)
     // S.C: O(n)
     int maximumPoints_recursion(vector<vector<int>>& points, int n) {
-        // Start computing maximum points collected from last element to first element. Starting with last day as '3',
+        // Start computing maximum points collected from last element to first element. Starting with last task as '3',
         // means task 3 (T3) was the last task, which actually does not exists in the list. This signifies that none of
         // the tasks are performed yet. So geek is free to take up any task(T0, T1, T2) initially.
         return solve(n-1, 3, points);
@@ -65,7 +67,7 @@ public:
             return maxi;
         }
 
-        if(dp[day][last] != -1) return dp[day][last];
+        if(dp[day][last] != -1) return dp[day][last];   // if this subproblem has been previously solved, return it.
 
         int maxi = 0;
         for(int task = 0; task <= 2; task++) {
@@ -74,14 +76,14 @@ public:
                 maxi = max(maxi, earning);
             }
         }
-        dp[day][last] = maxi;
+        dp[day][last] = maxi;   // dp[day][last] stores the maximum points collected till day 'day' when last day's task was 'last'.
         return dp[day][last];
     }
 
     // T.C: O(n * 4) = O(n)
     // S.C: O(n)(recursion stack space) + O(n * 4) = O(n)
     int maximumPoints_memoization(vector<vector<int>>& points, int n) {
-        vector<vector<int>> dp(n, vector<int>(4, -1));   // kept size 4 as tasks are T0, T1, T2, T3(for initial recursive call)
+        vector<vector<int>> dp(n, vector<int>(4, -1));   // kept column size 4 as tasks are T0, T1, T2, T3(for initial recursive call)
         return solve(n-1, 3, points, dp);
     }
 
@@ -90,15 +92,68 @@ public:
     // T.C: O(n * 4 * 3) = O(n)
     // S.C: O(n * 4) = O(n)
     int maximumPoints_tabulation(vector<vector<int>>& points, int n) {
-        
+        // Create a 2D DP (Dynamic Programming) table to store the maximum points
+        // dp[i][j] represents the maximum points at day i, considering the last activity as j
+        vector<vector<int>> dp(n, vector<int>(4, 0));
+        // Base cases: Initialize the DP table for the first day (day 0). At dp[0][last], store the
+        // maximum possible points by performing any of the task except the last day's task.
+        dp[0][0] = max(points[0][1], points[0][2]);
+        dp[0][1] = max(points[0][0], points[0][2]);
+        dp[0][2] = max(points[0][0], points[0][1]);
+        dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
+
+        // Populate 'dp' array starting from day 1 to day 'n-1'
+        for(int day = 1; day <= n-1; day++) {
+            for(int last = 0; last <= 3; last++) {
+                // Iterate through the tasks for the current day (just copy paste the memoization logic)
+                for(int task = 0; task <= 2; task++) {
+                    if(task != last) {
+                        // int earning = points[day][task] + dp[day-1][task];
+                        // dp[day][last] = max(dp[day][last], earning);
+                        dp[day][last] = max(dp[day][last], points[day][task] + dp[day-1][task]);   // the above 2 lines in 1 single line
+                    }
+                }
+            }
+        }
+
+        return dp[n-1][3];   // return the maximum points for the last day
     }
 
 // -----------------------------------------------------------------------------------------------------------------------
 
     // T.C: O(n * 4 * 3) = O(n)
-    // S.C: O(n * 4) = O(n)
+    // S.C: O(4)
     int maximumPoints_tabulation_SO(vector<vector<int>>& points, int n) {
-        
+        // Initialize a vector to store the maximum points for the previous day's activities
+        vector<int> prev(4, 0);
+
+        // Initialize the DP table for the first day (day 0)
+        prev[0] = max(points[0][1], points[0][2]);
+        prev[1] = max(points[0][0], points[0][2]);
+        prev[2] = max(points[0][0], points[0][1]);
+        prev[3] = max(points[0][0], max(points[0][1], points[0][2]));
+
+        // Iterate through the days starting from day 1
+        for(int day = 1; day < n; day++) {
+            // Create a temporary vector to store the maximum points for the current day's activities
+            vector<int> temp(4, 0);
+            for (int last = 0; last < 4; last++) {
+                temp[last] = 0;
+                // Iterate through the tasks for the current day
+                for(int task = 0; task <= 2; task++) {
+                    if(task != last) {
+                        // Calculate the points for the current activity and add it to the
+                        // maximum points obtained on the previous day (stored in prev)
+                        temp[last] = max(temp[last], points[day][task] + prev[task]);
+                    }
+                }
+            }
+            // Update prev with the maximum points for the current day
+            prev = temp;
+        }
+
+        // The maximum points for the last day with any activity can be found in prev[3]
+        return prev[3];
     }
 };
 
