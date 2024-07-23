79636bc17b260d47383ee0e7d65350604305930c
Bishnu Mishra <mishravishnu369@gmail.com>
2024-07-24 01:26:51 +0530

    g

--- BaseBuildingPrograms/22. Graph/G03_BFSTraversalOfGraph.cpp
+++ BaseBuildingPrograms/22. Graph/G03_BFSTraversalOfGraph.cpp
@@ -1,4 +1,4 @@
-// BFS Traversal of Tree means to traverse breadth wise, i.e., level order traversal.
+// BFS Traversal of Graph means to traverse breadth wise, i.e., level order traversal.
 // Consider first element of graph be 0, second be 1, and so on. Thus, if number of nodes(V)
 // in the graph is 5, the graph elements will be 0, 1, 2, 3 and 4.
 
---
+++ BaseBuildingPrograms/22. Graph/G04_DFSTraversalOfGraph.cpp
@@ -0,0 +1,3 @@
+// DFS Traversal of Graph means to traverse depth wise, i.e., level order traversal.
+// Consider first element of graph be 0, second be 1, and so on. Thus, if number of nodes(V)
+// in the graph is 5, the graph elements will be 0, 1, 2, 3 and 4.
