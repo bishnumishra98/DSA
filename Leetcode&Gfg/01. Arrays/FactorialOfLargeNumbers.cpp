4a7e67f79c93ba0c98e6f3bb62e86f01e3a2ba8c
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-24 02:16:04 +0530

    arr

--- Leetcode&Gfg/01. Arrays/FactorialOfLargeNumbers.cpp
+++ Leetcode&Gfg/01. Arrays/FactorialOfLargeNumbers.cpp
@@ -15,6 +15,8 @@
 // You don't need to read input or print anything. Complete the function factorial() that takes integer N as input parameter
 // and returns a list of integers denoting the digits that make up the factorial of N.
 
+// Algorithm: 
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -23,14 +25,30 @@ public:
     // T.C: 
     // S.C: 
     vector<int> factorial(int N){
-        
+        vector<int> ans;
+        ans.push_back(1);
+        int carry = 0;
+        for(int i = 2; i <= N; i++) {
+            for(int j = 0; j < ans.size(); j++) {
+                int product = ans[j] * i + carry;
+                ans[j] = product % 10;
+                carry = product / 10;
+            }
+            while(carry) {
+                ans.push_back(carry % 10);
+                carry = carry / 10;
+            }
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
 
 int main() {
-    int N = 20;
+    int N = 50;
     vector<int> ans = Solution().factorial(N);
-    for(auto it: ans) cout << it << " ";
+    for(auto it: ans) cout << it;
 
     return 0;
 }
