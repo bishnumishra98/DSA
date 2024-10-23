866eb56904924879f7fbe4a433e36f7cb4ea7d42
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-24 04:19:41 +0530

    arr

--- Leetcode&Gfg/01. Arrays/RemoveDuplicatesFromSortedArray.cpp
+++ Leetcode&Gfg/01. Arrays/RemoveDuplicatesFromSortedArray.cpp
@@ -18,23 +18,59 @@
 // Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
 
-// Algorithm:
+// Algorithm: The algorithm is simple. We will use two pointer approach.
+// 1. Initialize j with 0, and i with 1 index respectively. The idea of this algorithm is all elements till j must be unique.
+// 2. Compare nums[j] and nums[i] and if they are equal, a duplicate element is present at index i. Thus, increment i and
+//    move it ahead. Else if nums[j] and nums[i] are not equal, it means a different or unique element is present at index i.
+//    Thus, place that unique element just next after nums[j]. And then increment j as well, representing the boundary of
+//    updated range of unique elements. Continue the process until all elements of nums are iterated, i.e., until i reaches
+//    last index of nums.
+// 3. As j points to the index of last unique element of nums; the no.of unique elements is j+1. Thus, return j+1.
 
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    // T.C:
-    // S.C: 
+    // T.C: O(n);   where n = nums.size()
+    // S.C: O(1)
     int removeDuplicates(vector<int>& nums) {
-        
+        int j = 0, i = 1;
+        while(i < nums.size()) {
+            if(nums[j] == nums[i]) i++;   // if duplicate element is found at nums[i], go ahead to check next element
+            else {   // if(nums[j] != nums[i])
+                // If an unique element is found, keep it on (j+1)th index, i.e., just next after nums[j]
+                j++;
+                nums[j] = nums[i];
+                i++;
+            }
+        }
+
+        return j + 1;
+    }
+
+    // T.C: O(n);   where n = nums.size()
+    // S.C: O(1)
+    int removeDuplicates_shorthand(vector<int>& nums) {
+        int j = 0;
+        for(int i = 1; i < nums.size(); i++) {
+            // If an unique element is found, keep it on (j+1)th index, i.e., just next after nums[j].
+            if(nums[j] != nums[i]) nums[++j] = nums[i];
+        }
+
+        return j + 1;
     }
 };
 
+
 int main() {
     vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-    int k = Solution().removeDuplicates(nums);
+
+    // int k = Solution().removeDuplicates(nums);
+    // cout << "k: " << k << endl;
+    // for(auto it: nums) cout << it << " ";
+    
+    int k = Solution().removeDuplicates_shorthand(nums);
     cout << "k: " << k << endl;
     for(auto it: nums) cout << it << " ";
 
