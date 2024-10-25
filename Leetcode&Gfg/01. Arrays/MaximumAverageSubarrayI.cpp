747ef6e5a7d62634f04183b8ab89377089db51fb
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-25 19:37:36 +0530

    arr

--- Leetcode&Gfg/01. Arrays/MaximumAverageSubarrayI.cpp
+++ Leetcode&Gfg/01. Arrays/MaximumAverageSubarrayI.cpp
@@ -21,7 +21,7 @@ public:
     // T.C: O((n-k) * k) = O(n * k);   O(n-k) for outer loop, and O(k) for inner loop
     // S.C: O(1)
     double findMaxAverage_bruteforce(vector<int>& nums, int k) {
-        int i = 0, j = k-1;   // i and j surround the range of first k elements in 'nums'
+        int i = 0, j = k - 1;   // i and j surround the range of first k elements in 'nums'
         int maxSum = INT_MIN;
 
         while(j < nums.size()) {
@@ -38,13 +38,44 @@ public:
         double avg = (double)maxSum / k;
         return avg;
     }
+
+// -------------------------------------------------------------------------------------------
+
+    // Sliding window technique (just subtract and add one element in the window at a time; calculate sum and update maxSum if needed)
+    // T.C: O(k + (n-k)) = O(k) + O(n) = O(n)
+    // S.C: O(1)
+    double findMaxAverage(vector<int>& nums, int k) {
+        int sum = 0;
+        int maxSum = INT_MIN;
+
+        // Calculate sum of first k elements
+        for(int i = 0; i < k; i++) {
+            sum = sum + nums[i];
+        }
+        maxSum = max(maxSum, sum);   // update maxSum
+
+        // Now slide the window: subtract and add one element in the window at a time; calculate sum and update maxSum if needed.
+        int i = 0, j = k - 1;
+        while(j < nums.size() - 1) {
+            sum = sum - nums[i];
+            i++;
+            j++;
+            sum = sum + nums[j];
+            maxSum = max(maxSum, sum);
+        }
+
+        double avg = (double)maxSum / k;
+        return avg;
+    }
 };
+
 
 int main() {
     vector<int> nums = {1, 12, -5, -6, 50, 3};
     int k = 4;
 
-    cout << Solution().findMaxAverage_bruteforce(nums, k);
+    cout << Solution().findMaxAverage_bruteforce(nums, k) << endl;
+    cout << Solution().findMaxAverage(nums, k);
 
     return 0;
 }
