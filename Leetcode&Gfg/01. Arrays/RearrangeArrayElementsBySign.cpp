9d581f6f51216e1706cdc4411e32a10863ba7ee4
Bishnu Mishra <mishravishnu369@gmail.com>
2024-11-02 03:23:56 +0530

    arr

--- Leetcode&Gfg/01. Arrays/RearrangeArrayElementsBySign.cpp
+++ Leetcode&Gfg/01. Arrays/RearrangeArrayElementsBySign.cpp
@@ -21,74 +21,70 @@
 // 1 is the only positive integer and -1 the only negative integer in nums.
 // So nums is rearranged to [1,-1].
 
-#include <iostream>
-#include <vector>
+
+#include <bits/stdc++.h>
 using namespace std;
 
-void printVector(vector<int>& v) {
-    for (const auto& element : v) {
-        cout << element << " ";
-    }
-}
+class Solution {
+public:
+    // Brute force
+    // T.C: O(2.5n)
+    // S.C: O(2n)
+    vector<int> rearrangeArray_bruteForce(vector<int>& nums) {
+        int n = nums.size();
+        vector <int> positive;
+        vector <int> negative;
 
-// Brute force
-// T.C: O(n)
-// S.C: O(n)
-vector<int> rearrangeArray_bruteForce(vector<int>& nums) {
-    int n = nums.size();
-    vector <int> positive;
-    vector <int> negative;
-
-    for(int i=0; i<n; i++) {
-        if(nums[i] > 0) {
-            positive.push_back(nums[i]);
-        } else {
-            negative.push_back(nums[i]);
+        for(int i=0; i<n; i++) {
+            if(nums[i] > 0) {
+                positive.push_back(nums[i]);
+            } else {
+                negative.push_back(nums[i]);
+            }
         }
-    }
 
-    vector <int> arr;
+        vector <int> arr;
 
-    for(int i=0; i<n/2; i++) {
-        arr.push_back(positive[i]);
-        arr.push_back(negative[i]);
-    }
-
-    return arr;
-}
-
-// Two pointer approach
-// T.C: O(n)
-// S.C: O(n)
-vector<int> rearrangeArray(vector<int>& nums) {
-    vector <int> ans(nums.size(), -1);
-
-    int even = 0, odd = 1;
-
-    for(int i=0; i<nums.size(); i++) {
-        if(nums[i] > 0) {
-            ans[even] = nums[i];
-            even += 2;
-        } else {   // if(nums[i] < 0)
-            ans[odd] = nums[i];
-            odd += 2;
+        for(int i=0; i<n/2; i++) {
+            arr.push_back(positive[i]);
+            arr.push_back(negative[i]);
         }
+
+        return arr;
     }
 
-    return ans;
-}
+// -------------------------------------------------------------
+
+    // Two pointer approach
+    // T.C: O(n)
+    // S.C: O(n)
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        int even = 0, odd = 1;
+
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] > 0) {
+                ans[even] = nums[i];
+                even += 2;
+            } else {   // if(nums[i] < 0)
+                ans[odd] = nums[i];
+                odd += 2;
+            }
+        }
+
+        return ans;
+    }
+};
 
 
 int main() {
     vector <int> v = {3, 1, -2, -5, 2, -4};
 
-    // v = rearrangeArray_bruteForce(v);
+    // vector<int> ans = Solution().rearrangeArray_bruteForce(v);
+    // for(auto it: ans) cout << it << " ";
 
-    v = rearrangeArray(v);   // This approach is better in terms of space efficiency
-                            // and is equally efficient in terms of time complexity,
-                           // from the above function 'rearrangeArray_bruteForce()'.
-
-    printVector(v);
+    vector<int> ans = Solution().rearrangeArray(v);
+    for(auto it: ans) cout << it << " ";
 
     return 0;
 }
