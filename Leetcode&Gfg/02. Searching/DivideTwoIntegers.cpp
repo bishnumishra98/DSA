095f6ef1f1ae9c89f2c45c3c6a58fee3dc85c82d
Bishnu Mishra <mishravishnu369@gmail.com>
2024-11-07 22:14:01 +0530

    nm

--- Leetcode&Gfg/02. Searching/DivideTwoIntegers.cpp
+++ Leetcode&Gfg/02. Searching/DivideTwoIntegers.cpp
@@ -1,8 +1,21 @@
-// Leetcode: 29. Divide Two Integers ---> Given two integers dividend and divisor,
-// divide two integers without using multiplication, division, and mod operator.
-// Return the integral part of quotient after dividing dividend by divisor.
+// Leetcode: 29. Divide Two Integers   --->   Given two integers dividend and divisor, divide two integers without
+// using multiplication, division, and mod operator.
+// The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would
+// be truncated to 8, and -2.7335 would be truncated to -2.
+// Return the quotient after dividing dividend by divisor.
+// Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer
+// range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1,
+// and if the quotient is strictly less than -231, then return -231.
 
-// Constraints -> -2^31 <= dividend, divisor <= 2^31 - 1, divisor != 0
+// Example 1:
+// Input: dividend = 10, divisor = 3
+// Output: 3
+// Explanation: 10/3 = 3.33333.. which is truncated to 3.
+
+// Example 2:
+// Input: dividend = 7, divisor = -3
+// Output: -2
+// Explanation: 7/-3 = -2.33333.. which is truncated to -2.
 
 // This problem is an implementation of binary search, and search space concept.
 
@@ -10,59 +23,51 @@
 #include <climits>   // for INT_MIN and INT_MAX
 using namespace std;
 
-int correctSign(int dividend, int divisor) {
-    if((dividend>0 && divisor<0) || (dividend<0 && divisor>0)) {
-        return -1;
-    }
-    return 1;
-}
+class Solution {
+public:
+    // T.C: O(log(dividend))
+    // S.C: O(1)
+    int divide(int dividend, int divisor) {
+        // If we convert the negative value '-2147483648' to its absolute value,
+        // its value will become outside the range of a 32-bit signed integer '2147483647'. 
+        if(dividend == INT_MIN && divisor == -1) return INT_MAX;
+        if(dividend == INT_MIN && divisor == 1) return INT_MIN;
 
-// Leetcode given function
-// T.C: O(log(dividend))
-// S.C: O(1)
-int divide(int dividend, int divisor) {
-    // If we convert the negative value '-2147483648' to its absolute value,
-   // its value will become outside the range of a 32-bit signed integer '2147483647'. 
-    if((dividend == INT_MIN) && (divisor==-1)) {
-        return INT_MAX;
-    } else if((dividend == INT_MIN) && (divisor==1)) {
-        return INT_MIN;
-    }
+        int sign = ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) ? -1 : 1;
 
-    int sign = correctSign(dividend, divisor);
+        // Making sure our binary search algorithm gets only positive integers
+        long long long_dividend = abs(dividend);
+        long long long_divisor = abs(divisor);
 
-    // making sure our binary search algorithm gets only positive integers
-    long long long_dividend = abs(dividend);
-    long long long_divisor = abs(divisor);
+        long long start = 0;
+        long long end = long_dividend;   // Our search space is going to be from start to end.
+        long long mid = start + (end - start)/2;   // consider mid as quotient.
+        long long ans = -1;
 
-    long long start = 0;
-    long long end = long_dividend;   // Our search space is going to be from start to end.
-    long long mid = start + (end - start)/2;   // consider mid as quotient.
-    long long ans = -1;
+        while(start <= end) {
+            if((mid * long_divisor) == long_dividend) {   // if mid is the quotient
+                return mid * sign;
+            } else if((mid * long_divisor) < long_dividend) {   // store ans and go to right
+                ans = mid;   // if '(mid*long_divisor) < long_dividend', then 'mid' may be a potential
+                // answer for quotient. That's why we are storing its value before going to
+                // further step. If 'long_dividend' was a perfect square, then storing 'mid' was not
+                // necessary. Dry run for better understanding.
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
 
-    while(start <= end) {
-        if((mid*long_divisor) == long_dividend) {   // if mid is the quotient
-            return mid*sign;
-        } else if((mid*long_divisor) < long_dividend) {   // store ans and go to right
-            ans = mid;   // if '(mid*long_divisor) < long_dividend', then 'mid' may be a potential
-            // answer for quotient. That's why we are storing its value before going to
-           // further step. If 'long_dividend' was a perfect square, then storing 'mid' was not
-          // necessary. Dry run for better understanding.
-            start = mid + 1;
-        } else {
-            end = mid - 1;
+            mid = start + (end - start) / 2;
         }
 
-        mid = start + (end - start)/2;
+        return ans * sign;   // returning the answer with correct sign
     }
-
-    return ans*sign;   // returning the answer with correct sign
-}
+};
 
 
 int main() {
-    int dividend = 2147483647, divisor = 2;
-    cout << divide(dividend, divisor);
+    int dividend = 7, divisor = -3;
+    cout << Solution().divide(dividend, divisor);
 
     return 0;
 }
