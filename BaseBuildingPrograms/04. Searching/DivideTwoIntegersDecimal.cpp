aca3fcb4e59954a39914fab930a65b06302965d7
Bishnu Mishra <mishravishnu369@gmail.com>
2024-11-09 23:38:57 +0530

    search

--- BaseBuildingPrograms/04. Searching/DivideTwoIntegersDecimal.cpp
+++ BaseBuildingPrograms/04. Searching/DivideTwoIntegersDecimal.cpp
@@ -15,51 +15,32 @@
 #include <climits>   // for INT_MIN and INT_MAX
 using namespace std;
 
-int correctSign(int dividend, int divisor) {
-    if((dividend>0 && divisor<0) || (dividend<0 && divisor>0)) {
-        return -1;
-    }
-    return 1;
-}
-
-// integer quotient function
 int divide(int dividend, int divisor) {
-    // If we convert the negative value '-2147483648' to its absolute value,
-   // its value will become outside the range of a 32-bit signed integer '2147483647'. 
-    if((dividend == INT_MIN) && (divisor==-1)) {
-        return INT_MAX;
-    } else if((dividend == INT_MIN) && (divisor==1)) {
-        return INT_MIN;
-    }
+    if(dividend == INT_MIN && divisor == -1) return INT_MAX;
+    if(dividend == INT_MIN && divisor == 1) return INT_MIN;
 
-    int sign = correctSign(dividend, divisor);
+    int sign = ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) ? -1 : 1;
 
-    // making sure our binary search algorithm gets only positive integers
-    long long long_dividend = abs(dividend);
-    long long long_divisor = abs(divisor);
+    long long abs_dividend = abs(dividend);
+    long long abs_divisor = abs(divisor);
 
     long long start = 0;
-    long long end = long_dividend;   // Our search space is going to be from start to end.
-    long long mid = start + (end - start)/2;   // consider mid as quotient.
-    long long ans = -1;
+    long long end = abs_dividend;
+    long long mid = start + (end - start)/2;
+    long long ans = 0;
 
     while(start <= end) {
-        if((mid*long_divisor) == long_dividend) {   // if mid is the quotient
-            return mid*sign;
-        } else if((mid*long_divisor) < long_dividend) {   // store ans and go to right
-            ans = mid;   // if '(mid*long_divisor) < long_dividend', then 'mid' may be a potential
-            // answer for quotient. That's why we are storing its value before going to
-           // further step. If 'long_dividend' was a perfect square, then storing 'mid' was not
-          // necessary. Dry run for better understanding.
+        if(mid * abs_divisor > abs_dividend) end = mid - 1;
+        else if(mid * abs_divisor == abs_dividend) return mid * sign;
+        else if(mid * abs_divisor < abs_dividend) {
+            ans = mid;
             start = mid + 1;
-        } else {
-            end = mid - 1;
         }
 
-        mid = start + (end - start)/2;
+        mid = start + (end - start) / 2;
     }
 
-    return ans*sign;   // returning the answer with correct sign
+    return ans * sign;
 }
 
 // T.C: O(precision*log(dividend))
@@ -69,11 +50,11 @@ double divide_Decimal(int dividend, int divisor, int precision) {
     
     double step = 1;
 
-    for(int i=0; i<precision; i++) {
+    for(int i = 0; i < precision; i++) {
         step = step / 10;
         double j = quot;
 
-        while(j*divisor <= dividend) {
+        while(j * divisor <= dividend) {
             quot = j;
             j += step;
         }
