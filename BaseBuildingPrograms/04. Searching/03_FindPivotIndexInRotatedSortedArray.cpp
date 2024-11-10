b794c2f56b836086f4ce4535922c06ee6b8d75db
Bishnu Mishra <mishravishnu369@gmail.com>
2024-11-10 21:54:08 +0530

    search

--- BaseBuildingPrograms/04. Searching/03_FindPivotIndexInRotatedSortedArray.cpp
+++ BaseBuildingPrograms/04. Searching/03_FindPivotIndexInRotatedSortedArray.cpp
@@ -6,6 +6,35 @@
 #include <vector>
 using namespace std;
 
+// Algorithm: Algo is very simple. Lets consider the array: {40, 50, 60, 70, 10, 20, 30}. There are two monotonic
+//            functions formed here. One is line A (40, 50, 60, 70) and other one is line B (10, 20, 30). Line A
+//            is higher than line B in graphical representation.
+//            Although binary search cannot be applied direct on the whole array, but can be applied separately on
+//            line A and line B elements respectively, as they are sorted; and the two elements that are unsorted
+//            for each other(pivot and element next to pivot) shall be handled separately.
+//            Keep in mind, that pivot element will always lie on line A and it will be the greatest element of line A.
+//            All total there can be 4 cases combined, and as usual start = 0, mid = (start+end)/2 and end = last
+//            element of array, and we will be comparing arr[mid] with other elements until 'start' crosses 'end'.
+
+// Case 1: If element is on line A and pivot index, i.e., largest element, i.e., on 70.
+//         If element is on pivot index, the next element has to be smaller than it. Thus, it is the
+//         only case when arr[mid] > arr[mid+1]. Thus, return the pivot index(mid) in this case.
+
+// Case 2: If element is on line B and first element of line B, i.e., smallest element, i.e., on 10.
+//         If elements is smallest one, it is the only case when arr[mid] < arr[mid-1]. Thus, in this
+//         case return the pivot index which lies just behind it, i.e., 'on mid-1' position.
+
+// Case 3: If element is on line B, other than smallest element.
+//         If element is on line B, other than smallest element, it means arr[mid] < arr[start], because altitude of all
+//         elements of line A is more than altitude of all elements of line B. Thus, in this case the pivot will be not
+//         present on this line, it will lie on the above line, i.e., A. Thus, shift the search space towards left,
+//         i.e., end = mid-1.
+
+// Case 4: If element is on line A, other than pivot index.
+//         If element is on line A, other than pivot index, it means arr[mid] >= arr[start]. Thus, in this case
+//         the pivot will be somewhere on the right. Hence, shift the search space towards right, i.e., start = mid+1.
+
+
 // T.C: O(logn)
 // S.C: O(1)
 int findPivotIndex(vector<int>& nums) {
@@ -13,26 +42,12 @@ int findPivotIndex(vector<int>& nums) {
     int start = 0, end = n - 1, mid = start + (end - start)/2;
 
     while(start <= end) {
-        if(start == end) {
-            // if only 1 element is present in the array, then it itself is the pivot element
-            return start;
-        }
+        if(start == end) return start;   // if only 1 element is present in the array, then it itself is the pivot element
 
-        if((mid+1 < n) && (nums[mid] > nums[mid+1])) {   // if mid is on element 70, i.e.
-               // pivot element itself. '(mid+1 < n)' ensures we do not move out of index.
-              // Make sure to write the condition '(mid+1 < n)' as first operand of &&, or else some compilers may throw compile error.
-            return mid;
-        } else if((mid-1 >= 0) && (nums[mid] < nums[mid-1])) {   // if mid is on element 10, i.e.
-          // element just after pivot element. '(mid-1 >= 0)' ensures we do not move out of index.
-         // Make sure to write the condition '(mid-1 >= 0)' as first operand of &&, or else some compilers may throw compile error.
-            return mid-1;
-        } else if(nums[mid] < nums[start]) {   // if mid is on line B, i.e. shorter ascending slope.
-                                              // In this case, move to left.
-            end = mid - 1;
-        } else {   // if(nums[mid] > nums[start]), i.e. mid is on line A, i.e. taller ascending
-                  // slope. In this case, move to right.
-            start = mid + 1;
-        }
+        if((mid+1 < n) && (nums[mid] > nums[mid+1])) return mid;
+        else if((mid-1 >= 0) && (nums[mid] < nums[mid-1])) return mid-1;
+        else if(nums[mid] < nums[start]) end = mid - 1;
+        else start = mid + 1;
 
         mid = start + (end - start)/2;
     }
