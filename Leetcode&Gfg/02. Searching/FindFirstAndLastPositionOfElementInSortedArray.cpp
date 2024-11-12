8fe5c6ee6df96e9f04b8cf9d1c9cc60f30e377a6
Bishnu Mishra <mishravishnu369@gmail.com>
2024-11-12 19:01:41 +0530

    nm

--- Leetcode&Gfg/02. Searching/FindFirstAndLastPositionOfElementInSortedArray.cpp
+++ Leetcode&Gfg/02. Searching/FindFirstAndLastPositionOfElementInSortedArray.cpp
@@ -1,87 +1,93 @@
-// Leetcode: 34. Find First and Last Position of Element in Sorted Array
-// This problem is an implementation of binary search
+// Leetcode: 34. Find First and Last Position of Element in Sorted Array   --->   Given an array of integers nums
+// sorted in non-decreasing order, find the starting and ending position of a given target value.
+// If target is not found in the array, return [-1, -1].
+// You must write an algorithm with O(log n) runtime complexity.
 
-#include <iostream>
-#include <vector>
+// Example 1:
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+
+// Example 2:
+// Input: nums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+
+// Example 3:
+// Input: nums = [], target = 0
+// Output: [-1,-1]
+
+#include <bits/stdc++.h>
 using namespace std;
 
-void printVector(vector<int>& v) {
-    // Using a range-based for loop with auto
-    for (const auto& element : v) {
-        cout << element << " ";
-    }
-}
+class Solution {
+private:
+    // T.C: O(logn)
+    // S.C: O(1)
+    int findFirstPosition(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = start + (end - start) / 2;
+        int ans = -1;
 
-// T.C: O(logn)
-// S.C: O(1)
-int findFirstPosition(vector<int>& nums, int target) {
-    int start = 0;
-    int end = nums.size() - 1;
-    int mid = start + (end - start)/2;
-    int ans = -1;
+        while(start <= end) {
+            if(target == nums[mid]) {
+                ans = mid;
+                end = mid - 1;
+            } else if(target > nums[mid]) {
+                start = mid + 1;
+            } else {   // if(target < nums[mid])
+                end = mid - 1;
+            }
 
-    while(start <= end) {
-        if(target == nums[mid]) {
-            ans = mid;
-            end = mid - 1;
-        } else if(target > nums[mid]) {
-            start = mid + 1;
-        } else {   // if(target < nums[mid])
-            end = mid - 1;
+            mid = start + (end - start) / 2;
         }
 
-        mid = start + (end - start)/2;
+        return ans;
     }
 
-    return ans;
-}
+    // T.C: O(logn)
+    // S.C: O(1)
+    int findLastPosition(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = start + (end - start) / 2;
+        int ans = -1;
 
-// T.C: O(logn)
-// S.C: O(1)
-int findLastPosition(vector<int>& nums, int target) {
-    int start = 0;
-    int end = nums.size() - 1;
-    int mid = start + (end - start)/2;
-    int ans = -1;
+        while(start <= end) {
+            if(target == nums[mid]) {
+                ans = mid;
+                start = mid + 1;
+            } else if(target > nums[mid]) {
+                start = mid + 1;
+            } else {   // if(target < nums[mid])
+                end = mid - 1;
+            }
 
-    while(start <= end) {
-        if(target == nums[mid]) {
-            ans = mid;
-            start = mid + 1;
-        } else if(target > nums[mid]) {
-            start = mid + 1;
-        } else {   // if(target < nums[mid])
-            end = mid - 1;
+            mid = start + (end - start) / 2;
         }
 
-        mid = start + (end - start)/2;
+        return ans;
     }
 
-    return ans;
-}
+public:
+    // T.C: O(logn) + O(logn) = O(logn)
+    // S.C: O(1) + O(1) = O(1)
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int firstPosition = findFirstPosition(nums, target);
+        int lastPosition = findLastPosition(nums, target);
 
-// leetcode given function
-// T.C: O(logn) + O(logn) = O(logn)
-// S.C: O(1) + O(1) = O(1)
-vector<int> searchRange(vector<int>& nums, int target) {
-    int firstPosition = findFirstPosition(nums, target);
-    int lastPosition = findLastPosition(nums, target);
+        vector <int> v;
+        v.push_back(firstPosition);
+        v.push_back(lastPosition);
+        return v;
+    }
+};
 
-    vector <int> v;
-
-    v.push_back(firstPosition);
-    v.push_back(lastPosition);
-
-    return v;
-}
 
 int main() {
     vector <int> v = {5, 7, 7, 7, 8, 8, 10};
     int target = 7;
 
-    vector <int> ans = searchRange(v, target);
-
-    printVector(ans);
+    vector <int> ans = Solution().searchRange(v, target);
 
     // Additional info
     cout << "\nTotal no.of occurrences of target: " << (ans.back() - ans.front() + 1);
@@ -89,4 +95,3 @@ int main() {
 
     return 0;
 }
-
