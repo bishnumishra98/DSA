8fce4f5ffb26d71e0c1a3df998d25cafcbcb0377
Bishnu Mishra <mishravishnu369@gmail.com>
2024-11-13 11:43:44 +0530

    nm

--- Leetcode&Gfg/02. Searching/SearchInRotatedSortedArray.cpp
+++ Leetcode&Gfg/02. Searching/SearchInRotatedSortedArray.cpp
@@ -39,41 +39,28 @@ private:
         int start = 0, end = n - 1, mid = start + (end - start)/2;
 
         while(start <= end) {
-            if(start == end) {   // if only 1 element is present in array
-                return start;
-            }
+            if(start == end) return start;   // if only 1 element is present in the array, then it itself is the pivot element
 
-            if((mid+1 < n) && (nums[mid] > nums[mid+1])) {   // on pivot element
-                return mid;
-            } else if((mid-1 >= 0) && (nums[mid] < nums[mid-1])) {   // on element just next to pivot
-                return mid-1;
-            } else if(nums[mid] < nums[start]) {   // on line B, i.e shorter ascending slope
-                end = mid - 1;
-            } else {   // on line A, i.e ascending taller slope
-                start = mid + 1;
-            }
+            if((mid+1 < n) && (nums[mid] > nums[mid+1])) return mid;
+            else if((mid-1 >= 0) && (nums[mid] < nums[mid-1])) return mid-1;
+            else if(nums[mid] < nums[start]) end = mid - 1;
+            else start = mid + 1;   // if(nums[mid] >= nums[start])
 
-            mid = start + (end - start)/2;
+            mid = start + (end - start) / 2;
         }
 
-        // Returning a default value if none of the conditions are met
-        return -1;
+        return -1;   // to avoid error 'function does not return a value in all control paths'
     }
 
-
     int findTargetIndexByBinarySearch(vector<int>& arr, int start, int end, int target) {
-        int mid = start + (end - start)/2;
+        int mid = start + (end - start) /2 ;
 
         while(start <= end) {
-            if(target == arr[mid]) {
-                return mid;
-            } else if(target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+            if(target > arr[mid]) start = mid + 1;
+            else if(target < arr[mid]) end = mid - 1;
+            else return mid;   // if(target == arr[mid])
 
-            mid = start + (end - start)/2;
+            mid = start + (end - start) / 2;
         }
 
         return -1;   // if target not found in array
@@ -90,10 +77,10 @@ public:
 
         // Part 2: Applying binary search in each of the two monotonic parts, i.e., line A and line B.
         // If target lies in line A, i.e., taller ascending slope
-        if((target >= nums[0]) && (target <= nums[pivotIndex])) {
+        if(target >= nums[0] && target <= nums[pivotIndex]) {
             ans = findTargetIndexByBinarySearch(nums, 0, pivotIndex, target);   // T.C: O(logn), S.C: O(1)
         } else {
-            // if target lies in line B, i.e shorter ascending slope
+            // If target lies in line B, i.e shorter ascending slope
             ans = findTargetIndexByBinarySearch(nums, pivotIndex+1, n-1, target);   // T.C: O(logn), S.C: O(1)
         }
 
