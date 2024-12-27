d1d1f0edbba539ddee88f9ab6e468e9450d081e8
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-27 21:00:02 +0530

    nm

--- Leetcode&Gfg/00. Fundamental_programming/setKthBit.cpp
+++ Leetcode&Gfg/00. Fundamental_programming/setKthBit.cpp
@@ -8,11 +8,8 @@
 // Output:
 // 14
 // Explanation:
-// Binary representation of the given number
-// 10 is: 1 0 1 0, number of bits in the 
-// binary reprsentation is 4. Thus 2nd bit
-// from right is 0. The number after changing
-// this bit to 1 is: 14(1 1 1 0).
+// Binary representation of the given number 10 is: 1 0 1 0, number of bits in the binary reprsentation is 4. Thus 2nd bit
+// from right is 0. The number after changing this bit to 1 is: 14(1 1 1 0).
 
 // Example 2:
 // Input:
@@ -21,12 +18,8 @@
 // Output:
 // 15
 // Explanation:
-// The binary representation of the given
-// number 15 is: 1 1 1 1, number of bits
-// in the binary representation is 4. Thus
-// 3rd bit from the right is 1. The number
-// after changing this bit to 1 is
-// 15(1 1 1 1).
+// The binary representation of the given number 15 is: 1 1 1 1, number of bits in the binary representation is 4. Thus
+// 3rd bit from the right is 1. The number after changing this bit to 1 is 15(1 1 1 1).
 
 // Your Task:  
 // You don't need to read input or print anything. Your task is to complete the function setKthBit() which takes two
