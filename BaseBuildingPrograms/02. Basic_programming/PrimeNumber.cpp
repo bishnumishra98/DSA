1281da9fce02510cee21466df3c4f3b36677638c
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-27 20:57:01 +0530

    nm

--- BaseBuildingPrograms/02. Basic_programming/PrimeNumber.cpp
+++ BaseBuildingPrograms/02. Basic_programming/PrimeNumber.cpp
@@ -5,11 +5,11 @@ using namespace std;
 // T.C: O(sqrt(n))
 // S.C: O(1)
 bool checkPrime(int n) {
-    if(n<=1) return false;   // numbers less than equal 1 are non-prime
+    if(n <= 1) return false;   // numbers less than equal 1 are non-prime
 
     // a non-prime number will always have a factor between 2 and sqrt(n)
-    for(int i=2; i<=sqrt(n); i++) {
-        if(n%i==0) return false;   // found a factor, thus return false
+    for(int i = 2; i <= sqrt(n); i++) {
+        if(n % i == 0) return false;   // found a factor, thus return false
     }
     
     // If program comes here, it means no factors are found. Thus, it is a prime number. Hence, return true.
