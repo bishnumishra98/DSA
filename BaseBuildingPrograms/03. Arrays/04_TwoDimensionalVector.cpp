9d0698f3f6f37b2f7ce21a28a940cbe63082623b
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-29 19:35:39 +0530

    nm

--- BaseBuildingPrograms/03. Arrays/04_TwoDimensionalVector.cpp
+++ BaseBuildingPrograms/03. Arrays/04_TwoDimensionalVector.cpp
@@ -19,7 +19,7 @@ int main() {
         vector <int> v;
 
         cout << "Enter the elements one by one:\n";
-        for(int j = 0; j<col; j++) {
+        for(int j = 0; j < col; j++) {
             int element;
             cin >> element;
             v.push_back(element);
@@ -30,8 +30,8 @@ int main() {
     }
 
     // Printing the vector
-    for(int i = 0; i<matrix.size(); i++) {   // matrix.size() returns the number of rows in the 2D vector matrix
-        for (int j = 0; j<matrix[i].size(); j++) {   // matrix[i].size() returns the number of elements in the i-th row of the 2D vector 
+    for(int i = 0; i < matrix.size(); i++) {   // matrix.size() returns the number of rows in the 2D vector matrix
+        for (int j = 0; j < matrix[i].size(); j++) {   // matrix[i].size() returns the number of elements in the i-th row of the 2D vector 
             cout << matrix[i][j] << " ";
         }
         cout << endl;
