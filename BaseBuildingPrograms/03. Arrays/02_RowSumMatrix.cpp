52163372246724de6d922442e728d0134981a639
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-29 19:33:41 +0530

    nm

--- BaseBuildingPrograms/03. Arrays/02_RowSumMatrix.cpp
+++ BaseBuildingPrograms/03. Arrays/02_RowSumMatrix.cpp
@@ -14,9 +14,9 @@ int main() {
 
     int row = sizeof(arr) / sizeof(arr[0]);
 
-    for(int i=0; i<row; i++) {
+    for(int i = 0; i < row; i++) {
         int sum = 0;
-        for(int j=0; j<col; j++) {
+        for(int j = 0; j < col; j++) {
             sum += arr[i][j];
         }
         cout << sum << endl;
