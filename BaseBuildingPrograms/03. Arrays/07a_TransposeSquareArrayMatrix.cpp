ca6190675261c7a66ff2664169a91b93f3e5c9a6
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-30 19:42:22 +0530

    nm

--- BaseBuildingPrograms/03. Arrays/07a_TransposeSquareArrayMatrix.cpp
+++ BaseBuildingPrograms/03. Arrays/07a_TransposeSquareArrayMatrix.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 // Function to print a Square array
 void printSquareArray(int arr[][3], int row, int col) {
-    for(int i=0; i<row; i++) {
-        for(int j=0; j<col; j++) {
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
             cout << arr[i][j] << " ";
         }
         cout << endl;
