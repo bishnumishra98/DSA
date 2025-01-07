a07bbe2206a35539e9d5990e3cbc09aa608e6a9a
Bishnu Mishra <mishravishnu369@gmail.com>
2025-01-07 22:06:47 +0530

    nm

--- Leetcode&Gfg/01. Arrays/CommonIn3SortedArrays.cpp
+++ Leetcode&Gfg/01. Arrays/CommonIn3SortedArrays.cpp
@@ -24,18 +24,21 @@ using namespace std;
 class Solution {
 public:
     // T.C: O(n1^n2^n3);   where n1, n2 and n3 are size of arr1, arr2 and arr3 respectively.
-    // S.C: O(n)
+    // S.C: O(2n)
     vector<int> commonElements_bruteforce(vector<int> &arr1, vector<int> &arr2, vector<int> &arr3) {
-        vector<int> commonElements;
+        set<int> st;   // set data structure is used to ensure we don't store any duplicate common elements
 
         for(int i = 0; i < arr1.size(); i++) {
             for(int j = 0; j < arr2.size(); j++) {
                 for(int k = 0; k < arr3.size(); k++) {
-                    if(arr1[i] == arr2[j] && arr2[j] == arr3[k]) commonElements.push_back(arr1[i]);
+                    if(arr1[i] == arr2[j] && arr2[j] == arr3[k]) st.insert(arr1[i]);
                 }
             }
         }
 
+        // vector<int> commonElements;
+        // for(int it: st) commonElements.push_back(it);
+        vector<int> commonElements(st.begin(), st.end());
         return commonElements;
     }
 
@@ -57,7 +60,6 @@ public:
     // T.C: O(n1+n2+n3);   where n1, n2 and n3 are size of arr1, arr2 and arr3 respectively.
     // S.C: O(2n)
     vector<int> commonElements_timeOptimised(vector<int> &arr1, vector<int> &arr2, vector<int> &arr3) {
-        vector<int> commonElements;
         set<int> st;   // set data structure is used to ensure we don't store any duplicate common elements
         int i = 0, j = 0, k = 0;
 
@@ -73,7 +75,7 @@ public:
             else k++;   // if element pointed by k is smaller, then move k
         }
 
-        for(int it: st) commonElements.push_back(it);
+        vector<int> commonElements(st.begin(), st.end());
         return commonElements;
     }
 
@@ -105,7 +107,7 @@ public:
 
 
 int main() {
-    vector<int> arr1 = {1, 5, 10, 20, 40, 80} , arr2 = {6, 7, 20, 80, 100}, arr3 = {3, 4, 15, 20, 30, 70, 80, 120};
+    vector<int> arr1 = {1, 1, 1, 2, 2, 2} , arr2 = {1, 1, 2, 2, 2}, arr3 = {1, 1, 1, 1, 2, 2, 2, 2};
     vector<int> ans1 = Solution().commonElements_bruteforce(arr1, arr2, arr3);
     for(auto it: ans1) cout << it << " ";
     cout << endl;
