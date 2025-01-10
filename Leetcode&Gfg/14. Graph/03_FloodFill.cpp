00954753294fa3632de122c19f45e10b6d0190bb
Bishnu Mishra <mishravishnu369@gmail.com>
2025-01-10 19:07:04 +0530

    nm

--- Leetcode&Gfg/14. Graph/03_FloodFill.cpp
+++ Leetcode&Gfg/14. Graph/03_FloodFill.cpp
@@ -1,4 +1,4 @@
-// Leetcode: 733. Flood fill   --->   An image is represented by an m x n integer grid image where image{i][j]
+// Leetcode: 733. Flood fill   --->   An image is represented by an m x n integer grid image where image[i][j]
 // represents the pixel value of the image.
 // You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting
 // from the pixel image[sr][sc].
