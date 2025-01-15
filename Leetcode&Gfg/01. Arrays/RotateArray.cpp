07ad288b655267d89e978dc9c705006e174cdc6b
Bishnu Mishra <mishravishnu369@gmail.com>
2025-01-16 01:04:12 +0530

    nm

--- Leetcode&Gfg/01. Arrays/RotateArray.cpp
+++ Leetcode&Gfg/01. Arrays/RotateArray.cpp
@@ -30,7 +30,7 @@ public:
         vector<int> temp(n);
 
         for(int i = 0; i < n; i++) {
-            temp[(i + k) % n]= nums[i];
+            temp[(i + k) % n] = nums[i];
         }
 
         nums = temp;
