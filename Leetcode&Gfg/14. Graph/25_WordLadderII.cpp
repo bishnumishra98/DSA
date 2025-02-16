88732b67219495f11b92b344777879db8ae02aaf
Bishnu Mishra <mishravishnu369@gmail.com>
2025-02-16 14:48:05 +0530

    graph

--- Leetcode&Gfg/14. Graph/25_WordLadderII.cpp
+++ Leetcode&Gfg/14. Graph/25_WordLadderII.cpp
@@ -26,6 +26,10 @@
 // from startWord to targetWord.
 // Note: You don't have to return -1 in case of no possible sequence. Just return the Empty List.
 
+// Algorithm: The algorithm might look complex, but actually it is not. We shall follow the brute force approach. It is just an
+//            extension of the previous problem. The only difference is that we have to return all the valid words that come in the
+//            path while building the targetWord from the startWord.
+
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -35,7 +39,54 @@ public:
     // T.C: 
     // S.C: 
     vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList) {
-        
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        queue<vector<string>> q;
+        q.push({beginWord});
+        vector<string>usedOnLevel;
+        usedOnLevel.push_back(beginWord);
+        int level = 0;
+        vector<vector<string>> ans;
+
+        while(!q.empty()) {
+            vector<string> vec = q.front();
+            q.pop();
+            
+            // Erase all words that are already used on previous level
+            if(vec.size() > level) {
+                level++;
+                for(auto it: usedOnLevel) {
+                    st.erase(it);
+                }
+            }
+
+            string word = vec.back();
+
+            if(word == endWord) {
+                if(ans.size() == 0) {   // if the ans is empty, then push the first word sequence in the ans
+                    ans.push_back(vec);
+                } else if(ans[0].size() == vec.size()) {
+                    ans.push_back(vec);
+                } else {
+                    break;
+                }
+            }
+
+            for(int i = 0; i < word.size(); i++) {
+                char letter = word[i];
+                for(char c = 'a'; c <= 'z'; c++) {
+                    word[i] = c;
+                    if(st.find(word) != st.end()) {
+                        vec.push_back(word);
+                        q.push(vec);
+                        usedOnLevel.push_back(word);   // Store the word that is used on this level
+                        vec.pop_back();
+                    }
+                }
+                word[i] = letter;
+            }
+        }
+
+        return ans;
     }
 };
 
