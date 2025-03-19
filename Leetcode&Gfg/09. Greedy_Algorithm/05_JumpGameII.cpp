c5ce6dd303ca6946d7dc9c399b77a4d64d468651
Bishnu Mishra <mishravishnu369@gmail.com>
2025-03-19 23:27:00 +0530

    greedy

--- Leetcode&Gfg/09. Greedy_Algorithm/05_JumpGameII.cpp
+++ Leetcode&Gfg/09. Greedy_Algorithm/05_JumpGameII.cpp
@@ -9,24 +9,53 @@
 // Example 1:
 // Input: nums = [2,3,1,1,4]
 // Output: 2
-// Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the
+//              last index.
 
 // Example 2:
 // Input: nums = [2,3,0,1,4]
 // Output: 2
+
+// Example 3:
+// Input: nums = [1, 2, 3, 1, 2, 0, 2, 5]
+// Output: 5
+
+// Algorithm: This is a greedy approach.
+// 1. We will keep track of the current end index 'currEnd' and the current farthest index 'currFarthest' we can reach.
+//    Initialize jumps, currEnd, and currFarthest to 0. 'currEnd' represents the end of the current jump range, and
+//    'currFarthest' is the farthest index we can reach from the current jump range.
+// 2. Traverse the array from i = 0 to n-1, and for each index i, update the currFarthest with the maximum of currFarthest
+//    and i + nums[i]. This will help us to keep track of the maximum index we can reach from the current jump range.
+//    If i == currEnd, it means the current jump range is exhausted, and we need to jump to the next range. Thus, increment
+//    jumps and update currEnd with currFarthest.
+// 3. Finally, return the number of jumps.
 
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
+    // T.C: O(n);   where n = nums.size()
+    // S.C: O(1)
     int jump(vector<int>& nums) {
-        
+        int n = nums.size();
+        if(n == 1) return 0;
+
+        int jumps = 0, currEnd = 0, currFarthest = 0;
+        for(int i = 0; i < n - 1; i++) {
+            currFarthest = max(currFarthest, i + nums[i]);
+            if(i == currEnd) {
+                jumps++;
+                currEnd = currFarthest;
+            }
+        }
+
+        return jumps;
     }
 };
 
 int main() {
-    vector<int> nums = {2, 3, 1, 1, 4};
+    vector<int> nums = {1, 2, 3, 1, 2, 0, 2, 5};
 
     Solution obj;
     cout << obj.jump(nums);
