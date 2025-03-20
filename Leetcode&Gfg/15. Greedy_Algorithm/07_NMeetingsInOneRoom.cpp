b42181dbdb76876d885df9cb78e9bd92ea987a82
Bishnu Mishra <mishravishnu369@gmail.com>
2025-03-20 20:48:38 +0530

    greedy

--- Leetcode&Gfg/15. Greedy_Algorithm/07_NMeetingsInOneRoom.cpp
+++ Leetcode&Gfg/15. Greedy_Algorithm/07_NMeetingsInOneRoom.cpp
@@ -22,14 +22,14 @@
 // Algorithm: This is a greedy approach. We want to maximize the number of meetings that can be accommodated in a
 // single meeting room. Thus we will try to accommodate the meetings with the earliest end time first. Keeping this
 // point in mind, the algorithm is as follows:
-// 1. Create a vector of {end, {start, id}} to store the end time, start time, and id of each meeting.
+// 1. Create a vector of {end, start} to store the end and start time of each meeting.
 //    Sort the meetings in increasing order of end time, i.e., meetings with earlier end time will be considered first.
 // 2. Initialize two variables 'maxMeetings' and 'prevEnd' to keep track of the maximum number of meetings that can be
-//    accommodated in a single meeting room and the end time of the last meeting that was accommodated.
+//    accommodated and the end time of the previous meeting. '-1' means no meeting is scheduled yet.
 // 3. Iterate through all the meetings and check if the start time of the current meeting is greater than the end time of
-//    the last meeting that was accommodated. If yes, then accommodate the current meeting and update the maximum number of
-//    meetings and the end time of the last meeting that was accommodated.
-// 4. Return the maximum number of meetings that can be accommodated in a single meeting room.
+//    the previous meeting. If yes, then increment the 'maxMeetings' and update the 'prevEnd' to the end time of the current
+//    meeting.
+// 4. Return the maximum number of meetings that can be accommodated.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -62,6 +62,37 @@ public:
         
         return maxMeetings;
     }
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+    // An extension to this problem can be to return sequence of maximum meetings that can be accommodated in a
+    // single meeting room. In this case, job id shall be provided for each job, if not then we can assign job id
+    // as 1, 2, 3, ..., n. The algorithm will remain the same as above, just we need to store the job id of the
+    // meetings that are scheduled in the meeting room. The code snippet for the same is as follows:
+    vector<int> selectedMeetings(vector<int>& start, vector<int>& end) {
+        int n = start.size();
+        if (n == 0) return {};   // handle empty input case
+
+        vector<pair<int, pair<int, int>>> meetings(n);   // {end, {start, id}}
+        for (int i = 0; i < n; i++) {
+            meetings[i] = {end[i], {start[i], i + 1}};   // include original meeting ID (1-based index)
+        }
+
+        // Sort the meetings in increasing order of end time, i.e., meetings with earlier end time will be considered first.
+        sort(meetings.begin(), meetings.end());
+
+        int prevEnd = -1;
+        vector<int> result;
+
+        for (int i = 0; i < n; i++) {
+            if (meetings[i].second.first > prevEnd) {
+                prevEnd = meetings[i].first;
+                result.push_back(meetings[i].second.second);
+            }
+        }
+
+        return result;
+    }
 };
 
 int main() {
@@ -69,7 +100,12 @@ int main() {
     vector<int> end = {2, 4, 6, 7, 9, 9};
 
     Solution obj;
-    cout << obj.maxMeetings(start, end);
+    cout << obj.maxMeetings(start, end) << endl;
+
+    vector<int> ans = obj.selectedMeetings(start, end);
+    for(int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
 
     return 0;
 }
