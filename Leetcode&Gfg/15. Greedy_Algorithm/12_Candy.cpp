021f4dc0822e5dc4549ee4220d728886f1dd8b41
Bishnu Mishra <mishravishnu369@gmail.com>
2025-03-24 19:33:40 +0530

    candy greedy

--- Leetcode&Gfg/15. Greedy_Algorithm/12_Candy.cpp
+++ Leetcode&Gfg/15. Greedy_Algorithm/12_Candy.cpp
@@ -16,7 +16,12 @@
 // Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
 // The third child gets 1 candy because it satisfies the above two conditions.
 
-// Brute force algorithm: Extremely simple and easy to understand.
+// Example 3:
+// Input: ratings = [0,2,4,7,6,5,4,3,2,1,1,1,2,3,4,2,1,1,1]
+// Output: 50
+// Explanation: The candy allocation will be: [1,2,,3,7,6,5,4,3,2,1,1,1,2,3,4,2,1,1,1]
+
+// Brute force algorithm: Extremely simple and easy.
 // 1. Create two arrays left and right of size n.
 // 2. left[i] stores the number of candies given to the ith child considering only the left neighbour.
 // 3. right[i] stores the number of candies given to the ith child considering only the right neighbour.
@@ -59,11 +64,67 @@ public:
 
         return sum;
     }
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+    // Optimised algorithm: This is based on a slope approach. Imagine the ratings array as a graph, where the ratings are the
+    // y-coordinates and the children are the x-coordinates. We can see that the ratings array can be divided into several
+    // ascending or descending slopes. The children in the ascending slope must get 1 more candy than the previous child,
+    // and the children in the descending slope must get 1 less candy than the previous child.
+    // But the problem is while iterating the ratings array, we don't know how many children are there in the descending slope,
+    // so we can't just easily assign 1 candy less to the children in the descending slope, otherwise if the descending slope
+    // is longer than ascending slope, some children will get negative candies in the descending slope which is invalid.
+    // So, we assign candies in the increasing order to the children in the ascending slope and as well as in the descending slope,
+    // as we are not interested in the distribution of candies, we are just interested in the sum of candies.
+    // Side by side, we will also keep a track of length of ascending slope and descending slope, and if the descending slope is
+    // greater than the ascending slope, then we need to add the extra candies to the sum which is nothing but difference
+    // between length of ascending slope and descending slope.
+    // For better understanding, go through the image diagram in the same folder.
+
+    // T.C: O(n)
+    //S.C: O(1)
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int sum = 1, i = 1;
+
+        while(i < n) {
+            // If its a straight line: We can assign 1 candy to all the children.
+            while(i < n && ratings[i] == ratings[i - 1]) {
+                sum++;
+                i++;
+            }
+
+            // If its a ascending slope: We can assign candies in the increasing order to each children.
+            int peak = 1;
+            while(i < n && ratings[i] > ratings[i - 1]) {
+                peak++;   // if ratings[i] > ratings[i - 1], it means the previous child would have got 1 candy, so the current child,
+                         // i.e., ith child should get 1 more candy than the previous child. That's why peak++ first, them add it to sum.
+                sum += peak;
+                i++;
+            }
+
+            // If its a descending slope: We can assign candies in the decreasing order to each children.
+            int down = 1;
+            while(i < n && ratings[i] < ratings[i - 1]) {
+                sum += down;
+                down++;
+                i++;
+            }
+            
+            // If the descending slope is greater than the peak, then we need to add the extra candies to the sum.
+            if(down > peak) sum += down - peak;
+        }
+
+        return sum;
+    }
 };
 
 int main() {
+    vector<int> ratings = {0, 2, 4, 7, 6, 5, 4, 3, 2, 1, 1, 1, 2, 3, 4, 2, 1, 1, 1};
+
     Solution sol;
-    vector<int> ratings = {1, 0, 2};
-    cout << sol.candy_bruteforce(ratings);
+    cout << sol.candy_bruteforce(ratings) << endl;
+    cout << sol.candy(ratings);
+
     return 0;
 }
