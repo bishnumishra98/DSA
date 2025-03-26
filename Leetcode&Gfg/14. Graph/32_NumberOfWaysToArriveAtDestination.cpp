4cf225e60051ec20bf98092227a38fd3ba306fea
Bishnu Mishra <mishravishnu369@gmail.com>
2025-03-27 01:21:44 +0530

    graph

--- Leetcode&Gfg/14. Graph/32_NumberOfWaysToArriveAtDestination.cpp
+++ Leetcode&Gfg/14. Graph/32_NumberOfWaysToArriveAtDestination.cpp
@@ -23,6 +23,17 @@
 // Explanation: There is only one way to go from intersection 0 to intersection 1, and it takes 10 minutes.
 
 // Algorithm: Simple Dijkstra's Algorithm
+// Note: Number of ways to reach current node = sum of number of ways to reach the nodes from where the current node came.
+//       For example: node1 --- node3 --- node5
+//                      |  \               |   \
+//                      |    \             |   node7 
+//                      |      \           |   /
+//                    node2 --- node4 --- node6
+//       In the above undirected graph, the number of ways to reach:
+//       • ways[node7] = ways[node6] + ways[node5]
+//       • ways[node6] = ways[node5] + ways[node4]
+//       • ways[node5] = ways[node6] + ways[node3]
+
 // 1. Create an adjacency list of the graph.
 // 2. Create a priority queue to store the distance and node pair. Initialize the distance and ways vectors to track the
 //    least distance and the number of ways to reach a node.
@@ -30,10 +41,15 @@
 // 4. Run a while loop until the priority queue is empty.
 //    i.   Pop the top element from the priority queue. Extract the distance and node from the top element.
 //    ii.  Traverse the adjacency list of the current node. For each neighbour, check if the distance from the current node
-//         to the neighbour plus the distance of the current node is less than the distance of the neighbour. If yes, update
-//         the distance of the neighbour and the number of ways to reach the neighbour.
-//    iii. If the distance of the neighbour is equal to the distance of the current node plus the distance from the current
-//         node to the neighbour, then update the number of ways to reach the neighbour.
+//         to the neighbour plus the distance of the current node is less than the distance of the neighbour. If yes, it means
+//         we have first time visited the neighbour. Update the distance of the neighbour, the number of ways to reach the
+//         neighbour and push the neighbour into the priority queue with the updated distance. The number of ways to reach
+//         the neighbour will be the same as the number of ways to reach the current node as we have arrived at the neighbour
+//         for the first time.
+//    iii. Else if the distance of the neighbour is equal to the distance of the current node plus the distance from the current
+//         node to the neighbour, it means we have arrived at the neighbour with the same shortest distance again. In this case,
+//         update the number of ways to reach the neighbour as the sum of the number of ways to reach the neighbour and the number
+//         of ways to reach the current node. Modulo the number of ways to reach the neighbour by 10^9 + 7.
 // 5. Return the number of ways to reach the destination node.
 
 
@@ -57,8 +73,8 @@ public:
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;   // {distance, node}
         vector<int> dist(n, 1e9), ways(n, 0);   // distance, ways vectors to track least distance and no.of ways to reach a node
         pq.push({0, start});
-        dist[0] = 0;
-        ways[0] = 1;
+        dist[start] = 0;
+        ways[start] = 1;
 
         int mod = (int)(1e9 + 7);
 
@@ -71,11 +87,11 @@ public:
                 int adjNode = it.first;
                 int adjDis = it.second;
                 
-                if(dis + adjDis < dist[adjNode]) {   // this is the first time we are visiting this node
+                if(dis + adjDis < dist[adjNode]) {   // if a shorter distance found to reach 'adjNode', via 'node'
                     dist[adjNode] = dis + adjDis;
                     ways[adjNode] = ways[node];
                     pq.push({dis + adjDis, adjNode});
-                } else if(dis + adjDis == dist[adjNode]) {   // we have already visited this node
+                } else if(dis + adjDis == dist[adjNode]) {   // we have arrived at adjNode with the same shortest distance again
                     ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
                 }
             }
