bc3c6a7c5cc7dc7af0e804d92c746c07ff7e66c3
Bishnu Mishra <mishravishnu369@gmail.com>
2025-04-03 19:21:20 +0530

    graph

--- Leetcode&Gfg/14. Graph/38_NumberOfOperationsToMakeNetworkConnected.cpp
+++ Leetcode&Gfg/14. Graph/38_NumberOfOperationsToMakeNetworkConnected.cpp
@@ -1,4 +1,4 @@
-// Leetcode: 1319: Number of Operations to Make Network Connected   --->   There are n computers numbered from 0 to n - 1
+// Leetcode: 1319. Number of Operations to Make Network Connected   --->   There are n computers numbered from 0 to n - 1
 // connected by ethernet cables connections forming a network where connections[i] = [ai, bi] represents a connection
 // between computers ai and bi. Any computer can reach any other computer directly or indirectly through the network.
 // You are given an initial computer network connections. You can extract certain cables between two directly connected
