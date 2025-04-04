1d5f532cb970e806a37c42727d5057c8f1d2d9c3
Bishnu Mishra <mishravishnu369@gmail.com>
2025-04-04 05:52:53 +0530

    graph

--- Leetcode&Gfg/14. Graph/39_AccountsMerge.cpp
+++ Leetcode&Gfg/14. Graph/39_AccountsMerge.cpp
@@ -32,7 +32,38 @@
 //  ["Hanzo","Hanzo0@m.co","Hanzo1@m.co","Hanzo3@m.co"],["Kevin","Kevin0@m.co","Kevin3@m.co","Kevin5@m.co"],
 //  ["Fern","Fern0@m.co","Fern1@m.co","Fern5@m.co"]]
 
-// Algorithm: 
+// Intuition and Approach:
+// Consider the vector<vector<string>> accounts as a graph where each account is a node. Thus the graph consists of nodes
+// from 0 to n - 1, where n is the number of accounts, i.e., accounts.size().
+// Now if two accounts share a common email, then they should be connected to each other under a single parent node. Thus, we can
+// use a disjoint set to group all the connected components of the graph. The disjoint set will help us find the parent of each
+// account and merge the accounts that share a common email.
+
+// Algorithm:
+// 1. Create a disjoint set for all accounts in the graph.
+// 2. Create a map with {string, int} as key value pair, to store the first occurrence of each email in the accounts vector.
+//    The key will be the email and the value will be the index of the account in which the email first occurs. Now, if the
+//    same email occurs in multiple accounts, we can group all such accounts under the same parent node(the node where the
+//    email first occurred), with the help of unionByRank/unionBySize function of the disjoint set.
+// 3. Now, group all the emails under their respective ultimate parent nodes in a vector<vector<string>> mergedMail.
+//    Each index of the mergedMail will contain the list of grouped emails that should come under their respective parent node.
+//    The index of the mergedMail where the list is empty, means that account is attached to a different parent node.
+//    For example, for the Example 1,
+//    accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],
+//                ["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+//    the mergedMail will look like this:
+//    {john_newyork@mail.com, johnsmith@mail.com, john00@mail.com} 
+//    {}
+//    {mary@mail.com} 
+//    {johnnybravo@mail.com}
+// 4. Finally, format the output in the required format and return it. The required format is that the first element of each
+//    account is the name of the account holder and the rest of the elements are the emails of that account holder in sorted order.
+//    i.   Thus, we can create a vector<vector<string>> ans to store the final output.
+//    ii.  Traverse the mergedMail vector and for each index, if the list is empty, skip the iteration and move to next iteration.
+//    iii. Sort the emails in the list in lexicographical order and create a temporary vector<string> temp to store the name of the
+//         account holder and the emails of that account holder.
+//    iv.  Push this temporary vector into the 'ans' and finally return the 'ans'.
+
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -73,8 +104,8 @@ public:
 
 class Solution {
 public:
-    // T.C: 
-    // S.C: 
+    // T.C: O(n * m * log(m));   where n is the number of accounts and m is the maximum number of emails in an account
+    // S.C: O(n + m)   for the disjoint set and the mapMailNode
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
         int n = accounts.size();   // number of accounts
         DisjointSet ds(n);
@@ -92,7 +123,7 @@ public:
             }
         }
 
-        // Step 2: Group emails by their parent representative in the disjoint set
+        // Step 2: Group emails under their respective ultimate parent nodes
         vector<vector<string>> mergedMail(n);   // stores grouped emails
         for(auto it: mapMailNode) {
             string mail = it.first;
@@ -103,14 +134,14 @@ public:
         // Step 3: Format the final output and return
         vector<vector<string>> ans;
         for(int i = 0; i < n; i++) {
-            if(mergedMail[i].size() == 0) continue;
-            sort(mergedMail[i].begin(), mergedMail[i].end());
+            if(mergedMail[i].size() == 0) continue;   // skip empty lists
+            sort(mergedMail[i].begin(), mergedMail[i].end());   // sort the emails in lexicographical order
             vector<string> temp;
-            temp.push_back(accounts[i][0]);
-            for(auto it: mergedMail[i]) {
+            temp.push_back(accounts[i][0]);   // add the name of the account
+            for(auto it: mergedMail[i]) {   // add the emails of the account
                 temp.push_back(it);
             }
-            ans.push_back(temp);
+            ans.push_back(temp);   // add the account to the final answer
         }
 
         return ans;
