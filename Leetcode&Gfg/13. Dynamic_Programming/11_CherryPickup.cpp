25d97814990bccb5634f112972b1901447cea1fe
Bishnu Mishra <mishravishnu369@gmail.com>
2025-05-27 22:54:51 +0530

    dp

---
+++ Leetcode&Gfg/13. Dynamic_Programming/11_CherryPickup.cpp
@@ -0,0 +1,61 @@
+// Leetcode: 741. Cherry Pickup   --->   You are given an n x n grid representing a field of cherries, each cell is one
+// of three possible integers.
+// • 0 means the cell is empty, so you can pass through,
+// • 1 means the cell contains a cherry that you can pick up and pass through, or
+// • -1 means the cell contains a thorn that blocks your way.
+// Return the maximum number of cherries you can collect by following the rules below:
+// • Starting at the position (0, 0) and reaching (n - 1, n - 1) by moving right or down through valid path cells
+//   (cells with value 0 or 1).
+// • After reaching (n - 1, n - 1), returning to (0, 0) by moving left or up through valid path cells.
+// • When passing through a path cell containing a cherry, you pick it up, and the cell becomes an empty cell 0.
+// • If there is no valid path between (0, 0) and (n - 1, n - 1), then no cherries can be collected.
+
+// Example 1:
+// Input: grid = [[0,1,-1],[1,0,-1],[1,1,1]]
+// Output: 5
+// Explanation: The player started at (0, 0) and went down, down, right right to reach (2, 2).
+// 4 cherries were picked up during this single trip, and the matrix becomes [[0,1,-1],[0,0,-1],[0,0,0]].
+// Then, the player went left, up, up, left to return home, picking up one more cherry.
+// The total number of cherries picked up is 5, and this is the maximum possible.
+
+// Example 2:
+// Input: grid = [[1,1,-1],[1,-1,1],[-1,1,1]]
+// Output: 0
+
+// Algorithm: Extremely simple problem, it is nothing but same problem of Unique Paths II with some modifications.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int solve_going(int i, int j, vector<vector<int>>& grid) {
+        // Movement: up or left
+    }
+
+    int solve_returning(int i, int j, vector<vector<int>>& grid) {
+        // Movement: down or right
+    }
+
+    // T.C: 
+    // S.C: 
+    int cherryPickup_memoization(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int whileGoing = solve_going(n - 1, n - 1, grid);
+        int whileReturning = solve_returning(0, 0, grid);
+        return whileGoing + whileReturning;
+    }
+
+// ---------------------------------------------------------------------------------------
+};
+
+int main() {
+    vector<vector<int>> grid = {{0, 1, -1},
+                                {1, 0, 1},
+                                {-1, 1, 0}};
+
+    Solution sol;
+    cout << sol.cherryPickup_memoization(grid) << endl;
+
+    return 0;
+}
