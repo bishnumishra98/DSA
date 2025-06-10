38f05b2b3d8733c4dde884a7c8381b208d99d4b4
Bishnu Mishra <mishravishnu369@gmail.com>
2025-06-11 00:28:34 +0530

    dp

--- Leetcode&Gfg/13. Dynamic_Programming/20_PaintingTheFence.cpp
+++ Leetcode&Gfg/13. Dynamic_Programming/20_PaintingTheFence.cpp
@@ -137,17 +137,20 @@ int solve(int n, int k, vector<int>& dp) {
         if(n == 1) return prev2;
         if(n == 2) return prev2;
 
-        int curr = 0;
         for(int i = 3; i <= n; i++) {
             int ways1 = prev1 * (k - 1);
             int ways2 = prev2 * (k - 1);
+
+            int curr = ways1 + ways2;
+
             prev2 = prev1;
             prev1 = curr;
         }
 
-        return prev1;   // or we can even return curr as both stands on same value after the bove loop
+        return prev1;   // or we can even return curr as both stands on same value after the above loop
     }
 };
+
 
 int main() {
     int n = 3, k = 2;
@@ -159,83 +162,3 @@ int main() {
 
     return 0;
 }
-
-
-/*
-// T.C: O(2^n)
-// S.C: O(n)
-int getTotalWaysOfPainting_recursion(int n, int k) {
-    // base case
-    if(n == 1) return k;
-    if(n == 2) return k + k*(k-1);
-    
-    // recursive solution
-    return (k-1) * (getTotalWaysOfPainting_recursion(n-2, k) + getTotalWaysOfPainting_recursion(n-1, k));
-}
-
-// T.C: O(n);   Since each value from 1 to n is computed only once
-// S.C: O(n);   The dp array of size n+1 is used to store the computed results for each subproblem, requiring O(n) space.
-int getTotalWaysOfPainting_memoization(int n, int k, vector<int>& dp) {
-    // base case
-    if(n == 1) return k;
-    if(n == 2) return k + k*(k-1);
-
-    if(dp[n] != -1) return dp[n];
-    
-    dp[n] = (k-1) * (getTotalWaysOfPainting_memoization(n-2, k, dp) + getTotalWaysOfPainting_memoization(n-1, k, dp));
-    return dp[n];
-}
-
-// T.C: O(n)
-// S.C: O(n)
-int getTotalWaysOfPainting_tabulation(int n, int k) {
-    vector<int> dp(n+1, -1);
-
-    dp[1] = k;
-    dp[2] = k + k*(k-1);
-
-    for(int i=3; i<=n; i++) {
-        dp[i] = (k-1) * (dp[i-2] + dp[i-1]);
-    }
-
-    return dp[n];
-}
-
-// dp[i] depends on only 2 variables dp[i-2] and dp[i-1]. Thus, we can space optimize it further.
-// T.C: O(n)
-// S.C: O(1)
-int getTotalWaysOfPainting_tabulation_SO(int n, int k) {
-    int prev2 = k;
-    int prev1 = k + k*(k-1);
-
-    // base case
-    if(n==1) return prev2;
-    if(n==2) return prev1;
-
-    int curr;
-    for(int i=3; i<=n; i++) {
-        curr = (k-1) * (prev2 + prev1);   // curr represents dp[i], prev2 represents dp[i-2] and prev1 represents dp[i-1], respectively.
-        // update prev2 and prev1
-        prev2 = prev1;
-        prev1 = curr;
-    }
-
-    return curr;   // or we can even return prev1 as both stands on same value after the bove loop
-}
-
-int main() {
-    int n = 5;
-    int k = 3;
-
-    cout << "Recursion: " << getTotalWaysOfPainting_recursion(n, k) << endl;
-
-    vector<int> dp(n+1, -1);
-    cout << "Memoization: " << getTotalWaysOfPainting_memoization(n, k, dp) << endl;
-
-    cout << "Tabulation: " << getTotalWaysOfPainting_tabulation(n, k) << endl;
-
-    cout << "Tabulation_SO: " << getTotalWaysOfPainting_tabulation_SO(n, k) << endl;
-
-    return 0;
-}
-*/
