b2a615eeed92dde2dded5c916f2937bf19dcb530
Bishnu Mishra <mishravishnu369@gmail.com>
2025-06-19 01:03:08 +0530

    dp

--- Leetcode&Gfg/13. Dynamic_Programming/26_PrintAllLCSSequences.cpp
+++ Leetcode&Gfg/13. Dynamic_Programming/26_PrintAllLCSSequences.cpp
@@ -22,12 +22,85 @@
 //                    For this we will require the tabulation table of the previous problem
 //                    'Leetcode&Gfg\13. Dynamic_Programming\25_LongestCommonSubsequence.cpp'.
 //                    The dp table for the input text1 = "abcde", text2 = "ace" would look like this:
-// 0 0 0 0
-// 0 1 1 1
-// 0 1 1 1
-// 0 1 2 2
-// 0 1 2 2
-// 0 1 2 3
+//                                   a c e
+// Indexes of dp table(col):       0 1 2 3
+//                                --------
+// Indexes of dp table(row):   0 | 0 0 0 0
+//                           a 1 | 0 1 1 1
+//                           b 2 | 0 1 1 1
+//                           c 3 | 0 1 2 2
+//                           d 4 | 0 1 2 2
+//                           e 5 | 0 1 2 3
+//                     As the memoization was right shifted for tabulation, dp[i][0] and dp[0][j] do not have any
+//                     meaning, that's why initialzied to 0. The rest of the table tells the length of LCS.
+//                     Lets take dp[4][2] = 2. It means the length of longest common subsequence (LCS) between
+//                     s1[1 to 4] and s2[1 to 2], i.e., between "abcd" and "ac" is 2.
+//                     A cell(i, j) of the dp table gets filled in either of the two ways:
+//                     ● if(s1[i - 1] == s2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1]
+//                     ● else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+//                     In tabulation code, i and j both started from 1 and ended at 'm' and 'n' respectively, where
+//                     m = s1.length() and n = s2.length(). Thus, if s1[i] was compared s2[j], dp[i + 1][j + 1] was
+//                     filled as their output. For example: if s1[0] and s2[0] was compared, its output was filled
+//                     in dp[1][1]. If s1[4] and s2[2] was compared, its output was filled in dp[5][3].
+//                     Initialize two pointers 'i' and 'j' at index 'm' and 'n', and check from where dp[i][j] was
+//                     being filled. It was filled from either of the two ways.
+//                     Case I.  if(s1[i - 1] == s2[j - 1]): If s1[i - 1] is equal to s2[j - 1], then it was filled
+//                              by adding 1 to the cell at previous row & previous column. 
+//                     Case II. if(s1[i - 1] != s2[j - 1]): If s1[i - 1] is not equal to s2[j - 1], then it was filled
+//                              from max(cell at previous row & same column, cell at same row & previous column).
+//                     For illustration, lets start from the index 'm' and 'n'.
+//                             s2    a c e
+// Indexes of dp table(col):       0 1 2 3
+//                           s1   --------
+// Indexes of dp table(row):   0 | 0 0 0 0
+//                           a 1 | 0 1 1 1
+//                           b 2 | 0 1 1 1
+//                           c 3 | 0 1 2 2
+//                           d 4 | 0 1 2 2
+//                           e 5 | 0 1 2 3
+//                     ● We are at dp[5][3]. From where was it filled ? Compare s1[4] and s2[2].
+//                       As s1[4] = 'e' and s2[2] = 'e' matches, the value in dp[5][3] came from '1 + dp[4][2]'.
+//                     ● Now, we are at dp[4][2]. From where was it filled ? Compare s1[3] and s2[1].
+//                       As s1[3] = 'd' and s2[1] = 'c' does not matches, the value in dp[4][2] came from
+//                       max of dp[3][2] i.e., 2 and dp[4][1] i.e., 1 which is 2.
+//                     ● Now, we are at dp[3][2]. From where was it filled ? Compare s1[2] and s2[1].
+//                       As s1[2] = 'c' and s2[1] = 'c' matches, the value in dp[3][2] came from '1 + dp[2][1]'.
+//                     ● Now, we are at dp[2][1]. From where was it filled ? Compare s1[1] and s2[0].
+//                       As s1[1] = 'b' and s2[0] = 'a' does not matches, the value in dp[2][1] came from
+//                       max of dp[1][1] i.e., 1 and dp[2][0] i.e., 0 which is 1.
+//                     ● Now, we are at dp[1][1]. From where was it filled ? Compare s1[0] and s2[0].
+//                       As s1[0] = 'a' and s2[0] = 'a' matches, the value in dp[1][1] came from '1 + dp[0][0]'.
+//                     ● Now we reach dp[0][0]. The point when either 'i' or 'j' reaches 0, we have to stop as
+//                       dp[i][0] or dp[0][j] does have any meaning as we have already exhausted one of the strings.
+//                     The pattern to observe here is:
+//                     dp[0][0] (stop)
+//                          \
+//                        dp[1][1] (match character 'a')
+//                            |
+//                        dp[2][1] (no match)
+//                             \
+//                           dp[3][2] (match character 'c')
+//                               |
+//                           dp[4][2] (no match)
+//                                \ 
+//                              dp[5][3] (match character 'e')
+//                     Whenever there is a character match, we move diagonally left-up. And whenever there is no match, we
+//                     move either towards straight up or straight left whichever is maximum.
+//                     Thus, the LCS in the above illustration comes out to be "ace". The steps for this algorithm will be:
+//                     1. Generate the tabulated dp table as done in 'Leetcode&Gfg\13. Dynamic_Programming\25_LongestCommonSubsequence.cpp'.
+//                     2. Initialie 'i' and 'j' with s1.length() and s2.length() respectively.
+//                     3. Initialize an empty string, lets say named 'ans'. The LCS will be build in this string from back
+//                        to front, i.e., first "e" will be stored in 'ans', then "ce", then "ace".
+//                     4. While both 'i' and 'j' are greater than 0, compare s1[i - 1] and s2[j - 1]:
+//                        Case I.  if(s1[i - 1] == s2[j - 1]):
+//                                 If s1[i - 1] is equal to s2[j - 1], then a matching character is found. Hence,
+//                                 store the character in 'ans', and then move diagonally left-up, i.e., i--, j--.
+//                        Case II. if(s1[i - 1] != s2[j - 1]):
+//                                 If s1[i - 1] is not equal to s2[j - 1], then we have to move either towards
+//                                 straight left or straight up whichever is maximum. Thus, if(dp[i - 1][j] > dp[i][j - 1]),
+//                                 the straight up value is larger, hence move that side, i.e., i--.
+//                                 Else straight left is larger, move towards that side, i.e., j--.
+//                      When any one of the strings gets exhausted, the loop ends and the LCS is built inside 'ans'.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -99,10 +172,10 @@ public:
         string ans = "";
 
         while (i > 0 && j > 0) {
-            if (s1[i - 1] == s2[j - 1]) {
+            if(s1[i - 1] == s2[j - 1]) {
                 ans = s1[i - 1] + ans;   // prepend character, i.e., building string by inserting characters from back to front
                 i--, j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            } else if(dp[i - 1][j] > dp[i][j - 1]) {
                 i--;
             } else {
                 j--;
