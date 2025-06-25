b54b5eb0bbf3f450270ee0ed715680ffd4a04d80
Bishnu Mishra <mishravishnu369@gmail.com>
2025-06-25 06:51:18 +0530

    dp

--- Leetcode&Gfg/13. Dynamic_Programming/33_EditDistance.cpp
+++ Leetcode&Gfg/13. Dynamic_Programming/33_EditDistance.cpp
@@ -116,7 +116,7 @@ public:
 
     // T.C: O(m * n);   where m = word1.length(), n = word2.length()
     // S.C: O(m * n) for dp table + O(m + n) for recursion stack space = O(m * n)
-    int minDistance_recursion(string word1, string word2) {
+    int minDistance_memoization(string word1, string word2) {
         int m = word1.length();
         int n = word2.length();
         vector<vector<int>> dp(m, vector<int>(n, -1));
@@ -125,15 +125,88 @@ public:
 
 // ----------------------------------------------------------------------------------------------------
 
-// ----------------------------------------------------------------------------------------------------
+    int solve_rightShifted(int i, int j, string& word1, string& word2,vector<vector<int>>& dp) {
+        // Base cases considering 1 based indexing
+        //     a  b  c  d
+        // -1  0  1  2  3   ---> earlier
+        //  0  1  2  3  4   ---> after right shifting
+        if(i == 0) return j;   // 'j + 1'th index becomes 'j'th index after the string right shifted by 1 index
+        if(j == 0) return i;   // 'i + 1'th index becomes 'i'th index after the string right shifted by 1 index
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(word1[i - 1] == word2[j - 1]) return dp[i][j] = solve_rightShifted(i - 1, j - 1, word1, word2, dp);
+        return dp[i][j] = 1 + min(solve_rightShifted(i, j - 1, word1, word2, dp),
+                            min(solve_rightShifted(i - 1, j, word1, word2, dp),
+                                solve_rightShifted(i - 1, j - 1, word1, word2, dp)));
+    }
+
+    // T.C: O(m * n);   where m = word1.length(), n = word2.length()
+    // S.C: O(m * n) for dp table + O(m + n) for recursion stack space = O(m * n)
+    int minDistance_memoization_rightShifted(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));   // one more column is required after right shift
+        return solve_rightShifted(m, n, word1, word2, dp);
+    }
 
 // ----------------------------------------------------------------------------------------------------
+
+    // This is based on 'minDistance_memoization_rightShifted()' function
+    // T.C: O(m * n);   where m = word1.length(), n = word2.length()
+    // S.C: O(m * n)
+    int minDistance_tabulation(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for(int j = 0; j <= n; j++) dp[0][j] = j;
+        for(int i = 0; i <= m; i++) dp[i][0] = i;
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(word1[i - 1] == word2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = 1 + min(dp[i][j - 1], min(dp[i - 1][j], dp[i - 1][j - 1]));
+            }
+        }
+
+        return dp[m][n];
+    }
+
+// ----------------------------------------------------------------------------------------------------
+
+    // T.C: O(m * n);   where m = word1.length(), n = word2.length()
+    // S.C: O(n)
+    int minDistance_tabulation_SO(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+        vector<int> curr(n + 1, 0), prev(n + 1, 0);
+
+        for(int j = 0; j <= n; j++) prev[j] = j;
+
+        for(int i = 1; i <= m; i++) {
+            // As because 'dp[i][0] = i' in tabulation. So 0th column of every row has value 'i'.
+            // Hence, every row we visit will have first cell with value of 'i', i.e., its row number.
+            curr[0] = i;
+            for(int j = 1; j <= n; j++) {
+                if(word1[i - 1] == word2[j - 1]) curr[j] = prev[j - 1];
+                else curr[j] = 1 + min(curr[j - 1], min(prev[j], prev[j - 1]));
+            }
+            prev = curr;
+        }
+
+        return prev[n];
+    }
 };
 
 int main() {
     string word1 = "horse", word2 = "ros";
 
     cout << Solution().minDistance_recursion(word1, word2) << endl;
+    cout << Solution().minDistance_memoization(word1, word2) << endl;
+    cout << Solution().minDistance_memoization_rightShifted(word1, word2) << endl;
+    cout << Solution().minDistance_tabulation(word1, word2) << endl;
+    cout << Solution().minDistance_tabulation_SO(word1, word2) << endl;
 
     return 0;
 }
