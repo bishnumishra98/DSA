bec6c2e717b59c5ec1ac49266a40af66ca7f60e0
Bishnu Mishra <mishravishnu369@gmail.com>
2025-06-25 20:58:09 +0530

    dp

--- Leetcode&Gfg/13. Dynamic_Programming/37_MaximizeTheCutSequence.cpp
+++ Leetcode&Gfg/13. Dynamic_Programming/37_MaximizeTheCutSequence.cpp
@@ -1,30 +1,45 @@
-// gfg: Maximize The Cut Segments   --->   Given an integer N denoting the Length of a line segment.
-// You need to cut the line segment in such a way that the cut length of a line segment each time is
-// either x , y or z. Here x, y, and z are integers. After performing all the cut operations, your
-// total number of cut segments must be maximum. Note: if no segment can be cut then return 0.
+// GFG: Maximize The Cut Segments   --->   Given an integer n denoting the Length of a line segment. You need
+// to cut the line segment in such a way that the cut length of a line segment each time is either x , y or z.
+// Here x, y, and z are integers.
+// After performing all the cut operations, your total number of cut segments must be maximum. Return the
+// maximum number of cut segments possible.
+// Note: if no segment can be cut then return 0.
 
 // Example 1:
-// Input:
-// N = 4
-// x = 2, y = 1, z = 1
+// Input: n = 4, x = 2, y = 1, z = 1
 // Output: 4
-// Explanation: Total length is 4, and the cut lengths are 2, 1 and 1. We can make maximum
-// 4 segments each of length 1.
+// Explanation: Total length is 4, and the cut lengths are 2, 1 and 1.  We can make maximum 4 segments each of length 1.
 
 // Example 2:
-// Input:
-// N = 5
-// x = 5, y = 3, z = 2
+// Input: n = 5, x = 5, y = 3, z = 2
 // Output: 2
-// Explanation: Here total length is 5, and the cut lengths are 5, 3 and 2. We can make two
-// segments of lengths 3 and 2.
 
-// Note: This problem is being solved with recursion for now, just to understand recursion better.
-// But optimal approach will be solved by dynamic programming.
+// Example 3:
+// Explanation: Here total length is 5, and the cut lengths are 5, 3 and 2. We can make two segments of lengths 3 and 2.
+// Input: n = 7, x = 8, y = 9, z = 10
+// Output: 0
+// Explanation: Here the total length is 7, and the cut lengths are 8, 9, and 10. We cannot cut the segment into lengths
+// that fully utilize the segment, so the output is 0.
 
-#include <iostream>
-#include <limits.h>
+// Constraints
+// 1 <= n, x, y, z <= 10^4
+
+// Problem link: https://www.geeksforgeeks.org/problems/cutted-segments1642/1
+
+// Algorithm: 
+
+#include <bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+    // T.C: 
+    // S.C: 
+    int maximizeTheCuts(int n, int x, int y, int z) {
+        // Your code here
+        
+    }
+};
 
 // T.C: 3^n
 // S.C: O(n)
