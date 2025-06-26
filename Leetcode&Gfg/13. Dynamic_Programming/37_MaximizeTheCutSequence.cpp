67d62a0ba823e4068e65eccfb29d6526112d14aa
Bishnu Mishra <mishravishnu369@gmail.com>
2025-06-26 19:50:16 +0530

    dp

--- Leetcode&Gfg/13. Dynamic_Programming/37_MaximizeTheCutSequence.cpp
+++ Leetcode&Gfg/13. Dynamic_Programming/37_MaximizeTheCutSequence.cpp
@@ -13,9 +13,9 @@
 // Example 2:
 // Input: n = 5, x = 5, y = 3, z = 2
 // Output: 2
+// Explanation: Here total length is 5, and the cut lengths are 5, 3 and 2. We can make two segments of lengths 3 and 2.
 
 // Example 3:
-// Explanation: Here total length is 5, and the cut lengths are 5, 3 and 2. We can make two segments of lengths 3 and 2.
 // Input: n = 7, x = 8, y = 9, z = 10
 // Output: 0
 // Explanation: Here the total length is 7, and the cut lengths are 8, 9, and 10. We cannot cut the segment into lengths
@@ -26,50 +26,79 @@
 
 // Problem link: https://www.geeksforgeeks.org/problems/cutted-segments1642/1
 
-// Algorithm: 
+// Algorithm: Its an very easy problem. You are smart enough to understand the code.
 
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    // T.C: 
-    // S.C: 
-    int maximizeTheCuts(int n, int x, int y, int z) {
-        // Your code here
-        
+    // T.C: O(3^n)
+    // S.C: O(n) for recursion stack space
+    int maximizeTheCuts_recursion(int n, int x, int y, int z) {
+        if(n == 0) return 0;
+        if(n < 0) return INT_MIN;   // to avoid infinite recursion
+
+        // Cutting 'x' length from 'n', and leaving the rest on recursion
+        int option1 = 1 + maximizeTheCuts_recursion(n - x, x, y, z);
+
+        // Cutting 'y' length from 'n', and leaving the rest on recursion
+        int option2 = 1 + maximizeTheCuts_recursion(n - y, x, y, z);
+
+        // Cutting 'z' length from 'n', and leaving the rest on recursion
+        int option3 = 1 + maximizeTheCuts_recursion(n - z, x, y, z);
+
+        return max(option1, max(option2, option3));
+    }
+
+// -----------------------------------------------------------------------------------------------------------
+
+    int solve(int n, int x, int y, int z, vector<int>& dp) {
+        if(n == 0) return 0;
+        if(n < 0) return INT_MIN;
+
+        if(dp[n] != -1) return dp[n];
+
+        int option1 = 1 + maximizeTheCuts_recursion(n - x, x, y, z);
+        int option2 = 1 + maximizeTheCuts_recursion(n - y, x, y, z);
+        int option3 = 1 + maximizeTheCuts_recursion(n - z, x, y, z);
+
+        return dp[n] = max(option1, max(option2, option3));
+    }
+
+    // T.C: O(n)
+    // S.C: O(n) for dp array + O(n) for recursion stack space = O(n)
+    int maximizeTheCuts_memoization(int n, int x, int y, int z) {
+        vector<int> dp(n + 1, -1);
+        return solve(n, x, y, z, dp);
+    }
+
+// -----------------------------------------------------------------------------------------------------------
+
+    // T.C: O(n)
+    // S.C: O(n)
+    int maximizeTheCuts_tabulation(int n, int x, int y, int z) {
+        vector<int> dp(n + 1, INT_MIN);
+        dp[0] = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if(i - x >= 0) dp[i] = max(dp[i], 1 + dp[i - x]);
+            if(i - y >= 0) dp[i] = max(dp[i], 1 + dp[i - y]);
+            if(i - z >= 0) dp[i] = max(dp[i], 1 + dp[i - z]);
+        }
+
+        return dp[n] < 0 ? 0 : dp[n];
     }
 };
 
-// T.C: 3^n
-// S.C: O(n)
-int maximizeTheCuts(int n, int x, int y, int z) {
-    // base case
-    if(n == 0) {
-        return 0;
-    }
-    if(n < 0) {
-        return INT_MIN;   // to avoid infinite recursion
-    }
-
-    // processing
-    // cutting 'x' length from 'n', and leaving the rest on recursion
-    int option1 = 1 + maximizeTheCuts(n-x, x, y, z);   // recursive relation
-
-    // cutting 'y' length from 'n', and leaving the rest on recursion
-    int option2 = 1 + maximizeTheCuts(n-y, x, y, z);   // recursive relation
-
-    // cutting 'z' length from 'n', and leaving the rest on recursion
-    int option3 = 1 + maximizeTheCuts(n-z, x, y, z);   // recursive relation
-
-    return max(option1, max(option2, option3));
-}
 
 int main() {
-    int N = 4;
-    int x = 2, y = 1, z = 1;
+    int N = 7;
+    int x = 5, y = 5, z = 2;
 
-    cout << maximizeTheCuts(N, x, y, z);
+    cout << Solution().maximizeTheCuts_recursion(N, x, y, z) << endl;
+    cout << Solution().maximizeTheCuts_memoization(N, x, y, z) << endl;
+    cout << Solution().maximizeTheCuts_tabulation(N, x, y, z) << endl;
 
     return 0;
 }
