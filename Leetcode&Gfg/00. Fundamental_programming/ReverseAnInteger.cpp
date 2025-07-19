a7faaca620c6e5a567021cb3754d99fad67b8074
Bishnu Mishra <mishravishnu369@gmail.com>
2025-07-19 13:15:53 +0530

    nm

--- Leetcode&Gfg/00. Fundamental_programming/ReverseAnInteger.cpp
+++ Leetcode&Gfg/00. Fundamental_programming/ReverseAnInteger.cpp
@@ -1,6 +1,5 @@
-// Leetcode: 7. Reverse Integer
-// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes
-// the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+// Leetcode: 7. Reverse Integer --->   Given a signed 32-bit integer x, return x with its digits reversed. If
+// reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
 
 // Example 1:
 // Input: x = 123
@@ -14,8 +13,7 @@
 // Input: x = 120
 // Output: 21
 
-#include<iostream>
-#include<limits.h>   // for INT_MAX and INT_MIN
+#include<bits/stdc++.h>
 using namespace std;
 
 // T.C: O(logx)
