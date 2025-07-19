afa1db0cb3384060ab5e82ddaeede4332347e752
Bishnu Mishra <mishravishnu369@gmail.com>
2025-07-19 16:22:57 +0530

    nm

--- BaseBuildingPrograms/02. Basic_programming/BinaryToDecimal.cpp
+++ BaseBuildingPrograms/02. Basic_programming/BinaryToDecimal.cpp
@@ -5,10 +5,10 @@ using namespace std;
 // T.C: O(logn)
 // S.C: O(1)
 int BinaryToInt(long long n) {
-    int bit, i = 0, ans = 0;
+    int i = 0, ans = 0;
 
     while(n) {
-        bit = n % 10;   // kicking out last bit of binary number
+        int bit = n % 10;   // kicking out last bit of binary number
         ans = ans + (bit * round(pow(2, i)));
         n = n / 10;
         i++;
