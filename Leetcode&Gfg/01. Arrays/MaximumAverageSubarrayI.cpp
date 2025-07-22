7142363eb5d8b33cfefca93bf49b4f9bfa01b365
Bishnu Mishra <mishravishnu369@gmail.com>
2025-07-22 19:48:27 +0530

    nm

--- Leetcode&Gfg/01. Arrays/MaximumAverageSubarrayI.cpp
+++ Leetcode&Gfg/01. Arrays/MaximumAverageSubarrayI.cpp
@@ -1,7 +1,7 @@
 // Leetcode: 643. Maximum Average Subarray I   --->   You are given an integer array nums consisting of n elements,
 // and an integer k.
 // Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value.
-// Any answer with a calculation error less than 10-5 will be accepted.
+// Any answer with a calculation error less than 10^-5 will be accepted.
 
 // Example 1:
 // Input: nums = [1,12,-5,-6,50,3], k = 4
@@ -18,7 +18,7 @@ using namespace std;
 class Solution {
 public:
     // 2 pointer approach
-    // T.C: O((n-k) * k) = O(n * k);   O(n-k) for outer loop, and O(k) for inner loop
+    // T.C: O((n - k) * k) = O(n * k);   O(n - k) for outer loop, and O(k) for inner loop
     // S.C: O(1)
     double findMaxAverage_bruteforce(vector<int>& nums, int k) {
         int i = 0, j = k - 1;   // i and j surround the range of first k elements in 'nums'
@@ -42,7 +42,7 @@ public:
 // -------------------------------------------------------------------------------------------
 
     // Sliding window technique (just subtract and add one element in the window at a time; calculate sum and update maxSum if needed)
-    // T.C: O(k + (n-k)) = O(n)
+    // T.C: O(k + (n - k)) = O(n)
     // S.C: O(1)
     double findMaxAverage(vector<int>& nums, int k) {
         int sum = 0;
