19051b8727f9e5757d00d4e7a5d49dc7515a8b7b
Bishnu Mishra <mishravishnu369@gmail.com>
2025-07-31 01:54:50 +0530

    nm

--- Leetcode&Gfg/01. Arrays/MaximumSubarray.cpp
+++ Leetcode&Gfg/01. Arrays/MaximumSubarray.cpp
@@ -55,7 +55,7 @@ public:
         int maxSum = INT_MIN, sum = 0;
         for(int i = 0; i < n; i++) {
             sum += nums[i];
-            if(sum > maxSum) maxSum = sum;
+            maxSum = max(maxSum, sum);
             if(sum < 0) sum = 0;
         }
         return maxSum;
