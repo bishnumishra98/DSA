278ac2e0a2999adc57bb77294841a3c86ac946fa
Bishnu Mishra <mishravishnu369@gmail.com>
2025-07-31 12:49:39 +0530

    nm

--- Leetcode&Gfg/01. Arrays/SortColors.cpp
+++ Leetcode&Gfg/01. Arrays/SortColors.cpp
@@ -100,10 +100,10 @@ public:
     void sortColors(vector<int>& nums) {
         int low = 0, mid = 0, high = nums.size() - 1;
 
-        // 0 to low-1    -> 0
-        // low to mid-1  -> 1
-        // mid to high   -> random numbers
-        // high+1 to n-1 -> 2
+        // 0 to low - 1      -> 0
+        // low to mid - 1    -> 1
+        // mid to high       -> random numbers
+        // high + 1 to n - 1 -> 2
         while(mid <= high) {
             if(nums[mid] == 0) {
                 swap(nums[mid], nums[low]);
