8b7ac0308c09d39d22dde902dea92e5a28ab5d7e
Bishnu Mishra <mishravishnu369@gmail.com>
2025-09-05 18:11:13 +0530

    nm

--- BaseBuildingPrograms/05. Sorting/03_InsertionSort.cpp
+++ BaseBuildingPrograms/05. Sorting/03_InsertionSort.cpp
@@ -1,33 +1,19 @@
-// Insertion Sort: Starting from 1st to 'n-1' index, pick one element at a time and comparing it with the
-// elements on its left, insert it on its correct index.
+// Insertion Sort: The array is sorted from left to right. Take one element at a time from the list (starting from
+//                 the second one). This one element will be called key element. Compare the key element with the
+//                 elements before it. Insert the key element in the correct position among the sorted part. Repeat
+//                 until the whole list is sorted.
+//                 For inserting the key element in the correct position, we need to right shift all the elements
+//                 that are greater than the key element and fall on the left of the key element.
 
-// Approach   --->   Consider the following array: int arr[5] = {45, 35, 55, 15, 25};   here n = 5.
+// Illustration:
+// Unsorted array: {45, 35, 55, 15, 25}, n = 5
+// Pass 1: {35, 45, 55, 15, 25}   // key element is 35. 45 is right shifted and 35 is placed at index 0
+// Pass 2: {35, 45, 55, 15, 25}   // key element is 55. 55 is already at the correct position
+// Pass 3: {15, 35, 45, 55, 25}   // key element is 15. 35, 45 and 55 are right shifted and 15 is placed at index 0
+// Pass 4: {15, 25, 35, 45, 55}   // key element is 25. 35, 45 and 55 are right shifted and 25 is placed at index 1
+// No need of Pass 5 as the last element will be the largest element 55.
 
-// {45, 35, 55, 15, 25}
-//  0   1   2   3   4
-
-// For outer loop i=1,
-// {35, 45, 55, 15, 25}
-//  0   1   2   3   4
-//  j   i
-
-// For outer loop i=2, no change:
-// {35, 45, 55, 15, 25}
-//  0   1   2   3   4
-//      j   i
-
-// For outer loop i=3,
-// {15, 35, 45, 55, 25}
-//  0   1   2   3   4
-//          j   i
-
-// For outer loop i=4,
-// {15, 25, 35, 45, 55}
-//  0   1   2   3   4
-//              j   i
-
-// Hence, for n = 5, outer loop runs 'n-1' times. i represents outer loop, and j represents inner loop.
-// i runs from 1 to 'n-1'; and j runs from 'i-1' to 0. Dry run for better understanding.
+// Video link: https://www.youtube.com/watch?v=JU767SDMDvA
 
 
 #include <iostream>
@@ -45,20 +31,19 @@ void print(vector<int>& v) {
 void insertionSort(vector<int>& v) {
     int n = v.size();
 
-    for(int i=1; i<n; i++) {
+    for(int i = 1; i < n; i++) {
         int keyElement = v[i];
-        int j = i-1;
+        int j = i - 1;   // j is the index of the last element in the sorted part
 
-        while(j>=0 && v[j]>keyElement) {
-            // right shifting elements by 1 index that are bigger than key and fall on left of key
-            v[j+1] = v[j];
+        // Find the correct position to insert the keyElement in the sorted part
+        while(j >= 0 && v[j] > keyElement) {
+            // Right shift the elements that are greater than the keyElement
+            v[j + 1] = v[j];
             j--;
         }
 
-        // When program comes out of while loop, j is either -1 or v[j] is smaller than keyElement.
-        // v[j+1] is the index from where right elements are greater than keyElement and left elements
-        // are smaller than keyElement, i.e. it is the accurate index to place keyElement
-        v[j+1] = keyElement;
+        // Insert the keyElement at its correct position
+        v[j + 1] = keyElement;
     }
 }
 
