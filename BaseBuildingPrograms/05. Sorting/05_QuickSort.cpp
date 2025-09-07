867fdac30b5daac71951700eafd3d7e4b91debbc
Bishnu Mishra <mishravishnu369@gmail.com>
2025-09-07 18:38:46 +0530

    quick sort

--- BaseBuildingPrograms/05. Sorting/05_QuickSort.cpp
+++ BaseBuildingPrograms/05. Sorting/05_QuickSort.cpp
@@ -1,84 +1,72 @@
-// Quick sort is a method for organizing a list of things into order (from smallest to largest). Here's a simple way to understand it:
-// 1. Pick a Pivot: Choose one item from the list. This item is called the "pivot."
-// 2. Sort Around Pivot: Rearrange the list so that all items smaller than the pivot come before it, and all items larger come after it.
-// 3. Repeat: Now, take the smaller and larger sections and do the same thing (pick a pivot, sort around it) until the whole list is sorted.
+// Quick Sort: We pick a element called pivot element, and partition the array around the pivot such that all elements on left
+//             of pivot are smaller than or equal to pivot, and all elements on right of pivot are greater than or equal to
+//             pivot. Then, we recursively apply the same logic to left and right sub-arrays of pivot. And finally, the whole
+//             array gets sorted.
+
+// ● How to partition the array around the pivot element ?
+//   1. Choose a pivot element. Here, we are choosing the first element of the array as pivot.
+//   2. Take two pointers i and j. Initialize i to the first index and j to the last index of the array.
+//   3. Increment i till we find an element greater than pivot. Decrement j till we find an element smaller than or equal to pivot.
+//      Hence, i stands at the first element encountered from left which is greater than pivot, and j stands at the first element
+//      encountered from right which is smaller than or equal to pivot.
+//   4. If i is still less than j, it means we have found two elements which are at wrong halves, so we swap them. Repeat
+//      steps 3 and 4 till i meets or crosses j.
+//   5. Once i meets or crosses j, j comes to the region where every element is smaller than or equal to pivot. So, we swap the
+//      pivot element with the element at j. Now, pivot is at its correct position.
+//   6. Return j as the partition index, .i.e., the correct index of pivot element.
+
+// Video link: https://www.youtube.com/watch?v=WIrA4YexLRQ&t=417s
+
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-// Partition function to divide the array into two parts
-int partition(int arr[], int low, int high) {
-    int pivot = arr[low];   // considering first element of array as pivot
+// Partition function returns the correct index of the pivot element and segregates the array around the pivot element
+// such that all elements on left of pivot are smaller than or equal to pivot, and all elements on right of pivot are greater
+// than or equal to pivot.
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[low];   // considering first element of array as pivot, i.e., arr[low]
     int i = low;
     int j = high;
 
     while(i < j) {
-        while(pivot >= arr[i] && i <= high-1) i++;   // Move i to the right while elements are less than or equal to pivot and within bounds
-        while(pivot < arr[j] && j >= low+1) j--;   // Move j to the left while elements are greater than pivot and within bounds
-        if(i < j) swap(arr[i], arr[j]);   // If i is still less than j, swap elements at i and j
+        while(i <= high - 1 && arr[i] <= pivot) i++;   // increment i till we find an element greater than pivot
+        while(j >= low + 1 && arr[j] > pivot) j--;   // decrement j till we find an element smaller than / equal to pivot
+        // 'i <= high - 1' and 'j >= low + 1' are written to avoid going out of array bounds after incrementing/decrementing i/j respectively.
+        if(i < j) swap(arr[i], arr[j]);   // if i is still less than j, swap elements at i and j to put them in correct halves
     }
     
-    swap(arr[low], arr[j]);   // Swap pivot element with the element at j to place the pivot in its correct position
-    return j;   // Return the partition index. Element at index j is at its correct position now.
+    swap(arr[low], arr[j]);   // swap pivot element with the element at j to place the pivot in its correct position
+    return j;   // return the partition index, i.e., correct index of pivot element
+}
+
+void qs(vector<int>& arr, int low, int high) {
+    // Base case: If array has zero or one element, it is already sorted. Thus, we return.
+    if(low >= high) return;
+    int partitionIndex = partition(arr, low, high);   // considering arr[low] as pivot
+    qs(arr, low, partitionIndex - 1);
+    qs(arr, partitionIndex + 1, high);
 }
 
 // T.C: O(nlogn);   where n = size of array.
 //                  However, worst time complexity of quickSort is O(n^2), when pivot is chosen as the biggest element of array.
-// S.C: O(logn);   due to recursive stack.
-//                 However, worst case space complexity is O(n). 
-void quickSort(int arr[], int low, int high) {
-    if(low < high) {
-        int pivot = partition(arr, low, high);
-        quickSort(arr, low, pivot-1);
-        quickSort(arr, pivot+1, high);
-    }
+// S.C: O(1);   However, it occupies O(logn) recursive stack space, and worst case recursive stack space complexity can be O(n).
+void quickSort(vector<int>& arr) {
+    int low = 0;
+    int high = arr.size() - 1;
+    qs(arr, low, high);
 }
 
+
 int main() {
-    int arr[] = {30, 40, 20, 70, 10, 60, 50};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    int low = 0;
-    int high = size - 1;
+    vector<int> arr = {30, 40, 20, 70, 10, 60, 50};
+    quickSort(arr);
 
-    quickSort(arr, low, high);
-
-    for(int element: arr) {
-        cout << element << " ";
-    }
+    for(int element: arr) cout << element << " ";
 
     return 0;
 }
 
-
-
-/*
-// Lakshay bhaiya code for quickSort()
-// T.C: O(nlogn);   where n = size of array.
-//                  However, worst time complexity of quickSort is O(n^2), when pivot is chosen as the biggest element of array.
-// S.C: O(logn);   due to recursive stack.
-//                 However, worst case space complexity is O(n).
-void quickSort(int *arr, int start, int end) {
-    // base case
-    if(start >= end) return;
-
-    int pivot = end;
-    int i = start - 1;
-    int j = start;
-
-    while(j < pivot) {
-        if(arr[j] < arr[pivot]) {
-            i++;
-            swap(arr[i], arr[j]);
-        }
-        j++;
-    }
-
-    i++;
-    swap(arr[i], arr[pivot]);   // pivot element comes to its correct index at this step
-
-    // element at ith index has all elements smaller than it on its left, and all elements larger
-    // than it on its right. Then, we do quickSort for sub-array on its left and right also.
-    quickSort(arr, start, i-1);
-    quickSort(arr, i+1, end);
-}
-*/
+// EXTRA INFO: Considering first array element as pivot is called Hoare's Partition Scheme, and considering last array element
+//             as pivot is called Lomuto's Partition Scheme.
