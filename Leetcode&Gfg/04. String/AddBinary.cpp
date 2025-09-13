abc3a86c94762f12fb567f10b5c034986490b7ce
Bishnu Mishra <mishravishnu369@gmail.com>
2025-09-14 01:41:01 +0530

    nm

--- Leetcode&Gfg/04. String/AddBinary.cpp
+++ Leetcode&Gfg/04. String/AddBinary.cpp
@@ -13,39 +13,48 @@ using namespace std;
 
 class Solution {
 public:
-    // T.C: O(max(len(a),len(b)))
-    // S.C: O(max(len(a),len(b)))
+    // T.C: O(max(n1, n2));   n1 = length of string a;   n2 = length of string b
+    // S.C: O(1);   as the result string is not considered in space complexity
     string addBinary(string a, string b) {
-        string result = "";
-        int i = a.size() - 1;
-        int j = b.size() - 1;
+        int n1 = a.length();
+        int n2 = b.length();
         int carry = 0;
+        string result = "";
 
-        // Iterate while there are digits in either string or there is a carry
-        while(i >= 0 || j >= 0 || carry) {
-            int sum = carry;   // start with the carry value
-            if (i >= 0) sum += a[i--] - '0';   // add the digit of a at ith index and move the pointer
-            if (j >= 0) sum += b[j--] - '0';   // add the digit of b at jth index and move the pointer
-            carry = sum / 2;   // update the carry (1 if sum is 2 or more, otherwise 0)
-            result += (sum % 2) + '0';   // append the current digit (sum % 2) to the result
+        int i = n1 - 1;
+        int j = n2 - 1;
+        
+        while(i >= 0 && j >= 0) {
+            int sum = (a[i] - '0') + (b[j] - '0') + carry;
+            result.push_back((sum % 2) + '0');
+            carry = sum / 2;
+            i--;
+            j--;
         }
-
-        // Reverse the result string as it is built in reverse order
+        
+        while(i >= 0) {
+            int sum = (a[i] - '0') + carry;
+            result.push_back((sum % 2) + '0');
+            carry = sum / 2;
+            i--;
+        }
+        
+        while(j >= 0) {
+            int sum = (b[j] - '0') + carry;
+            result.push_back((sum % 2) + '0');
+            carry = sum / 2;
+            j--;
+        }
+        
+        while(carry) {
+            result.push_back((carry % 2) + '0');
+            carry /= 2;
+        }
+        
         reverse(result.begin(), result.end());
         return result;
     }
 };
-
-// Why a[i--] - '0' ?
-// The expression "- '0'" is used to convert a character representing a digit (e.g., '0', '1', ..., '9') to its
-// corresponding integer value.
-// '0' -> ASCII value 48
-// '1' -> ASCII value 49
-// '2' -> ASCII value 50
-// '9' -> ASCII value 57
-
-// '1' - '0' becomes 49 - 48, which is 1.
-// '5' - '0' becomes 53 - 48, which is 5.
 
 int main() {
     Solution sol;
