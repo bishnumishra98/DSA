2556ea35de03767c82f7ba5bca4d3662609ba0b1
Bishnu Mishra <mishravishnu369@gmail.com>
2025-09-16 21:33:37 +0530

    nm

--- Leetcode&Gfg/04. String/RemoveAllAdjacentDuplicatesInString.cpp
+++ Leetcode&Gfg/04. String/RemoveAllAdjacentDuplicatesInString.cpp
@@ -1,36 +1,48 @@
-// Leetcode: 1047. Remove All Adjacent Duplicates In String. Ex:-
-// (1) i/p: "abbaca",   o/p: "ca"           (2) i/p: "azxxzy",   o/p: "ay"
+// Leetcode: 1047. Remove All Adjacent Duplicates In String   --->   You are given a string s consisting of lowercase
+// English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+// We repeatedly make duplicate removals on s until we no longer can.
+// Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+// Example 1:
+// Input: s = "abbaca"
+// Output: "ca"
+// Explanation: 
+// For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.
+// The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+
+// Example 2:
+// Input: s = "azxxzy"
+// Output: "ay"
 
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-// T.C: O(n)
-// S.C: O(n)
-string removeDuplicates(string s) {
-    string ans = "";
-    int index = 0;
-    
-    while(index < s.size()) {
-        int n = ans.size();
+class Solution {
+public:
+    // T.C: O(n);   where n = s.length()
+    // S.C: O(n)
+    string removeDuplicates(string s) {
+        string result = "";
 
-        // if rightmost element in 'ans' is equal to s[index], then pop out rightmost element
-       // from 'ans'; else push s[index] as rightmost element in 'ans'
-        if((n-1 >= 0) && (ans[n-1] == s[index])) {
-            ans.pop_back();
-        } else {
-            ans.push_back(s[index]);
+        for(char ch : s) {
+            if(!result.empty() && result.back() == ch) {
+                // duplicate found → remove last char
+                result.pop_back();
+            } else {
+                // no duplicate → add char
+                result.push_back(ch);
+            }
         }
 
-        index++;
+        return result;
     }
-
-    return ans;
-}
+};
 
 int main() {
     string str = "abbaca";
-    cout << removeDuplicates(str);
+
+    cout << Solution().removeDuplicates(str);
 
     return 0;
 }
