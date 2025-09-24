8443ec41680e22dff66f0da8971a52a0bb06a9e4
Bishnu Mishra <mishravishnu369@gmail.com>
2025-09-25 04:42:46 +0530

    nm

--- BaseBuildingPrograms/07. Basic_Math/SieveOfEratosthenes.cpp
+++ BaseBuildingPrograms/07. Basic_Math/SieveOfEratosthenes.cpp
@@ -2,25 +2,24 @@
 // It is a very efficient algorithm, and it is still widely used today. Let's say given input is n.
 
 // The algorithm states that :-
-// 1) Create an array of size n, and mark all of them as prime, i.e., true.
-// 2) Iterate starting from 2 to n, mark all the numbers that comes in table of 2 as composite, i.e., false.
-// 3) Repeat step 2 till n, only for numbers that are marked prime.
-// 4) In the end of the loop, elements still marked as prime are the prime numbers in the range 2 to n.
+// 1. Create an array 'sieve' of size n + 1, and mark all of them as prime, i.e., true. sieve[0] represents 0,
+//    sieve[1] represents 1, and so on.
+// 2. Iterate starting from 2 to n, mark all the numbers that come in the table of 2 as composite, i.e., false.
+// 3. Repeat the above step till n, only for numbers that are marked prime.
+// 4. In the end of the loop, elements still marked as prime are the prime numbers in the range 2 to n.
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-// Here, createSieveArray() is a function that takes n as a parameter, and returns a sieve array.
-// Here, a sieve array is known as an array containing 'n+1' elements indexed from 0 to n,
-// all marked as false, except the ones present at prime indexes. Ex:- Elements at 0th, 1st,
-// 4th, etc indexes contain false, while elements at 2nd, 3rd, 5th, etc indexes contain true.
+// Here, createSieveArray() is a function that takes n as a parameter, and returns a sieve array,
+// which contains the information of prime numbers from 0 to n.
 // T.C: O(nlog(logn))
 // S.C: O(n);   as the function creates a vector of size 'n+1'
 vector<bool> createSieveArray(int n) {
     vector<bool> sieve(n + 1, true);   // all elements of sieve array contains true in the beginning
 
-    // sieve[0] and sieve[1] will be treated as 0 and 1 respectively. So marking them false, i.e composite.
+    // sieve[0] and sieve[1] will be treated as 0 and 1 respectively. So marking them false, i.e., composite.
     sieve[0] = sieve[1] = false;
 
     for(int i = 2; i <= n; i++) {
