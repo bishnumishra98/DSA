2d1fe4998930bd79265195599bd56a59842fc371
Bishnu Mishra <mishravishnu369@gmail.com>
2025-10-01 17:05:28 +0530

    gcd

--- BaseBuildingPrograms/09. Recursion/08_LCMOfTwoNumbers.cpp
+++ BaseBuildingPrograms/09. Recursion/08_LCMOfTwoNumbers.cpp
@@ -1,39 +1,34 @@
 #include <iostream>
 using namespace std;
 
-// T.C: O(min(A, B))
-// S.C: O(min(A, B))
-int gcd(int A, int B) {
-    // Euclid's algorithm to find gcd:
-    // gcd(a, b) = gcd(a-b, a);   if a>b  (OR) if a>=b
-    //           = gcd(b-a, b);   if b>=a (OR) if b>a
-    // Note: You can even use '%' instead of '-' in above algorithm.
-    // But '%' is computationally more expensive than '-', so we use '-'.
+// Euclid's algorithm to find gcd:
+// ● gcd(A, B) = gcd(A - B, B) if A > B
+// ● gcd(A, B) = gcd(A, B - A) if B > A
+// ● If A = B, then gcd(A, B) = A or B, anything works
+// ● gcd(A, 0) = A
+// ● gcd(0, B) = B
 
-    // base case
-    if(A == 0) return B;
+// T.C: O(min(A, B))
+// S.C: O(min(A, B))   due to recursion stack space
+int gcd(int A, int B) {
+    // Base cases
     if(B == 0) return A;
-    
-    // processing
-    if(A > B) {
-        A = A - B;
-    } else {   // if(B >=A)
-        B = B - A;
-    }
-    
-    // recursive relation
-    gcd(A, B);
+    if(A == 0) return B;
+
+    if(A == B) return A;
+    else if(A > B) return gcd(A - B, B);
+    else return gcd(A, B - A);   // if(B > A)
 }
 
 // T.C: O(min(A, B));   due to gcd()
 // S.C: O(min(A, B));   due to gcd()
 int lcm(int A, int B) {
     // lcm of 2 no.s = product of two numbers / hcf of 2 no.s
-    return (A*B) / gcd(A, B);
+    return (A * B) / gcd(A, B);
 }
 
 int main() {
-    cout << lcm(26, 40);
+    cout << lcm(4, 6);
 
     return 0;
 }
