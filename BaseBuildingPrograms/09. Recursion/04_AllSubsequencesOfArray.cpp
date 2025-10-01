c89897ad0f024cbb850fabf3ac8b5efb72cbee26
Bishnu Mishra <mishravishnu369@gmail.com>
2025-10-01 23:37:06 +0530

    recursion

--- BaseBuildingPrograms/09. Recursion/04_AllSubsequencesOfArray.cpp
+++ BaseBuildingPrograms/09. Recursion/04_AllSubsequencesOfArray.cpp
@@ -1,7 +1,7 @@
 // This type of problem follows PICK/NOT PICK pattern.
 
 // Given an array, print all its subsequence. All subsequence must follow the ordering of elements as it is in parent array.
-// For example, {8, 6} cannot be a subsequence, but {6, 8} is a subsequence.
+// For example, for the arrat {6, 7, 8}, {8, 6} cannot be a subsequence of it, but {6, 8} is a subsequence.
 // Example:
 // Input:
 // {6, 7, 8}
@@ -27,14 +27,14 @@ void printSubsequence(int index, int arr[], int n, vector<int>& ds) {
 
     // Case 1: Take the current element of 'arr' and proceed to make the subsequence
     ds.push_back(arr[index]);
-    printSubsequence(index+1, arr, n, ds);
+    printSubsequence(index + 1, arr, n, ds);
     ds.pop_back();   // ds.pop_back() is used to remove the last element that was added to the 'ds' vector in the previous
     // step. This is necessary because, after all subsequences that include the current element (at arr[index]) have been
     // printed, you need to backtrack and remove that element from 'ds' to explore subsequences that do not include this element.
     // However, note that we do not need to perform this pop operation, if Case 2 was written prior to Case 1.
 
     // Case 2: Not take the current element of 'arr' and proceed to make the subsequence
-    printSubsequence(index+1, arr, n, ds);
+    printSubsequence(index + 1, arr, n, ds);
 }
 
 int main() {
