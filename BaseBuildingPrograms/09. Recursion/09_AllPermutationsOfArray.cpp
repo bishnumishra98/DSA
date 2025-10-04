2933b9074c1db844ca55802d5696b9aef9306e51
Bishnu Mishra <mishravishnu369@gmail.com>
2025-10-04 20:00:37 +0530

    rec

--- BaseBuildingPrograms/09. Recursion/09_AllPermutationsOfArray.cpp
+++ BaseBuildingPrograms/09. Recursion/09_AllPermutationsOfArray.cpp
@@ -14,7 +14,7 @@
 #include <vector>
 using namespace std;
 
-void allPermut(vector<int>& candidates, vector<vector<int>>& ans, vector<int>& ds, int freq[]) {
+void allPermut(vector<int>& candidates, vector<vector<int>>& ans, vector<int>& ds, vector<bool>& freq) {
     // Base case: When size of 'ds' is equal to size of 'candidates', the permutation is built. Push it in 'ans' and return.
     if(ds.size() == candidates.size()) {
         ans.push_back(ds);
@@ -22,15 +22,15 @@ void allPermut(vector<int>& candidates, vector<vector<int>>& ans, vector<int>& d
     }
 
     // Iterate over each element of 'candidate' to build all possible permutation from ith element
-    for(int i=0; i<candidates.size(); i++) {
-        // If this ith element is not piced up yet
-        if(freq[i] == 0) {
+    for(int i = 0; i < candidates.size(); i++) {
+        // If this ith element is not picked up yet
+        if(freq[i] == false) {
             ds.push_back(candidates[i]);   // Push the ith element into 'ds'
-            freq[i] = 1;   // Mark freq[i] as 1, stating that ith element has been picked
+            freq[i] = true;   // Mark freq[i] as true, stating that ith element has been picked
             allPermut(candidates, ans, ds, freq);   // Call the allPermut() function to find the other elements that
                                                    // can be paired with ith element to build a unique permutation
             // Backtrack: After coming from the recursive call, unmark freq[i], and remove the last element from 'ds'
-            freq[i] = 0;
+            freq[i] = false;
             ds.pop_back();
         }
     }
@@ -42,12 +42,8 @@ vector<vector<int>> findPermutations_bruteForce(vector<int>& candidates) {
     vector<vector<int>> ans;
     vector<int> ds;   // helper data structure to store elements of candidates temporarily
 
-    // Initializing a 'freq' array of size 'candidates.size()', with initial values 0.
-    // int freq[candidates.size()];   // helper data structure to keep a track of elements that have been picked
-    // for(int i=0; i<candidates.size(); i++) freq[i] = 0;
-
-    // The above two steps can be done in 1 line:
-    int freq[candidates.size()] = {0};
+    // Initializing a 'freq' array of size 'candidates.size()', with initial values 0
+    vector<bool> freq(candidates.size(), false);
 
     allPermut(candidates, ans, ds, freq);
     return ans;
@@ -57,17 +53,16 @@ vector<vector<int>> findPermutations_bruteForce(vector<int>& candidates) {
 
     void allPermut(int index, vector<int>& candidates, vector<vector<int>>& ans) {
         // Base case: When we reach beyond last element of 'candidates', push the 'candidates' vector in 'ans' and return.
-        if(index == candidates.size()) {   // Base case can also be if(index == candidates.size() - 1) {...} as when 'index'
-                                          // reaches 'candidates.size() - 1', no change in position of elements occur on further
-                                         // recursive call. Thus, the 2nd last level of recursion can also be treated as the base case.
+        if(index == candidates.size()) {   // or, if(index == candidates.size() - 1) as when 'index' reaches 'candidates.size() - 1',
+                                          // no change in position of elements occur on next recursive call (last recursive call).
             ans.push_back(candidates);
             return;
         }
 
         // Swapping current element with itself and elements on its right.
-        for(int i=index; i<candidates.size(); i++) {
+        for(int i = index; i < candidates.size(); i++) {
             swap(candidates[index], candidates[i]);
-            allPermut(index+1, candidates, ans);
+            allPermut(index + 1, candidates, ans);
             swap(candidates[index], candidates[i]);   // backtrack to unswap 'candidates' after coming from the above recursion call
         }
     }
