9647c0fb0f84282aec8629527e38f464b56927f8
Bishnu Mishra <mishravishnu369@gmail.com>
2025-10-22 18:21:47 +0530

    nm

--- Leetcode&Gfg/05. Recursion/CombinationSum_PartI.cpp
+++ Leetcode&Gfg/05. Recursion/CombinationSum_PartI.cpp
@@ -7,7 +7,7 @@
 // 150 combinations for the given input.
 
 // Algorithm: This type of problem follows PICK/NOT PICK pattern, that we learnt in
-//           'BaseBuildingPrograms/09. Recursion_I/08_AllSubsequenceTargetSum.cpp'.
+//           'BaseBuildingPrograms\09. Recursion\05_AllSubsequenceTargetSum.cpp'.
 //           The only difference being we can pick an element multiple times to make the target.
 
 // Example 1:
