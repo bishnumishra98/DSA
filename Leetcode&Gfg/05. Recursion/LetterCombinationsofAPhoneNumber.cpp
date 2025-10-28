82c27dd38db3499e61d77796ca90c96f2af82669
Bishnu Mishra <mishravishnu369@gmail.com>
2025-10-28 23:17:01 +0530

    nm

--- Leetcode&Gfg/05. Recursion/LetterCombinationsofAPhoneNumber.cpp
+++ Leetcode&Gfg/05. Recursion/LetterCombinationsofAPhoneNumber.cpp
@@ -4,8 +4,6 @@
 // A mapping of digits to letters (just like on the telephone buttons) is given below. Note that
 // 1 does not map to any letters.
 
-// PATTERN: ALL SUBSEQUENCES
-
 // Example 1:
 // Input: digits = "23"
 // Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
@@ -18,12 +16,15 @@
 // Input: digits = "2"
 // Output: ["a","b","c"]
 
+// Problem link: https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    void findCombination(int index, vector<string>& ans, string ds, string mapping[], string digits) {
+    void findCombination(int index, vector<string>& ans, string ds, vector<string>& mapping, string digits) {
         // Base case: When index reaches beyond the last element of digits, store the answer made in 'ds' and return
         if(index == digits.size()) {
             ans.push_back(ds);
@@ -33,10 +34,10 @@ public:
         int mapIndex = digits[index] - '0';   // if digits[index] is '2'(char), we will get 2(int) in mapIndex.
         string s = mapping[mapIndex];   // s will store "abc" as mapping[2] contains "abc".
 
-        // Iterating over each element of string 's'
-        for(int i=0; i<s.length(); i++) {
+        // Iterating over each element of string 's' and making recursive calls to find further combinations
+        for(int i = 0; i < s.length(); i++) {
             ds.push_back(s[i]);
-            findCombination(index+1, ans, ds, mapping, digits);
+            findCombination(index + 1, ans, ds, mapping, digits);
             ds.pop_back();   // Backtrack
         }
     }
@@ -48,15 +49,15 @@ public:
     //                    combination involves concatenating characters, which takes O(n) time because each combination
     //                    is of length n. Therefore, the total time complexity is the number of combinations multiplied
     //                    by the time to construct each combination: O(n * 4^n)
-    // S.C: O(n * 4^n)
-    //                    The depth of the recursion is equal to the number of digits n, leading to a space complexity of O(n)
-    //                    for the recursion stack. Each combination is stored in a vector. There are 4^n combinations, and each
-    //                    combination is of length n. Therefore, the space required to store the combinations is O(n * 4^n).
+    // S.C: O(n * 4^n)    The depth of the recursion is equal to the number of digits n, leading to a space complexity
+    //                    of O(n) for the recursion stack. Each combination is stored in a vector. There are
+    //                    4^n combinations, and each combination is of length n. Therefore, the space required to store
+    //                    the combinations is O(n * 4^n).
     vector<string> letterCombinations(string digits) {
         vector<string> ans;
         if(digits.length() == 0) return ans;   // for empty string input, return blank vector of strings
         string ds;   // will be used as helper data structure to store temporary answers
-        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         findCombination(0, ans, ds, mapping, digits);
         return ans;
     }
