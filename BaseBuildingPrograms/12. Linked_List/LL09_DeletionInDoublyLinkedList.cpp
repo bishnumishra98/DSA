4f96f935095ca2338fbf80f9a52f2cff09bfff7f
Bishnu Mishra <mishravishnu369@gmail.com>
2025-11-08 15:19:46 +0530

    nm

--- BaseBuildingPrograms/12. Linked_List/LL09_DeletionInDoublyLinkedList.cpp
+++ BaseBuildingPrograms/12. Linked_List/LL09_DeletionInDoublyLinkedList.cpp
@@ -1,53 +1,53 @@
 // A Node in linked list can be deleted in any one of the three areas:
 // 1) At head of linked list:
-//      Step 1: Create a temporary node, i.e. Node* temp = head
-//      Step 2: Make the node just next to head, as the new head node, i.e. head = head->next
-//      Step 3: Unlink temp from linked list, i.e. temp->next = NULL
-//      Step 4: Unlink head from temp, i.e. head->prev = NULL
-//      Step 5: Delete temp node, i.e. delete temp
+//      Step 1: Create a temporary node, i.e., Node* temp = head
+//      Step 2: Make the node just next to head, as the new head node, i.e., head = head->next
+//      Step 3: Unlink temp from linked list, i.e., temp->next = NULL
+//      Step 4: Unlink head from temp, i.e., head->prev = NULL
+//      Step 5: Delete temp node, i.e., delete temp
 // 2) At tail of linked list:
-//      Step 1: Find tail and mark its previous node by prevNode, i.e. Node* prevNode = tail->prev
-//      Step 2: Unlink prevNode from tail of linked list, i.e. prevNode->next = NULL
+//      Step 1: Find tail and mark its previous node by prevNode, i.e., Node* prevNode = tail->prev
+//      Step 2: Unlink prevNode from tail of linked list, i.e., prevNode->next = NULL
 //      Step 3: Unlink prev of tail from prevNode of linked list, i.e tail->prev = NULL
-//      Step 4: Delete tail node, i.e. delete tail
-// 3) In between the linked list, i.e. at a particular position(let positions start from 1):
+//      Step 4: Delete tail node, i.e., delete tail
+// 3) In between the linked list, i.e., at a particular position(let positions start from 1):
 //      Step 1: Initialize a node named 'currNode' with head.
 //      Step 2: Move forward the 'currNode' pointer in such a way that the 'currNode' node reaches
 //              the position where deletion is to be performed. For example, if 3rd Node is to be
 //              deleted, then the 'currNode' pointer should be pointing at the 3rd node of the
 //              linked list.
 //      Step 3: Initialize a new node named 'prevNode' as prev of currNode,
-//              i.e. Node* prevNode = currNode->prev
+//              i.e., Node* prevNode = currNode->prev
 //              Similarly, Initialize one more new node named 'nextNode' as next of currNode,
-//              i.e. Node* nextNode = currNode->next
-//      Step 4: Repoint prevNode to nextNode, i.e. prevNode->next = nextNode,
-//              and point prev of nextNode to preNode, i.e. nextNode->prev = prevNode
-//      Step 5: Unlink currNode from linked list, i.e. currNode->prev = NULL, currNode->next = NULL
+//              i.e., Node* nextNode = currNode->next
+//      Step 4: Repoint prevNode to nextNode, i.e., prevNode->next = nextNode,
+//              and point prev of nextNode to preNode, i.e., nextNode->prev = prevNode
+//      Step 5: Unlink currNode from linked list, i.e., currNode->prev = NULL, currNode->next = NULL
 
 #include <iostream>
 using namespace std;
 
-// interface of Node for doubly linked list
+// Interface of Node for doubly linked list
 class Node {
-    public:
-        int data;
-        Node* prev;
-        Node* next;
-        // Constructors
-        Node() : prev(NULL), next(NULL) {}
-        Node(int data) : data(data), prev(NULL), next(NULL) {}
+public:
+    int data;
+    Node* prev;
+    Node* next;
+    // Constructors
+    Node() : prev(NULL), next(NULL) {}
+    Node(int data) : data(data), prev(NULL), next(NULL) {}
 
-        // Destructor
-        ~Node() {
-            cout << "Destructor called for the Node: " << this->data << endl;
-            delete prev;   // releasing dynamically allocated memory
-            delete next;   // releasing dynamically allocated memory
-            prev = NULL;   // setting prev to NULL to avoid potential issues
-            next = NULL;   // setting next to NULL to avoid potential issues
-        }
+    // Destructor
+    ~Node() {
+        cout << "Destructor called for the Node: " << this->data << endl;
+        delete prev;   // releasing dynamically allocated memory
+        delete next;   // releasing dynamically allocated memory
+        prev = NULL;   // setting prev to NULL to avoid potential issues
+        next = NULL;   // setting next to NULL to avoid potential issues
+    }
 };
 
-// function to print elements of linked list
+// Function to print elements of linked list
 void printElements(Node* head) {
     Node* temp = head;
     while(temp != NULL) {
@@ -56,7 +56,7 @@ void printElements(Node* head) {
     }
 }
 
-// function to find length of linked list, i.e. no.of nodes
+// Function to find length of linked list, i.e., no.of nodes
 int lengthLL(Node* head) {
     Node* temp = head;
     int len = 0;
@@ -67,9 +67,9 @@ int lengthLL(Node* head) {
     return len;
 }
 
-// function to find tail of linked list. This function returns address of tail Node of linked list.
+// Function to find tail of linked list. This function returns address of tail Node of linked list.
 Node* findTail(Node* head) {
-    if(head == NULL) {   // if linked list is empty, i.e. there are no nodes, then tail will
+    if(head == NULL) {   // if linked list is empty, i.e., there are no nodes, then tail will
                         // also point to NULL.
         return NULL;
     }
@@ -126,14 +126,14 @@ void deleteAtPosition(Node* &head, int position) {
 }
 
 int main() {
-    // creating nodes
+    // Creating nodes
     Node* first = new Node(2);
     Node* second = new Node(4);
     Node* third = new Node(6);
     Node* fourth = new Node(8);
     Node* fifth = new Node(10);
 
-    // creating linked list
+    // Creating linked list
     first->next = second;
     second->next = third;
     third->next = fourth;
