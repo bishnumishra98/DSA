9b8019ee66ffc5c644b619ff409ee2243e3f2410
Bishnu Mishra <mishravishnu369@gmail.com>
2025-11-08 18:25:34 +0530

    nm

--- BaseBuildingPrograms/12. Linked_List/LL12_CyclesInLL.cpp
+++ BaseBuildingPrograms/12. Linked_List/LL12_CyclesInLL.cpp
@@ -4,21 +4,21 @@
 #include <map>
 using namespace std;
 
-// interface of Node for singly linked list
+// Interface of Node for singly linked list
 class Node {
-    public:
-        int data;
-        Node* next;
-        Node() {
-            this->next = NULL;
-        }
-        Node(int data) {
-            this->data = data;
-            this->next = NULL;
-        }
+public:
+    int data;
+    Node* next;
+    Node() {
+        this->next = NULL;
+    }
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
 };
 
-// finding cycles using map
+// Finding cycles using map
 // T.C: O(n)
 // S.C: O(n)
 bool hasCycle_map(Node *head) {
@@ -30,17 +30,17 @@ bool hasCycle_map(Node *head) {
         // Thus we will mark this Node as visited, by marking true.
             table[temp] = true;
         } else {
-            // if table[temp] is true, this Node is visited before, which signifies a cycle.
+            // If table[temp] is true, this Node is visited before, which signifies a cycle.
             return true;
         }
         temp = temp->next;
     }
-    // if loop ends, it means complete linked list was traversed without any cycles.
+    // If loop ends, it means complete linked list was traversed without any cycles.
     // Thus, we return false signifying that linked list contains no cycles.
     return false;
 }
 
-// finding cycles using two pointers / tortoise and hare algorithm / Floyd's cycle-finding algorithm
+// Finding cycles using two pointers / tortoise and hare algorithm / Floyd's cycle-finding algorithm
 // T.C: O(n)
 // S.C: O(1)
 bool hasCycle_twoPointer(Node* head) {
@@ -51,7 +51,7 @@ bool hasCycle_twoPointer(Node* head) {
         fast = fast->next->next;
         slow = slow->next;
         if(fast == slow) {
-            // if cycle is present in LL, fast and slow pointers will certainly meet at a certain point.
+            // If cycle is present in LL, fast and slow pointers will certainly meet at a certain point.
             return true;
         }
     }
@@ -73,7 +73,7 @@ int main() {
     node5->next = node3;   // node5 points to node3, making a cycle
 
     // cout << hasCycle_map(head);
-    cout << hasCycle_twoPointer(head);
+    cout << hasCycle_twoPointer(head);   // o/p: 1 (true)
     
     return 0;
 }
