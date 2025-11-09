053d6f11bc4f60c3a06a7326cbc3bbfe9ce2b903
Bishnu Mishra <mishravishnu369@gmail.com>
2025-11-09 15:00:36 +0530

    nm

--- BaseBuildingPrograms/12. Linked_List/LL15_AddOneToLL.cpp
+++ BaseBuildingPrograms/12. Linked_List/LL15_AddOneToLL.cpp
@@ -23,16 +23,16 @@ using namespace std;
 
 // Interface of Node for singly linked list
 class Node {
-    public:
-        int data;
-        Node* next;
-        Node() {
-            this->next = NULL;
-        }
-        Node(int data) {
-            this->data = data;
-            this->next = NULL;
-        }
+public:
+    int data;
+    Node* next;
+    Node() {
+        this->next = NULL;
+    }
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
 };
 
 // Function to print elements of linked list
@@ -61,38 +61,38 @@ Node* reverseLL(Node* head) {
 // Approach 1
 // T.C: O(n)
 // S.C: O(1)
-// Node* addOneToLL(Node* head) {
-//     head = reverseLL(head);   // reversing list to make addition easy       
-//     Node* temp = head;
+Node* addOneToLL_easy(Node* head) {
+    head = reverseLL(head);   // reversing list to make addition easy       
+    Node* temp = head;
     
-//     while(1) {
-//         temp->data += 1;   // adding 1 to temp node
+    while(1) {
+        temp->data += 1;   // adding 1 to temp node
         
-//         if(temp->data < 10) {    // if 'data < 10', no need of any carry. We can return reversed list.
-//             return reverseLL(head);
-//         } else {   // else we continue with making data as 0 and add 1(carry) to next node
-//             temp->data = 0;
-//         }
+        if(temp->data < 10) {    // if 'data < 10', no need of any carry. We can return reversed list.
+            return reverseLL(head);
+        } else {   // else we continue with making data as 0 and add 1(carry) to next node
+            temp->data = 0;
+        }
             
-//         if(temp->next != NULL) {   // if we didn't reached last node of LL, keep moving forward
-//             temp = temp->next;
-//         } else {
-//             break;   // If last node of LL is reached, break from the loop
-//         }
-//     }
+        if(temp->next != NULL) {   // if we didn't reached last node of LL, keep moving forward
+            temp = temp->next;
+        } else {
+            break;   // If last node of LL is reached, break from the loop
+        }
+    }
 
-//     // Reaching here implies that the last node's data is still not less than 10, and the LL has
-//     // ended. This means there is a carry of 1 left to be inserted after the last node. This only
-//     // happens when input is all 9. For example: 999, 9999, etc. Thus, we insert a new node
-//     // with data as 1 at the end of the LL and return the reversed list.
-//     temp->next = new Node(1);  // adding new node for the carried 1
-//     return reverseLL(head);
-// }
+    // Reaching here implies that the last node's data is still not less than 10, and the LL has
+    // ended. This means there is a carry of 1 left to be inserted after the last node. This only
+    // happens when input is all 9. For example: 999, 9999, etc. Thus, we insert a new node
+    // with data as 1 at the end of the LL and return the reversed list.
+    temp->next = new Node(1);  // adding new node for the carried 1
+    return reverseLL(head);
+}
 
 // Approach 2
 // T.C: O(n)
 // S.C: O(1)
-Node* addOneToLL(Node* head) {
+Node* addOneToLL_generalised(Node* head) {
     head = reverseLL(head);   // reversing list to make addition easy 
     Node* p = head;
     Node* temp = NULL;   // temp will denote tail at end of while loop
@@ -130,7 +130,8 @@ int main() {
 
     printLL(head);
     cout << "\n";
-    Node* ans = addOneToLL(head);
+    // Node* ans = addOneToLL_easy(head);
+    Node* ans = addOneToLL_generalised(head);
     printLL(ans);
 
     return 0;
