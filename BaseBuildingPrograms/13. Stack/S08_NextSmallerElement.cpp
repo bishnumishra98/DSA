d8b12981a4d71c5828fbc8c4c90232e2c8b921da
Bishnu Mishra <mishravishnu369@gmail.com>
2025-11-24 16:18:42 +0530

    nm

--- BaseBuildingPrograms/13. Stack/S08_NextSmallerElement.cpp
+++ BaseBuildingPrograms/13. Stack/S08_NextSmallerElement.cpp
@@ -11,17 +11,26 @@
 // Input:   2  4  5  3  3
 // Output: -1  3  3 -1 -1
 
-// Algorithm:
-// Stack behaviour: bigger elements on bottom, smaller elements on top
-// 1) We know that no elements are present on the right of last element of the array. Thus,
+// Bruteforce Algorithm:
+// 1. Traverse the array from left to right.
+// 2. For each element at ith index, traverse the array from (i + 1)th index to rightmost index, and check for the first
+//    element smaller than the element at ith index.
+// 3. If found, place that element at ith index of special array, else place -1 there.
+
+// Optimal Algorithm:
+// CHEAT CODE OF THIS PROBLEM: Increasing stack + RL traversal
+// Stack objective: smaller elements at bottom, bigger elements on top
+// Array traversal: Right to Left
+// 1. We know that no elements are present on the right of last element of the array. Thus,
 //    last element in special array will always be -1. So initialize a stack with -1.
-// 2) Traversing from right to left of given array, we have to compare each element of array with
-//    top element of stack. If array element is greater than stack element, then that stack element
-//    must be stored in the special array on the same index as that of the element in original array.
-//    Post this, push the array element on the stack, and it becomes the new top element of stack.
-// 3) If the array element is not greater than stack element, pop out stack elements until array
-//    element becomes greater than stack element. Then follow the same process as we followed above.
-//    Follow the above two steps until we reach leftmost element of array.
+// 2. Traversing from right to left of given array, we have to compare each element of array with
+//    top element of stack.
+//    If top element of stack is bigger than or equal to current array element, we pop out the top element of stack
+//    until we find a smaller element than current array element. In ensures the stack has smaller elements
+//    at bottom and bigger elements at top.
+// 3. After popping out all the bigger or equal elements than current array element, the top element of stack
+//    will be the next smaller element for current array element. We store that top element of stack in special array.
+// 4. Finally, we push the current array element into stack and move to the next element on left of current array element.
 
 #include <iostream>
 #include <vector>
@@ -41,13 +50,13 @@ vector<int> nextSmallerElement(vector<int>& arr) {
     for(int i = size - 1; i >= 0; i--) {
         int currElement = arr[i];
 
-        // Popping stack until currElement becomes greater than top element of stack
-        while(currElement <= st.top()) st.pop();
+        // Continuously pop out top element of stack if top element of stack is greater than or equal to current array element
+        while(st.top() >= currElement) st.pop();
 
-        // Storing top element of stack in ans
+        // Store top element of stack in ans
         ans[i] = st.top();
 
-        // Pushing array element into stack
+        // Push the bigger array element into stack now
         st.push(currElement);
     }
 
