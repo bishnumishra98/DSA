29e9d91071c6bf10ee988cfe495027b2575947d2
Bishnu Mishra <mishravishnu369@gmail.com>
2025-11-26 01:24:30 +0530

    nm

--- Leetcode&Gfg/07. Stack/MinStack.cpp
+++ Leetcode&Gfg/07. Stack/MinStack.cpp
@@ -1,93 +1,37 @@
-// Leetcode: 155. Min Stack   --->   Design a stack that supports push, pop, top, and retrieving
-// the minimum element in constant time. Implement the MinStack class:
-// -> MinStack() initializes the stack object.
-// -> void push(int val) pushes the element val onto the stack.
-// -> void pop() removes the element on the top of the stack.
-// -> int top() gets the top element of the stack.
-// -> int getMin() retrieves the minimum element in the stack.
-// -> You must implement a solution with O(1) time complexity for each function.
+// Leetcode: 155. Min Stack   --->   Design a stack that supports push, pop, top, and retrieving the minimum element
+// in constant time.
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+// Implement the MinStack class:
 
-// Algorithm: We will create a vector of pairs. And store the elements of stack in 1st part of the
-//            pair, and the smallest element of the stack till then, in the 2nd part pf pair.
+// ● MinStack() initializes the stack object.
+// ● void push(int val) pushes the element val onto the stack.
+// ● void pop() removes the element on the top of the stack.
+// ● int top() gets the top element of the stack.
+// ● int getMin() retrieves the minimum element in the stack.
+// ● You must implement a solution with O(1) time complexity for each function.
 
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-/*
-// Detailed code:
 class MinStack {
+private:
+    vector<pair<int, int>> st;
+    
 public:
-    vector <pair<int, int>> st;
-
-    // constructor
-    MinStack() { }
-    
-    // T.C: O(1)
-    void push(int val) {
-        if(st.empty()) {
-            // if stack is empty, it means we are pushing the 1st element; i.e., it itself is
-            // only the smallest element. So we push 'val' in both 'p.first' and 'p.second'.
-            pair<int, int> p;
-            p.first = val;
-            p.second = val;
-            st.push_back(p);
-        } else {
-            // if stack is non empty, then we push 'val' is 'p.first', and smallest element
-            // of the stack till now, in 'p.second'.
-            pair<int, int> p;
-            p.first = val;
-            int previousMin = st.back().second;
-            p.second = min(previousMin, val);
-            st.push_back(p);
-        }
-    }
-    
-    // T.C: O(1)
-    void pop() {
-        st.pop_back();
-    }
-    
-    // T.C: O(1)
-    int top() {
-        pair<int, int> rightMostPair= st.back();
-        return rightMostPair.first;
-    }
-    
-    // T.C: O(1)
-    int getMin() {
-        pair<int, int> rightMostPair= st.back();
-        return rightMostPair.second;
-    }
-};
-*/
-
-// Concise code
-class MinStack {
-public:
-    vector <pair<int, int>> st;
     MinStack() {
-        
+        // No need to do anything here as vector 'st' will be initialized
+        // automatically when MinStack object is created.
     }
     
     void push(int val) {
         if(st.empty()) {
-            // if vector is empty, it means we are pushing the 1st element; i.e., it itself is
-            // only the smallest element. So we push 'val' in both 1st and 2nd part of pair.
+            // If vector is empty, it means we are pushing the 1st element; i.e., it itself is
+            // only the smallest element. So we push the same 'val' in both 1st and 2nd part of pair.
             st.push_back({val, val});
         } else {
-            // if stack is non empty, then we push 'val' is 1st part,
+            // If stack is non empty, then we push 'val' in 1st part of pair,
             // and smallest element of the stack till now, in 2nd part of pair.
-            st.push_back({val, min(val, st.back().second)});
+            st.push_back({val, min(val, st.back().second)});   // st.back() gives the last pair in the vector
         }
     }
     
@@ -104,7 +48,7 @@ public:
     }
 };
 
-/*
+
 int main() {
     // Create a MinStack static object
     MinStack obj;
@@ -115,36 +59,12 @@ int main() {
     obj.push(5);
     obj.push(1);
 
-    cout << "Top element: " << obj.top() << endl;
-    cout << "Minimum element: " << obj.getMin() << endl;
+    cout << "Top element: " << obj.top() << endl;   // o/p: 1
+    cout << "Minimum element: " << obj.getMin() << endl;   // o/p: 1
 
     obj.pop();
-    cout << "After pop, top element: " << obj.top() << endl;
-    cout << "After pop, minimum element: " << obj.getMin() << endl;
-
-    return 0;
-}
-*/
-
-int main() {
-    // Create a MinStack dynamic object
-    MinStack* obj = new MinStack();
-
-    // Perform operations
-    obj->push(3);
-    obj->push(2);
-    obj->push(5);
-    obj->push(1);
-
-    cout << "Top element: " << obj->top() << endl;
-    cout << "Minimum element: " << obj->getMin() << endl;
-
-    obj->pop();
-    cout << "After pop, top element: " << obj->top() << endl;
-    cout << "After pop, minimum element: " << obj->getMin() << endl;
-
-    // Cleanup
-    delete obj;
+    cout << "After pop, top element: " << obj.top() << endl;   // o/p: 5
+    cout << "After pop, minimum element: " << obj.getMin() << endl;   // o/p: 2
 
     return 0;
 }
