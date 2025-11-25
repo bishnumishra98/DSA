db15cdf1443e83b3948011add37cdc3e1a2a3981
Bishnu Mishra <mishravishnu369@gmail.com>
2025-11-26 01:58:32 +0530

    nm

--- Leetcode&Gfg/07. Stack/ValidParentheses.cpp
+++ Leetcode&Gfg/07. Stack/ValidParentheses.cpp
@@ -16,11 +16,8 @@
 // Input: s = "(]"
 // Output: false
 
-// Note: This problem looks easy, but the if else conditions can be tricky.
-//       So don't take this problem lightly.
 
-#include <iostream>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
@@ -33,27 +30,25 @@ public:
         for(int i = 0; i < s.length(); i++) {
             char ch = s[i];
 
-            if(ch == '(' || ch == '[' || ch == '{') {
-                // if opening brackets found
+            if(ch == '(' || ch == '[' || ch == '{') {   // if opening brackets found
                 st.push(ch);
-            } else {
-                // if closing brackets found
+            } else {   // if closing brackets found
                 if(!st.empty()) {
-                    if((ch == ')' && st.top() == '(') ||
-                    (ch == ']' && st.top() == '[') ||
-                    (ch == '}' && st.top() == '{')) {
+                    if((ch == ')' && st.top() == '(') || (ch == ']' && st.top() == '[') || (ch == '}' && st.top() == '{')) {
                         st.pop();
                     } else {
-                        // if closing brackets found, but none of the above if conditions matched,
+                        // If closing brackets found, but none of the above if conditions matched,
                         // it means a closing bracket doesn't have its corresponding opening bracket.
                         return false;
                     }
                 } else {
-                    // stack is empty, but closing bracket found.
+                    // Stack is empty, but closing bracket found.
                     return false;
                 }
             }
         }
+
+        // After complete traversal of string, if stack is empty, then valid parentheses; else invalid.
         if(st.empty()) return true;
         else return false;
     }
@@ -63,7 +58,7 @@ int main() {
     Solution obj;
     string s = "(])";
 
-    cout << obj.isValid(s);
+    cout << obj.isValid(s);   // o/p: 0 (false)
 
     return 0;
 }
