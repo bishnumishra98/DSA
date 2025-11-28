c2fa3772d197384ceff0c04a01ef4a8e633d65b7
Bishnu Mishra <mishravishnu369@gmail.com>
2025-11-28 22:44:06 +0530

    nm

--- Leetcode&Gfg/08. Queue/FirstNonRepeatingCharacterInAStream.cpp
+++ Leetcode&Gfg/08. Queue/FirstNonRepeatingCharacterInAStream.cpp
@@ -19,21 +19,17 @@
 
 // Problem link: https://www.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1
 
-// Algorithm:
-// 1) Take one character at a time from the string, increase its count in the 'freq' array,
-//    and push it into the queue.
-// 2) Process the queue:
-//    i.   While the queue is not empty, continuously check if the front character of the queue
-//         occurred only once in the string according to the 'freq' array. If it occurred only
-//         once, store it in the 'ans' string and break out of the loop.
-//    ii.  If the front character occurred more than once, pop it out from the queue until an
-//         element comes to the front of the queue, which occurred only once according to
-//         the 'freq' array.
-//    iii. If the queue becomes empty during the popping process, store '#' in 'ans'.
+// Algorithm: It is simple.
+// The idea is to maintain a frequency array to store the frequency of each character in the stream.
+// We also maintain a queue to store the characters in the order they appear in the stream.
+// For each character in the stream, we do the following:
+// 1. Increment its frequency in the frequency array.
+// 2. Push it into the queue.
+// 3. While the front character of the queue has frequency more than 1, we pop it from the queue.
+// 4. If the queue is empty, it means there is no non-repeating character in the stream so far, we append '#' to the answer.
+//    Otherwise, we append the front character of the queue to the answer.
+// Finally, we return the answer string.
 
-// Q) Why we used queue in this question ?
-// A) Because with the help of a queue data structure, we can iterate elements from L to R in
-//    a string/array. Further, queue will help us keep a track of unique characters of string.
 
 #include <iostream>
 #include <queue>
@@ -42,42 +38,31 @@ using namespace std;
 // T.C: O(n)
 // S.C: O(1)
 string firstNonRepeatingCharacter(string str) {
-    int n = str.length();
-    string ans(n, ' ');   // initialized a string of length 'n' with spaces
-    queue<char> q;
-    int freq[26] = {0};   // as there can be only 26 alphabets in English
-    // int freq[n] = {x}; means initializing 1st element of array with 'x', and rest with '0'.
+    vector<int> freq(26, 0);   // frequency of each lowercase letter
+    queue<char> q;   // store characters in order of arrival
+    string ans;
 
-    for(int i=0; i<n; i++) {
-        char ch = str[i];
-        freq[ch - 'a']++;
-        q.push(ch);
+    for(char ch: str) {
+        int idx = ch - 'a';
+        freq[idx]++;   // increment frequency
+        q.push(ch);   // push this character into queue
 
-        // Process the queue to find the first non-repeating character
-        while(!q.empty()) {
-            char frontChar = q.front();
-            if(freq[frontChar - 'a'] > 1) {
-                // If frontChar occurred more than once, remove it from the queue
-                q.pop();
-            } else {
-                // If frontChar occurred only once, i.e., 'freq[frontChar - 'a'] == 1',
-                // update the 'ans' and break the loop
-                ans[i] = frontChar;
-                break;
-            }
+        // Remove characters from the front of the queue which have frequency more than 1, because 'q' is
+        // supposed to store only characters that have appeared only once, i.e., characters with frequency 1.
+        while(!q.empty() && freq[q.front() - 'a'] > 1) {
+            q.pop();
         }
 
-        // If the queue is empty, set the ans[i] character to '#'
-        if(q.empty()) {
-            ans[i] = '#';
-        }
+        // If queue is empty, no unique character is found; thus append '#'; else append front character
+        if(q.empty()) ans.push_back('#');
+        else ans.push_back(q.front());
     }
 
     return ans;
 }
 
 int main() {
-    string str = "ababc";
+    string str = "aabac";
     cout << firstNonRepeatingCharacter(str);
 
     return 0;
