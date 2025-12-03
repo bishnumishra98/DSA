32e96f4f2aa4915cac4b5ca6da030acc1cea08fa
Bishnu Mishra <mishravishnu369@gmail.com>
2025-12-03 18:34:17 +0530

    nm

--- Leetcode&Gfg/09. Binary_Tree/BalancedBinaryTree.cpp
+++ Leetcode&Gfg/09. Binary_Tree/BalancedBinaryTree.cpp
@@ -1,11 +1,24 @@
 // Leetcode: 110. Balanced Binary Tree   --->   Given a binary tree, determine if it is height-balanced.
-// A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+// A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never
+// differs by more than one.
 
 // Example 1:
 // Input: root = [3,9,20,null,null,15,7]
+//       3
+//      / \
+//     9  20
+//        / \
+//       15  7
 // Output: true
 
 // Example 2:
+//       1
+//      / \
+//     2   2
+//    / \
+//   3   3
+//  / \
+// 4   4
 // Input: root = [1,2,2,3,3,null,null,4,4]
 // Output: false
 
