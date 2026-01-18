e85a3c9a6d210f7ec0f28591f5ee9be8eef1dc91
Bishnu Mishra <mishravishnu369@gmail.com>
2026-01-18 22:51:29 +0530

    nm

--- Leetcode&Gfg/14. Graphs/02_FindTheNumberOfIslands.cpp
+++ Leetcode&Gfg/14. Graphs/02_FindTheNumberOfIslands.cpp
@@ -1,4 +1,4 @@
-// GFG. Find the number of islands   --->   Given a grid of size n*m (n is the number of rows and m is the
+// GFG: Find the number of islands   --->   Given a grid of size n*m (n is the number of rows and m is the
 // number of columns in the grid) consisting of '0's (Water) and '1's(Land). Find the number of islands.
 // Note: An island is either surrounded by water or boundary of grid and is formed by connecting
 // adjacent lands horizontally or vertically or diagonally i.e., in all 8 directions.
@@ -18,7 +18,7 @@
 //                {0,0,1,1,0,1,0}}
 // Output: 2
 
-// Problem link: https://www.geeksforgeeks.org/problems/number-of-islands/1
+// Problem link: https://www.geeksforgeeks.org/problems/find-the-number-of-islands/1
 
 // Understand 'NumberOfProvinces.cpp' before this problem. This problem also has a similar approach.
 // Algorithm:
